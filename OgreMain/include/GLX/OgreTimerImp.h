292abea846f61fc42092422a2a85ecd4e21170ab
Steve Streeting <steve@stevestreeting.com>
2008-06-15 21:26:43 +0000

    More allocator work

--- OgreMain/include/GLX/OgreTimerImp.h
+++ OgreMain/include/GLX/OgreTimerImp.h
@@ -35,7 +35,7 @@ Torus Knot Software Ltd.
 namespace Ogre
 {
 	/** Timer class */
-	class _OgreExport Timer
+	class _OgreExport Timer : public TimerAlloc
 	{
 	private:
 		struct timeval start;
--- OgreMain/include/OSX/OgreTimerImp.h
+++ OgreMain/include/OSX/OgreTimerImp.h
@@ -35,7 +35,7 @@ Torus Knot Software Ltd.
 namespace Ogre
 {
 	/** Timer class */
-	class Timer
+	class Timer : public TimerAlloc
 	{
 	private:
 		struct timeval start;
