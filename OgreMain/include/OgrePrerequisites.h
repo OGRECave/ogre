2e7f18acad226cd44de14e3b59d997d06be79b33
Steve Streeting <steve@stevestreeting.com>
2008-10-10 13:11:55 +0000

    Version bump, remove RC1

--- OgreMain/include/OgrePrerequisites.h
+++ OgreMain/include/OgrePrerequisites.h
@@ -101,7 +101,7 @@ namespace Ogre {
     #define OGRE_VERSION_MAJOR 1
     #define OGRE_VERSION_MINOR 6
     #define OGRE_VERSION_PATCH 0
-	#define OGRE_VERSION_SUFFIX "RC1"
+	#define OGRE_VERSION_SUFFIX ""
     #define OGRE_VERSION_NAME "Shoggoth"
 
     #define OGRE_VERSION    ((OGRE_VERSION_MAJOR << 16) | (OGRE_VERSION_MINOR << 8) | OGRE_VERSION_PATCH)
