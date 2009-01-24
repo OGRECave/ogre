488dd5d8da03919f0099cf385fe98ec98582d8f8
Steve Streeting <steve@stevestreeting.com>
2009-01-24 17:58:37 +0000

    Remove a few unused params & code sections from STLAllocator

--- OgreMain/include/OgrePrerequisites.h
+++ OgreMain/include/OgrePrerequisites.h
@@ -100,7 +100,7 @@ namespace Ogre {
     // Define ogre version
     #define OGRE_VERSION_MAJOR 1
     #define OGRE_VERSION_MINOR 7
-    #define OGRE_VERSION_PATCH 0
+    #define OGRE_VERSION_PATCH 1
 	#define OGRE_VERSION_SUFFIX ""
     #define OGRE_VERSION_NAME "Cthugha"
 
