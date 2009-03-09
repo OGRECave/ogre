32a60ef7408cdcaee289e773f4dd16813b1d3a50
Holger Frydrych <h.frydrych@gmx.de>
2009-03-09 23:12:53 +0000

    CMake: Added Static postfix to libs in static build

--- CMake/Utils/OgreInstall.cmake
+++ CMake/Utils/OgreInstall.cmake
@@ -11,10 +11,16 @@ elseif (APPLE)
 endif ()
 
 function(ogre_install_lib LIBNAME)
+  # add static prefix, if compiling static version
+  if (OGRE_STATIC)
+    set_target_properties(${LIBNAME} PROPERTIES
+      OUTPUT_NAME ${LIBNAME}Static
+    )
+  endif ()
   install(TARGETS ${LIBNAME}
     RUNTIME DESTINATION "bin${OGRE_RELEASE_PATH}" CONFIGURATIONS Release MinSizeRel RelWithDebInfo
-	LIBRARY DESTINATION "lib" CONFIGURATIONS Release MinSizeRel RelWithDebInfo
-	ARCHIVE DESTINATION "lib" CONFIGURATIONS Release MinSizeRel RelWithDebInfo
+	LIBRARY DESTINATION "lib" CONFIGURATIONS Release MinSizeRel RelWithDebInfo None
+	ARCHIVE DESTINATION "lib" CONFIGURATIONS Release MinSizeRel RelWithDebInfo None
   )
   install(TARGETS ${LIBNAME}
     RUNTIME DESTINATION "bin${OGRE_DEBUG_PATH}" CONFIGURATIONS DEBUG
@@ -24,10 +30,16 @@ function(ogre_install_lib LIBNAME)
 endfunction(ogre_install_lib)
 
 function(ogre_install_plugin PLUGINNAME)
+  # add static prefix, if compiling static version
+  if (OGRE_STATIC)
+    set_target_properties(${PLUGINNAME} PROPERTIES
+      OUTPUT_NAME ${PLUGINNAME}Static
+    )
+  endif ()
   install(TARGETS ${PLUGINNAME}
     RUNTIME DESTINATION "bin${OGRE_RELEASE_PATH}" CONFIGURATIONS Release MinSizeRel RelWithDebInfo
-	LIBRARY DESTINATION "lib${OGRE_PLUGIN_PATH}" CONFIGURATIONS Release MinSizeRel RelWithDebInfo
-	ARCHIVE DESTINATION "lib${OGRE_PLUGIN_PATH}" CONFIGURATIONS Release MinSizeRel RelWithDebInfo
+	LIBRARY DESTINATION "lib${OGRE_PLUGIN_PATH}" CONFIGURATIONS Release MinSizeRel RelWithDebInfo None
+	ARCHIVE DESTINATION "lib${OGRE_PLUGIN_PATH}" CONFIGURATIONS Release MinSizeRel RelWithDebInfo None
   )
   install(TARGETS ${PLUGINNAME}
     RUNTIME DESTINATION "bin${OGRE_DEBUG_PATH}" CONFIGURATIONS DEBUG
@@ -39,7 +51,7 @@ endfunction(ogre_install_plugin)
 function(ogre_install_sample SAMPLENAME)
   if (OGRE_INSTALL_SAMPLES)
     install(TARGETS ${SAMPLENAME}
-      RUNTIME DESTINATION "bin${OGRE_RELEASE_PATH}" CONFIGURATIONS Release MinSizeRel RelWithDebInfo OPTIONAL
+      RUNTIME DESTINATION "bin${OGRE_RELEASE_PATH}" CONFIGURATIONS Release MinSizeRel RelWithDebInfo None OPTIONAL
     )
     install(TARGETS ${SAMPLENAME}
       RUNTIME DESTINATION "bin${OGRE_DEBUG_PATH}" CONFIGURATIONS DEBUG OPTIONAL
