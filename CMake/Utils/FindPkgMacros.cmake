b42c8db6be02eb831211d2cf22a35c8cee376ccf
Steve Streeting <steve@stevestreeting.com>
2009-03-15 15:23:23 +0000

    Fix small typo in findpkg_framework macro (${fwk} instead of {fwk})

--- CMake/Utils/FindPkgMacros.cmake
+++ CMake/Utils/FindPkgMacros.cmake
@@ -107,7 +107,7 @@ MACRO(findpkg_framework fwk)
     FOREACH(dir ${${fwk}_FRAMEWORK_PATH})
       SET(fwkpath ${dir}/${fwk}.framework)
       IF(EXISTS ${fwkpath})
-        SET(${fwk}_FRAMEWORK_INCLUDES ${{fwk}_FRAMEWORK_INCLUDES}
+        SET(${fwk}_FRAMEWORK_INCLUDES ${${fwk}_FRAMEWORK_INCLUDES}
           ${fwkpath}/Headers ${fwkpath}/PrivateHeaders)
         if (NOT ${fwk}_LIBRARY_FWK)
           SET(${fwk}_LIBRARY_FWK "-framework ${fwk}" CACHE STRING "${fwk} library")
