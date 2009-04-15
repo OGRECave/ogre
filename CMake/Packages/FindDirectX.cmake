34cc72b3a4050f45b7f48ab48ac8016ec692e9a4
Holger Frydrych <h.frydrych@gmx.de>
2009-04-15 16:01:30 +0000

    Modified FindDirectX.cmake to behave closer to our other find scripts. Should fix part 2 of 2765664.

--- CMake/Packages/FindDirectX.cmake
+++ CMake/Packages/FindDirectX.cmake
@@ -6,51 +6,49 @@
 # DirectX_LIBRARY
 # DirectX_ROOT_DIR
 
-set(DirectX_FOUND false)
-
 if(WIN32) # The only platform it makes sense to check for DirectX SDK
-
-  # We start with Microsoft DirectX SDK (August 2008) 9.24.1400
-  # Example of path is C:\apps_x86\Microsoft DirectX SDK (August 2008)\Include
-  find_path(DirectX_INCLUDE_DIR d3d9.h
-    "$ENV{DXSDK_DIR}/Include"
-    "C:/apps_x86/Microsoft DirectX SDK*/Include"
-    "C:/Program Files (x86)/Microsoft DirectX SDK*/Include"
-    "C:/apps/Microsoft DirectX SDK*/Include"
-    "C:/Program Files/Microsoft DirectX SDK*/Include"
-    )
-  mark_as_advanced(DirectX_INCLUDE_DIR)
+  include(FindPkgMacros)
+  findpkg_begin(DirectX)
+  
+  # construct search paths
+  set(DirectX_PREFIX_PATH ${DXSDK_DIR} $ENV{DXSDK_DIR}
+    "C:/apps_x86/Microsoft DirectX SDK*"
+    "C:/Program Files (x86)/Microsoft DirectX SDK*"
+    "C:/apps/Microsoft DirectX SDK*"
+    "C:/Program Files/Microsoft DirectX SDK*"
+	"$ENV{ProgramFiles}/Microsoft DirectX SDK*"
+  )
+  create_search_paths(DirectX)
+  # redo search if prefix path changed
+  clear_if_changed(DirectX_PREFIX_PATH
+    DirectX_LIBRARY
+	DirectX_INCLUDE_DIR
+	DirectX_ROOT_DIR
+  )
+  
+  find_path(DirectX_INCLUDE_DIR NAMES d3d9.h HINTS ${DirectX_INC_SEARCH_PATH})
+  # dlls are in DirectX_ROOT_DIR/Developer Runtime/x64|x86
+  # lib files are in DirectX_ROOT_DIR/Lib/x64|x86
+  if(CMAKE_CL_64)
+    set(DirectX_LIBPATH_SUFFIX "x64")
+  else(CMAKE_CL_64)
+    set(DirectX_LIBPATH_SUFFIX "x86")
+  endif(CMAKE_CL_64)
+  find_library(DirectX_LIBRARY NAMES d3d9 HINTS ${DirectX_LIB_SEARCH_PATH} PATH_SUFFIXES ${DirectX_LIBPATH_SUFFIX})
+  
   if(DirectX_INCLUDE_DIR)
     set(DirectX_ROOT_DIR "${DirectX_INCLUDE_DIR}/..") 
   endif(DirectX_INCLUDE_DIR)
 
-  # dlls are in DirectX_ROOT_DIR/Developer Runtime/x64|x86
-  # lib files are in DirectX_ROOT_DIR/Lib/x64|x86
 
-  set(DirectX_LIBRARY_PATHS)
-  if(CMAKE_CL_64)
-    set(DirectX_LIBRARY_PATHS 
-      "${DirectX_ROOT_DIR}/Lib/x64")
-  else(CMAKE_CL_64)
-    set(DirectX_LIBRARY_PATHS 
-      "${DirectX_ROOT_DIR}/Lib/x86" "${DirectX_ROOT_DIR}/Lib")
-  endif(CMAKE_CL_64)
+  findpkg_finish(DirectX)
 
-  find_library(DirectX_LIBRARY d3d9 ${DirectX_LIBRARY_PATHS} NO_DEFAULT_PATH)
-  if(DirectX_INCLUDE_DIR AND DirectX_LIBRARY)
-    set(DirectX_FOUND true)
-  endif(DirectX_INCLUDE_DIR AND DirectX_LIBRARY)
-  mark_as_advanced(DirectX_LIBRARY)
-
-  #message("DirectX_FOUND=${DirectX_FOUND}")
-  #message("DirectX_INCLUDE_DIR=${DirectX_INCLUDE_DIR}")
-  #message("DirectX_LIBRARY=${DirectX_LIBRARY}")
-  #message("DirectX_ROOT_DIR=${DirectX_ROOT_DIR}")
-  
   # look for D3D10.1 components
   if (DirectX_FOUND)
-    find_path(DirectX_D3D10_INCLUDE_DIR NAMES d3d10_1shader.h HINTS ${DirectX_INCLUDE_DIR})
-	find_library(DirectX_D3D10_LIBRARY NAMES d3d10 HINTS ${DirectX_LIBRARY_PATHS} NO_DEFAULT_PATH)
+    find_path(DirectX_D3D10_INCLUDE_DIR NAMES d3d10_1shader.h HINTS ${DirectX_INCLUDE_DIR} NO_DEFAULT_PATH)
+	get_filename_component(DirectX_LIBRARY_DIR "${DirectX_LIBRARY}" PATH)
+	message(STATUS "DX lib dir: ${DirectX_LIBRARY_DIR}")
+	find_library(DirectX_D3D10_LIBRARY NAMES d3d10 HINTS ${DirectX_LIBRARY_DIR} NO_DEFAULT_PATH)
 	if (DirectX_D3D10_INCLUDE_DIR AND DirectX_D3D10_LIBRARY)
 	  set(DirectX_D3D10_FOUND TRUE)
 	  set(DirectX_D3D10_INCLUDE_DIRS ${DirectX_D3D10_INCLUDE_DIR})
