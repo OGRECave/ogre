32f5607b2a5da37c16edd53747a57125a34a104b
Steve Streeting <steve@stevestreeting.com>
2009-06-12 17:03:36 +0000

    Ditch the static templates, too much trouble with finnicky gcc and the fixed enum limits the extensibility anyway

--- Components/Property/include/OgreProperty.h
+++ Components/Property/include/OgreProperty.h
@@ -159,9 +159,22 @@ namespace Ogre
 		/// Get a string name of a property type
 		static const String& getTypeName(PropertyType theType);
 
-		/// Get the expected type code for a given value
-		template<typename T>
-		static PropertyType getTypeForValue(const T& val) { return PROP_UNKNOWN; }
+		static PropertyType PropertyDef::getTypeForValue(const short& val) { return PROP_SHORT; }
+		static PropertyType PropertyDef::getTypeForValue(const unsigned short& val) { return PROP_UNSIGNED_SHORT; }
+		static PropertyType PropertyDef::getTypeForValue(const int& val) { return PROP_INT; }
+		static PropertyType PropertyDef::getTypeForValue(const unsigned int& val) { return PROP_UNSIGNED_INT; }
+		static PropertyType PropertyDef::getTypeForValue(const long& val) { return PROP_LONG; }
+		static PropertyType PropertyDef::getTypeForValue(const unsigned long& val) { return PROP_UNSIGNED_LONG; }
+		static PropertyType PropertyDef::getTypeForValue(const Real& val) { return PROP_REAL; }
+		static PropertyType PropertyDef::getTypeForValue(const String& val) { return PROP_STRING; }
+		static PropertyType PropertyDef::getTypeForValue(const Vector2& val) { return PROP_VECTOR2; }
+		static PropertyType PropertyDef::getTypeForValue(const Vector3& val) { return PROP_VECTOR3; }
+		static PropertyType PropertyDef::getTypeForValue(const Vector4& val) { return PROP_VECTOR4; }
+		static PropertyType PropertyDef::getTypeForValue(const ColourValue& val) { return PROP_COLOUR; }
+		static PropertyType PropertyDef::getTypeForValue(const bool& val) { return PROP_BOOL; }
+		static PropertyType PropertyDef::getTypeForValue(const Quaternion& val) { return PROP_QUATERNION; }
+		static PropertyType PropertyDef::getTypeForValue(const Matrix3& val) { return PROP_MATRIX3; }
+		static PropertyType PropertyDef::getTypeForValue(const Matrix4& val) { return PROP_MATRIX4; }
 
 	protected:
 		// no default construction
@@ -172,22 +185,6 @@ namespace Ogre
 		PropertyType mType;
 
 	};
-	template<> static PropertyType PropertyDef::getTypeForValue(const short& val) { return PROP_SHORT; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const unsigned short& val) { return PROP_UNSIGNED_SHORT; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const int& val) { return PROP_INT; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const unsigned int& val) { return PROP_UNSIGNED_INT; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const long& val) { return PROP_LONG; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const unsigned long& val) { return PROP_UNSIGNED_LONG; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const Real& val) { return PROP_REAL; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const String& val) { return PROP_STRING; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const Vector2& val) { return PROP_VECTOR2; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const Vector3& val) { return PROP_VECTOR3; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const Vector4& val) { return PROP_VECTOR4; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const ColourValue& val) { return PROP_COLOUR; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const bool& val) { return PROP_BOOL; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const Quaternion& val) { return PROP_QUATERNION; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const Matrix3& val) { return PROP_MATRIX3; }
-	template<> static PropertyType PropertyDef::getTypeForValue(const Matrix4& val) { return PROP_MATRIX4; }
 
 	/// Map from property name to shared definition
 	typedef map<String, PropertyDef>::type PropertyDefMap;
