302673d4a834fc3dd45b36860e0e0350d81e1fc5
Steve Streeting <steve@stevestreeting.com>
2009-07-03 12:46:29 +0000

    Fix a bug introduced by svn rev 8550 (patch 2693798); HLSL float4 arrays were not working because the RegisterCount (which was used instead of RowCount in the patch) increases, which made it think it was a matrix, and set elementCount to 16 instead of 4 which broke updating using array auto-params. Change the code so that we use the Class element of the description to determine if it's a matrix or a float array to get the correct results.

--- RenderSystems/Direct3D9/src/OgreD3D9HLSLProgram.cpp
+++ RenderSystems/Direct3D9/src/OgreD3D9HLSLProgram.cpp
@@ -364,9 +364,68 @@ namespace Ogre {
 			} // columns
 			break;
 		case D3DXPT_FLOAT:
-			switch(d3dDesc.RegisterCount)
+			switch(d3dDesc.Class)
 			{
-			case 1:
+			case D3DXPC_MATRIX_COLUMNS:
+			case D3DXPC_MATRIX_ROWS:
+				switch(d3dDesc.RegisterCount)
+				{
+				case 2:
+					switch(d3dDesc.Columns)
+					{
+					case 2:
+						def.constType = GCT_MATRIX_2X2;
+						def.elementSize = 8; // HLSL always packs
+						break;
+					case 3:
+						def.constType = GCT_MATRIX_2X3;
+						def.elementSize = 8; // HLSL always packs
+						break;
+					case 4:
+						def.constType = GCT_MATRIX_2X4;
+						def.elementSize = 8; 
+						break;
+					} // columns
+					break;
+				case 3:
+					switch(d3dDesc.Columns)
+					{
+					case 2:
+						def.constType = GCT_MATRIX_3X2;
+						def.elementSize = 12; // HLSL always packs
+						break;
+					case 3:
+						def.constType = GCT_MATRIX_3X3;
+						def.elementSize = 12; // HLSL always packs
+						break;
+					case 4:
+						def.constType = GCT_MATRIX_3X4;
+						def.elementSize = 12; 
+						break;
+					} // columns
+					break;
+				case 4:
+					switch(d3dDesc.Columns)
+					{
+					case 2:
+						def.constType = GCT_MATRIX_4X2;
+						def.elementSize = 16; // HLSL always packs
+						break;
+					case 3:
+						def.constType = GCT_MATRIX_4X3;
+						def.elementSize = 16; // HLSL always packs
+						break;
+					case 4:
+						def.constType = GCT_MATRIX_4X4;
+						def.elementSize = 16; 
+						break;
+					} // columns
+					break;
+
+				} // rows
+				break;
+			case D3DXPC_SCALAR:
+			case D3DXPC_VECTOR:
 				switch(d3dDesc.Columns)
 				{
 				case 1:
@@ -387,61 +446,7 @@ namespace Ogre {
 					break;
 				} // columns
 				break;
-			case 2:
-				switch(d3dDesc.Columns)
-				{
-				case 2:
-					def.constType = GCT_MATRIX_2X2;
-					def.elementSize = 8; // HLSL always packs
-					break;
-				case 3:
-					def.constType = GCT_MATRIX_2X3;
-					def.elementSize = 8; // HLSL always packs
-					break;
-				case 4:
-					def.constType = GCT_MATRIX_2X4;
-					def.elementSize = 8; 
-					break;
-				} // columns
-				break;
-			case 3:
-				switch(d3dDesc.Columns)
-				{
-				case 2:
-					def.constType = GCT_MATRIX_3X2;
-					def.elementSize = 12; // HLSL always packs
-					break;
-				case 3:
-					def.constType = GCT_MATRIX_3X3;
-					def.elementSize = 12; // HLSL always packs
-					break;
-				case 4:
-					def.constType = GCT_MATRIX_3X4;
-					def.elementSize = 12; 
-					break;
-				} // columns
-				break;
-			case 4:
-				switch(d3dDesc.Columns)
-				{
-				case 2:
-					def.constType = GCT_MATRIX_4X2;
-					def.elementSize = 16; // HLSL always packs
-					break;
-				case 3:
-					def.constType = GCT_MATRIX_4X3;
-					def.elementSize = 16; // HLSL always packs
-					break;
-				case 4:
-					def.constType = GCT_MATRIX_4X4;
-					def.elementSize = 16; 
-					break;
-				} // columns
-				break;
-
-			} // rows
-			break;
-			
+			}
 		default:
 			// not mapping samplers, don't need to take the space 
 			break;
