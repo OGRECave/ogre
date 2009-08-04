9d9dc18acd15c67ac065abac72afa54d200ecb7f
David Rogers <davidrogers@me.com>
2009-08-04 13:34:35 +0000

    Fix for bug #0000226.  Also added a couple other safety checks.

--- Mac/Ogre/src/OgreOSXCocoaView.mm
+++ Mac/Ogre/src/OgreOSXCocoaView.mm
@@ -1,27 +1,31 @@
 /*
------------------------------------------------------------------------------
-This source file is part of OGRE
-    (Object-oriented Graphics Rendering Engine)
-For the latest info, see http://www.ogre3d.org/
-
-Copyright (c) 2000-2005 The OGRE Team
-Also see acknowledgements in Readme.html
-
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-http://www.gnu.org/copyleft/lesser.txt.
------------------------------------------------------------------------------
-*/
+ -----------------------------------------------------------------------------
+ This source file is part of OGRE
+ (Object-oriented Graphics Rendering Engine)
+ For the latest info, see http://www.ogre3d.org/
+ 
+ Copyright (c) 2000-2006 Torus Knot Software Ltd
+ Also see acknowledgements in Readme.html
+ 
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later
+ version.
+ 
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License along with
+ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+ http://www.gnu.org/copyleft/lesser.txt.
+ 
+ You may alternatively use this source under the terms of a specific version of
+ the OGRE Unrestricted License provided you have obtained such a license from
+ Torus Knot Software Ltd.
+ -----------------------------------------------------------------------------
+ */
 
 #include "OgreOSXCocoaView.h"
 
@@ -33,18 +37,21 @@ http://www.gnu.org/copyleft/lesser.txt.
 
 - (id)initWithFrame:(NSRect)f
 {
-	[super initWithFrame:f];
-	NSApplicationLoad();
-	
-	window = 0;
-
+	if((self = [super initWithFrame:f]))
+    {
+        NSApplicationLoad();
+        
+        window = 0;
+    }
 	return self;
 }
 
 - (id)initWithGLOSXWindow:(Ogre::RenderWindow*)w
 {
-	[super init];
-	window = w;
+	if((self = [super initWithFrame:[(OgreWindow *)w frame]]))
+    {
+        window = w;
+    }
 	return self;
 }
 
@@ -61,7 +68,8 @@ http://www.gnu.org/copyleft/lesser.txt.
 - (void)setFrameSize:(NSSize)s
 {
 	[super setFrameSize:s];
-	window->windowMovedOrResized();
+    if (window)
+        window->windowMovedOrResized();
 }
 
 - (void)drawRect:(NSRect)r
