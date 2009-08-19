db52cfbeb1528fbc2d48cb9590f38d49547f7f79
Steve Streeting <steve@stevestreeting.com>
2009-08-19 10:54:11 +0000

    GL: when locking a sub-area of a pixel buffer, we must remember the original lock area and not just the PixelBox that is returned (which is rebased). Upload should only upload the area which was locked.

--- OgreMain/include/OgreHardwarePixelBuffer.h
+++ OgreMain/include/OgreHardwarePixelBuffer.h
@@ -53,8 +53,10 @@ namespace Ogre {
         size_t mRowPitch, mSlicePitch;
         // Internal format
         PixelFormat mFormat;
-        // Currently locked region
+        // Currently locked region (local coords)
         PixelBox mCurrentLock;
+		// The current locked box of this surface (entire surface coords)
+		Image::Box mLockedBox;
         
         /// Internal implementation of lock(), must be overridden in subclasses
         virtual PixelBox lockImpl(const Image::Box lockBox,  LockOptions options) = 0;
