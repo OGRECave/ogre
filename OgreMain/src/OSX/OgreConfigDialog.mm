21cc4edd0b37c9ce75c1cc37ab718cd9f9569eae
Steve Streeting <steve@stevestreeting.com>
2009-09-15 14:24:55 +0000

    Documentation updates to reflect our switch to the MIT License for 1.7 Commit being done in batches because it keeps timing out

--- OgreMain/src/OSX/OgreConfigDialog.mm
+++ OgreMain/src/OSX/OgreConfigDialog.mm
@@ -4,7 +4,7 @@
  (Object-oriented Graphics Rendering Engine)
  For the latest info, see http://www.ogre3d.org/
  
- Copyright (c) 2000-2006 Torus Knot Software Ltd
+ Copyright (c) 2000-2009 Torus Knot Software Ltd
  Also see acknowledgements in Readme.html
  
  This program is free software; you can redistribute it and/or modify it under
--- OgreMain/src/OSX/macUtils.cpp
+++ OgreMain/src/OSX/macUtils.cpp
@@ -4,7 +4,7 @@ This source file is part of OGRE
     (Object-oriented Graphics Rendering Engine)
 For the latest info, see http://www.ogre3d.org/
 
-Copyright (c) 2000-2006 Torus Knot Software Ltd
+Copyright (c) 2000-2009 Torus Knot Software Ltd
 Also see acknowledgements in Readme.html
 
 This program is free software; you can redistribute it and/or modify it under
