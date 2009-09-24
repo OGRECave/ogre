ee64074f6181421a0a60da595923bf44e0d4ffd8
Steve Streeting <steve@stevestreeting.com>
2009-09-24 12:12:58 +0000

    Tolerate Renderable::getTechnique returning null in MORecvShadVisitor::visit Backported from r9064 since it affects stable too

--- OgreMain/src/OgreMovableObject.cpp
+++ OgreMain/src/OgreMovableObject.cpp
@@ -406,8 +406,10 @@ namespace Ogre {
 		void visit(Renderable* rend, ushort lodIndex, bool isDebug, 
 			Any* pAny = 0)
 		{
+			Technique* tech = rend->getTechnique();
+			bool techReceivesShadows = tech && tech->getParent()->getReceiveShadows();
 			anyReceiveShadows = anyReceiveShadows || 
-				rend->getTechnique()->getParent()->getReceiveShadows();
+				techReceivesShadows || !tech;
 		}
 	};
 	//---------------------------------------------------------------------
