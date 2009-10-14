a02ab0d5e73c116b710ccdbb6e8c7bd5ab20b308
Noam Gat <noamgat@gmail.com>
2009-10-14 13:42:42 +0000

    Updated credits for sibenik model and same license comment for all soc09-compositor demo files.

--- Samples/Compositor/src/HelperLogics.cpp
+++ Samples/Compositor/src/HelperLogics.cpp
@@ -1,3 +1,17 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2009 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+You may use this sample code for anything you like, it is not covered by the
+same license as the rest of the engine.
+-----------------------------------------------------------------------------
+*/
+
 #include "HelperLogics.h"
 
 #include "Ogre.h"
--- Samples/DeferredShading/include/DeferredShading.h
+++ Samples/DeferredShading/include/DeferredShading.h
@@ -1,3 +1,17 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2009 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+You may use this sample code for anything you like, it is not covered by the
+same license as the rest of the engine.
+-----------------------------------------------------------------------------
+*/
+
 /**
 Implementation of a Deferred Shading engine in OGRE, using Multiple Render Targets and
 CG high level language shaders.
@@ -13,29 +27,10 @@ point light, cone for spot light and quad for directional) to render their contr
 
 The wiki article explaining this demo can be found here :
   http://www.ogre3d.org/wiki/index.php/Deferred_Shading
-*******************************************************************************
-Copyright (c) W.J. van der Laan
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of 
-this software  and associated documentation files (the "Software"), to deal in 
-the Software without restriction, including without limitation the rights to use, 
-copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
-Software, and to permit persons to whom the Software is furnished to do so, subject 
-to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies 
-or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE 
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*******************************************************************************
 */
-#ifndef H_WJ_DeferredShadingSystem
-#define H_WJ_DeferredShadingSystem
+
+#ifndef H_DeferredShadingSystem
+#define H_DeferredShadingSystem
 
 #include "OgreCompositorInstance.h"
 #include "OgreSceneManager.h"
