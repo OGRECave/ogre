233d39c72b9bd6b249b69b3496fd341a78633353
Nir Hasson <nhasson76@hotmail.com>
2009-12-26 16:18:57 +0000

    ShaderGeneratorTechniqueResolverListener modified to resolve the generated technique immediately (Needed by D3D11 / gles2.0 RS)

--- Samples/Common/include/Sample.h
+++ Samples/Common/include/Sample.h
@@ -62,22 +62,43 @@ public:
 	virtual Ogre::Technique* handleSchemeNotFound(unsigned short schemeIndex, 
 		const Ogre::String& schemeName, Ogre::Material* originalMaterial, unsigned short lodIndex, 
 		const Ogre::Renderable* rend)
-	{		
+	{	
+		Ogre::Technique* generatedTech = NULL;
+
 		// Case this is the default shader generator scheme.
 		if (schemeName == Ogre::RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME)
 		{
 			bool techniqueCreated;
 
 			// Create shader generated technique for this material.
-			// Note: Actual material technique and shaders will be generated upon scheme validation within the 
-			// ShaderGenerator::validateScheme method.
 			techniqueCreated = mShaderGenerator->createShaderBasedTechnique(
 				originalMaterial->getName(), 
 				Ogre::MaterialManager::DEFAULT_SCHEME_NAME, 
-				schemeName);				
+				schemeName);	
+
+			// Case technique registration succeeded.
+			if (techniqueCreated)
+			{
+				// Force creating the shaders for the generated technique.
+				mShaderGenerator->validateMaterial(schemeName, originalMaterial->getName());
+				
+				// Grab the generated technique.
+				Ogre::Material::TechniqueIterator itTech = originalMaterial->getTechniqueIterator();
+
+				while (itTech.hasMoreElements())
+				{
+					Ogre::Technique* curTech = itTech.getNext();
+
+					if (curTech->getSchemeName() == schemeName)
+					{
+						generatedTech = curTech;
+						break;
+					}
+				}				
+			}
 		}
 
-		return NULL;
+		return generatedTech;
 	}
 
 protected:	
