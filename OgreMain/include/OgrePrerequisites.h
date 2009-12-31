b4dbba5addc9400163ed56a02f18f979d7a2904a
Steve Streeting <steve@stevestreeting.com>
2009-12-31 11:26:11 +0000

    Changelog & version update ready for 1.7 RC1

--- OgreMain/include/OgrePrerequisites.h
+++ OgreMain/include/OgrePrerequisites.h
@@ -56,7 +56,7 @@ namespace Ogre {
     #define OGRE_VERSION_MAJOR 1
     #define OGRE_VERSION_MINOR 7
     #define OGRE_VERSION_PATCH 0
-	#define OGRE_VERSION_SUFFIX "dev-unstable"
+	#define OGRE_VERSION_SUFFIX "RC1"
     #define OGRE_VERSION_NAME "Cthugha"
 
     #define OGRE_VERSION    ((OGRE_VERSION_MAJOR << 16) | (OGRE_VERSION_MINOR << 8) | OGRE_VERSION_PATCH)
