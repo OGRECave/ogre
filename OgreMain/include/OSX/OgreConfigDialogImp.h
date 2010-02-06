c9706c79ae8ab450eeb4e316dc094d72ce57ec60
David Rogers <davidrogers@me.com>
2010-02-06 01:25:47 +0000

    Applying a couple Mac patches from ejtttje
    2946668 - use framework-relative paths in #includes
    
    2946664 - PluginFolder setting always relative to bundle

--- OgreMain/include/OSX/OgreConfigDialogImp.h
+++ OgreMain/include/OSX/OgreConfigDialogImp.h
@@ -29,10 +29,10 @@ THE SOFTWARE.
 #ifndef __OSXConfigDialog_H__
 #define __OSXConfigDialog_H__
 
-#include "OgrePrerequisites.h"
-#include "OgreRoot.h"
-#include "OgreRenderSystem.h"
-#include "OgreConfigOptionMap.h"
+#include "Ogre/OgrePrerequisites.h"
+#include "Ogre/OgreRoot.h"
+#include "Ogre/OgreRenderSystem.h"
+#include "Ogre/OgreConfigOptionMap.h"
 
 #ifdef __OBJC__
 #import <Cocoa/Cocoa.h>
