9a2af167abee48cd1a70d75a5d32d691d9592479
Assaf Raman <assaf@assaf.co.il>
2010-02-14 15:04:59 +0000

    Symbian port: Fixed environment initialization of the SampleBrowser. Seems that the "main" function needs to be under a "TRAP" statement - else all file read operations crash the program with a "Main Panic E32USER-CBase 66" error. The solution to the problem involved creating a new main function that initialize the environment and from inside it call the original main function (now called mainWithTrap) with a TRAP statment.

--- Samples/Browser/src/SampleBrowser.cpp
+++ Samples/Browser/src/SampleBrowser.cpp
@@ -47,6 +47,41 @@
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 INT WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
+#elif OGRE_PLATFORM == OGRE_PLATFORM_SYMBIAN    
+int mainWithTrap();
+int main()
+{
+	int res = 0;
+    __UHEAP_MARK; 
+ 
+	// Create the control environment.
+	CCoeEnv* environment = new (ELeave) CCoeEnv();    
+
+	TRAPD( err, environment->ConstructL( ETrue, 0 ) );
+
+	if( err != KErrNone )
+	{
+		printf( "Unable to create a CCoeEnv!\n" );
+		getchar();            
+	}
+    
+    TRAP( err, res = mainWithTrap());
+
+    // Close the stdout & stdin, else printf / getchar causes a memory leak.
+    fclose( stdout );
+    fclose( stdin );
+    
+	// Cleanup
+	CCoeEnv::Static()->DestroyEnvironment();
+	delete CCoeEnv::Static();
+       
+    __UHEAP_MARKEND;
+    
+    return res;
+}    
+
+int mainWithTrap()
+
 #else
 int main(int argc, char *argv[])
 #endif
@@ -57,22 +92,6 @@ int main(int argc, char *argv[])
 	[pool release];
 	return retVal;
 #else
-
-#if OGRE_PLATFORM == OGRE_PLATFORM_SYMBIAN    
-    __UHEAP_MARK; 
-
-    // Create the control environment.
-    CCoeEnv* environment = new (ELeave) CCoeEnv();    
-    
-    TRAPD( err, environment->ConstructL( ETrue, 0 ) );
-    
-    if( err != KErrNone )
-    {
-        printf( "Unable to create a CCoeEnv!\n" );
-        getchar();            
-    }
-
-#endif
 
 	try
 	{
@@ -91,17 +110,6 @@ int main(int argc, char *argv[])
 #endif
 
 	}
-#if OGRE_PLATFORM == OGRE_PLATFORM_SYMBIAN    
-    // Close the stdout & stdin, else printf / getchar causes a memory leak.
-    fclose( stdout );
-    fclose( stdin );
-    
-    // Cleanup
-    CCoeEnv::Static()->DestroyEnvironment();
-    delete CCoeEnv::Static();
-       
-    __UHEAP_MARKEND;
-#endif
 
 #endif
 	return 0;
