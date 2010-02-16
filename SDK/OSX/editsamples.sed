59d986ec560cff3db3c80c9968873d75efd4caa1
David Rogers <davidrogers@me.com>
2010-02-16 14:06:42 +0000

    Submitting OS X SDK work in progress.

--- SDK/OSX/editsamples.sed
+++ SDK/OSX/editsamples.sed
@@ -3,6 +3,5 @@ s/ln -s -f \.\.\//ln -s -f /g
 s/\.\.\/\.\.\/Samples\/.*\/include/include/
 s/\.\.\/\.\.\/Samples\/.*\/src/src/
 s/\.\.\/\.\.\/Dependencies/..\/Dependencies/
-s/\.\.\/Dependencies\/include\/CEGUI/..\/Dependencies\/CEGUI.framework\/Headers/
 s/\.\.\/\.\.\/OgreMain\/include/..\/Dependencies\/Ogre.framework\/Headers/
 s/path = Ogre\.framework\; sourceTree = BUILT_PRODUCTS_DIR/name = Ogre.framework\; path = ..\/Dependencies\/Ogre.framework\; sourceTree = SOURCE_ROOT/
--- SDK/OSX/make_osx.sh
+++ SDK/OSX/make_osx.sh
@@ -4,42 +4,48 @@ OGRE_VERSION="v1.7.0"
 # Only build for i386, halves the size
 ARCH="i386"
 REMOVE_ARCH="ppc"
+SDKBUILDDIR=`pwd`
 
-# invoke xcode build
-xcodebuild -project ../../build/OGRE.xcodeproj -alltargets -configuration Release
+# Configure with CMake
+mkdir ../../build
+cd ../../build
+cmake -DOGRE_INSTALL_SAMPLES_SOURCE:BOOL=TRUE -DOGRE_INSTALL_MEDIA:BOOL=TRUE -DOGRE_INSTALL_DOCS:BOOL=TRUE -G Xcode ..
+
+# Invoke xcode build
+xcodebuild -project OGRE.xcodeproj -target install -configuration Release -sdk macosx10.4 ARCHS=i386 GCC_VERSION=4.0 MACOSX_DEPLOYMENT_TARGET=10.4
 # Just release mode, debug is too big
-#xcodebuild -project ../../Mac/Ogre/Ogre.xcodeproj -alltargets -configuration Debug
-
-rm -rf sdk_contents 
-mkdir sdk_contents
-
-# frameworks
-echo Copying frameworks...
-mkdir sdk_contents/Dependencies
-
-# Stuff we've built
-ditto -arch $ARCH ../../build/lib/Release/Ogre.framework sdk_contents/Dependencies/Ogre.framework
-
-# dependencies
-ditto -arch $ARCH ../../Dependencies/Cg.framework sdk_contents/Dependencies/Cg.framework
-ditto -arch $ARCH ../../Dependencies/CEGUI.framework sdk_contents/Dependencies/CEGUI.framework
-
-echo Frameworks copied.
+#xcodebuild -project OGRE.xcodeproj -target install -configuration Debug -sdk macosx10.4 ARCHS=i386 GCC_VERSION=4.0 MACOSX_DEPLOYMENT_TARGET=10.4
 
 # Docs
 echo Building API docs...
-mkdir sdk_contents/docs
 
-# invoke doxygen
-pushd ../../Docs/src
-doxygen html.cfg
+# Invoke doxygen
+pushd ../Docs/src
+doxygen ../../build/html.cfg
 popd
 
-cp -R ../../Docs/api sdk_contents/docs/
-# delete unnecessary files
-rm -f sdk_contents/docs/api/html/*.hhk
-rm -f sdk_contents/docs/api/html/*.map
-rm -f sdk_contents/docs/api/html/*.md5
+cd api/html
+
+# Delete unnecessary files
+rm -f *.hhk
+rm -f *.map
+rm -f *.md5
+rm -f ../*.tmp
+
+# Build the Xcode docset and zip it up to save space
+#make
+#zip -9 -r org.ogre3d.documentation.Reference1_7.docset.zip org.ogre3d.documentation.Reference1_7.docset
+
+rm -rf $SDKBUILDDIR/sdk_contents 
+mkdir $SDKBUILDDIR/sdk_contents
+mkdir $SDKBUILDDIR/sdk_contents/docs
+
+# Copy the docset to the disc image.  Disabled to reduce the size of the disc image.
+#cp -R api $SDKBUILDDIR/sdk_contents/docs/
+#cp org.ogre3d.documentation.Reference1_7.docset.zip $SDKBUILDDIR/sdk_contents/docs/
+
+cd $SDKBUILDDIR
+
 cp -R ../../Docs/manual sdk_contents/docs/
 cp -R ../../Docs/licenses sdk_contents/docs/
 cp ReadMe.html sdk_contents/docs/
@@ -49,19 +55,33 @@ cp -R ../../Docs/*.gif sdk_contents/docs/
 
 echo API generation done.
 
-# do samples
+# frameworks
+echo Copying frameworks...
+mkdir sdk_contents/Dependencies
+
+# Stuff we've built
+ditto -arch $ARCH ../../build/lib/Release/Ogre.framework sdk_contents/Dependencies/Ogre.framework
+ditto -arch $ARCH ../../build/lib/Release/*.dylib sdk_contents/Dependencies/
+
+# Dependencies
+ditto -arch $ARCH ../../Dependencies/Cg.framework sdk_contents/Dependencies/Cg.framework
+
+echo Frameworks copied.
+
+# Do samples
 echo Copying samples...
 mkdir sdk_contents/Samples
 
 # Copy project location
-ditto ../../Mac/Samples sdk_contents/Samples/
-# copy source
+cp -R ../../build/OGRE.xcodeproj sdk_contents/Samples/
+
+# Copy source
 mkdir sdk_contents/Samples/src
 mkdir sdk_contents/Samples/include
 
-find ../../samples -iname *.cpp -exec cp \{\} sdk_contents/Samples/src \;
-find ../../samples -iname *.h -exec cp \{\} sdk_contents/Samples/include \;
-cp ../../ReferenceApplication/BspCollision/src/*.cpp sdk_contents/Samples/src
+find ../../Samples -iname *.cpp -exec cp \{\} sdk_contents/Samples/src \;
+find ../../Samples -iname *.mm -exec cp \{\} sdk_contents/Samples/src \;
+find ../../Samples -iname *.h -exec cp \{\} sdk_contents/Samples/include \;
 
 # Copy dependencies
 mkdir sdk_contents/Dependencies/include
@@ -73,10 +93,10 @@ cp -R ../../Dependencies/include/OIS sdk_contents/Dependencies/include
 cp ../../Dependencies/lib/Release/libois.a sdk_contents/Dependencies/lib/Release/
 
 # Fix up project references (2 stage rather than in-place since in-place only works for single sed commands)
-sed -f editsamples.sed sdk_contents/Samples/Samples.xcodeproj/project.pbxproj > tmp.xcodeproj
-mv tmp.xcodeproj sdk_contents/Samples/Samples.xcodeproj/project.pbxproj
+sed -f editsamples.sed sdk_contents/Samples/OGRE.xcodeproj/project.pbxproj > tmp.pbxproj
+mv tmp.pbxproj sdk_contents/Samples/OGRE.xcodeproj/project.pbxproj
 # Fix up architecture
-sed -i -e "s/$REMOVE_ARCH,//g" sdk_contents/Samples/Samples.xcodeproj/project.pbxproj
+sed -i -e "s/$REMOVE_ARCH,//g" sdk_contents/Samples/OGRE.xcodeproj/project.pbxproj
 
 echo Samples copied.
 
@@ -84,12 +104,17 @@ echo Copying Media...
 
 cp -R ../../Samples/Media sdk_contents/Samples/
 
+# Copy config files
+mkdir sdk_contents/Samples/config/
+cp ../../build/bin/*.cfg sdk_contents/Samples/config/
+
 # Fix up config files
-sed -i -e "s/\.\.\/\.\.\/\.\.\/\.\.\/Samples/..\/..\/..\/Samples/g" sdk_contents/samples/config/resources.cfg
+sed -i -e "s/\.\.\/\.\.\/\.\.\/\.\.\/Samples/..\/..\/..\/Samples/g" sdk_contents/Samples/config/resources.cfg
+sed -i -e "s/\.\.\/\.\.\/\.\.\/\.\.\/Samples/..\/..\/..\/Samples/g" sdk_contents/Samples/config/quakemap.cfg
 
 echo Media copied.
 
-#remove SVN files to avoid increasing the size of the SDK with duplicates
+# Remove SVN files to avoid increasing the size of the SDK with duplicates
 find sdk_contents -iname .svn -exec rm -rf \{\} \;
 
 echo Building DMG...
