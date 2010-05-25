0e9ae52cfa5e025ca893dde3412618698c863327
praetor <unknown>
2010-05-25 18:36:46 -0400

    Enabled Dot3Bump sample for Android.

--- Samples/Browser/include/SampleBrowser.h
+++ Samples/Browser/include/SampleBrowser.h
@@ -1130,7 +1130,7 @@ protected:
 			sampleList.push_back("Sample_CelShading");
 			//sampleList.push_back("Sample_Character");     
 			//sampleList.push_back("Sample_CubeMapping");    
-			//sampleList.push_back("Sample_Dot3Bump");
+			sampleList.push_back("Sample_Dot3Bump");
 			sampleList.push_back("Sample_DynTex");      
 			sampleList.push_back("Sample_FacialAnimation");
 			sampleList.push_back("Sample_Fresnel");
