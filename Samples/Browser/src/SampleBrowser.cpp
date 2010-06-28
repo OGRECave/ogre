80a968e89818402db03e6454a416aae94cd48e85
masterfalcon <unknown>
2010-06-28 00:36:50 -0500

    iPhone: Adding CADisplayLink support to SampleBrowser. Shutdown the app properly on iPhone and disable the loading bar to speed up loading on iOS 4

--- Samples/Browser/src/SampleBrowser.cpp
+++ Samples/Browser/src/SampleBrowser.cpp
@@ -43,6 +43,7 @@
 #include "windows.h"
 #elif OGRE_PLATFORM == OGRE_PLATFORM_IPHONE
 #import <UIKit/UIKit.h> 
+#import <QuartzCore/QuartzCore.h>
 #endif
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
@@ -121,18 +122,48 @@ int main(int argc, char *argv[])
 {
     NSTimer *mTimer;
     OgreBites::SampleBrowser sb;
+
+    // Use of the CADisplayLink class is the preferred method for controlling your animation timing.
+    // CADisplayLink will link to the main display and fire every vsync when added to a given run-loop.
+    // The NSTimer class is used only as fallback when running on a pre 3.1 device where CADisplayLink
+    // isn't available.
+    id mDisplayLink;
+    NSDate* mDate;
+    NSTimeInterval mLastFrameTime;
+    BOOL mDisplayLinkSupported;
 }
 
 - (void)go;
 - (void)renderOneFrame:(id)sender;
 
 @property (retain) NSTimer *mTimer;
+@property (nonatomic) NSTimeInterval mLastFrameTime;
 
 @end
 
 @implementation AppDelegate
 
 @synthesize mTimer;
+@dynamic mLastFrameTime;
+
+- (NSTimeInterval)mLastFrameTime
+{
+    return mLastFrameTime;
+}
+
+- (void)setLastFrameTime:(NSTimeInterval)frameInterval
+{
+    // Frame interval defines how many display frames must pass between each time the
+    // display link fires. The display link will only fire 30 times a second when the
+    // frame internal is two on a display that refreshes 60 times a second. The default
+    // frame interval setting of one will fire 60 times a second when the display refreshes
+    // at 60 times a second. A frame interval setting of less than one results in undefined
+    // behavior.
+    if (frameInterval >= 1)
+    {
+        mLastFrameTime = frameInterval;
+    }
+}
 
 - (void)go {
 
@@ -149,12 +180,27 @@ int main(int argc, char *argv[])
         std::cerr << "An exception has occurred: " <<
         e.getFullDescription().c_str() << std::endl;
     }
+
+    if (mDisplayLinkSupported)
+    {
+        // CADisplayLink is API new to iPhone SDK 3.1. Compiling against earlier versions will result in a warning, but can be dismissed
+        // if the system version runtime check for CADisplayLink exists in -initWithCoder:. The runtime check ensures this code will
+        // not be called in system versions earlier than 3.1.
+        mDate = [[NSDate alloc] init];
+        mLastFrameTime = -[mDate timeIntervalSinceNow];
         
-    mTimer = [NSTimer scheduledTimerWithTimeInterval:(NSTimeInterval)(1.0f / 60.0f)
-                                              target:self
-                                            selector:@selector(renderOneFrame:)
-                                            userInfo:nil
-                                             repeats:YES];
+        mDisplayLink = [NSClassFromString(@"CADisplayLink") displayLinkWithTarget:self selector:@selector(renderOneFrame:)];
+        [mDisplayLink setFrameInterval:mLastFrameTime];
+        [mDisplayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    }
+    else
+    {
+        mTimer = [NSTimer scheduledTimerWithTimeInterval:(NSTimeInterval)(1.0f / 60.0f) * mLastFrameTime
+                                                  target:self
+                                                selector:@selector(renderOneFrame:)
+                                                userInfo:nil
+                                                 repeats:YES];
+    }
     
     [pool release];
 }
@@ -163,6 +209,18 @@ int main(int argc, char *argv[])
     // Hide the status bar
     [[UIApplication sharedApplication] setStatusBarHidden:YES];
 
+    mDisplayLinkSupported = FALSE;
+    mLastFrameTime = 1;
+    mDisplayLink = nil;
+    mTimer = nil;
+
+    // A system version of 3.1 or greater is required to use CADisplayLink. The NSTimer
+    // class is used as fallback when it isn't available.
+    NSString *reqSysVer = @"3.1";
+    NSString *currSysVer = [[UIDevice currentDevice] systemVersion];
+    if ([currSysVer compare:reqSysVer options:NSNumericSearch] != NSOrderedAscending)
+        mDisplayLinkSupported = TRUE;
+    
     [self go];
 }
 
@@ -170,26 +228,40 @@ int main(int argc, char *argv[])
 {
     [sb.mGestureView becomeFirstResponder];
 
-    Root::getSingleton().renderOneFrame((Real)[mTimer timeInterval]);
+    if (mDisplayLinkSupported)
+    {
+        // NSTimerInterval is a simple typedef for double
+        NSTimeInterval currentFrameTime = -[mDate timeIntervalSinceNow];
+        NSTimeInterval differenceInSeconds = currentFrameTime - mLastFrameTime;
+        mLastFrameTime = currentFrameTime;
+
+        Root::getSingleton().renderOneFrame((Real)differenceInSeconds);
+    }
+    else
+    {
+        Root::getSingleton().renderOneFrame((Real)[mTimer timeInterval]);
+    }
 }
         
 - (void)applicationWillTerminate:(UIApplication *)application {
     Ogre::Root::getSingleton().queueEndRendering();
 }
 
-//- (void)applicationWillResignActive:(UIApplication *)application
-//{
-//    // Pause FrameListeners and rendering
-//}
-//
-//- (void)applicationDidBecomeActive:(UIApplication *)application
-//{
-//    // Resume FrameListeners and rendering
-//}
-
 - (void)dealloc {
-    [mTimer release];
-    
+    if (mDisplayLinkSupported)
+    {
+        [mDate release];
+        mDate = nil;
+
+        [mDisplayLink invalidate];
+        mDisplayLink = nil;
+    }
+    else
+    {
+        [mTimer invalidate];
+        mTimer = nil;
+    }
+
     [super dealloc];
 }
 
