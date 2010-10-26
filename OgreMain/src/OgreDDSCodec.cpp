415a844e7b51ed5a6fc999c9eef804deafcc3607
nirhasson <nhasson76@hotmail.com>
2010-10-26 12:43:29 +0200

    DDSCodec - Fixed crash when no render system exists. (Used AtlasImageTool of PU on DDS source files)

--- OgreMain/src/OgreDDSCodec.cpp
+++ OgreMain/src/OgreDDSCodec.cpp
@@ -652,8 +652,8 @@ namespace Ogre {
 
 		if (PixelUtil::isCompressed(sourceFormat))
 		{
-			if (!Root::getSingleton().getRenderSystem()->getCapabilities()
-				->hasCapability(RSC_TEXTURE_COMPRESSION_DXT))
+			if (Root::getSingleton().getRenderSystem() == NULL ||
+				Root::getSingleton().getRenderSystem()->getCapabilities()->hasCapability(RSC_TEXTURE_COMPRESSION_DXT) == false)
 			{
 				// We'll need to decompress
 				decompressDXT = true;
