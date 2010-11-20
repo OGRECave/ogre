9797391e46484d290d7a564ab46d6b7b54c9c1fc
Assaf Raman <assaf@assaf.co.il>
2010-11-20 16:13:12 +0000

    Texture array sample: Added some more textures and documentation.

--- Samples/TextureArray/include/TextureArray.h
+++ Samples/TextureArray/include/TextureArray.h
@@ -57,9 +57,7 @@ protected:
 		mCameraMan->setStyle(CS_ORBIT);
 		mTrayMgr->showCursor();
 
-        TexturePtr tex;
-
-		// the names of the four tex we will use
+		// the names of the textures we will use (all need to be the same size: 512*512 in our case)
 		vector<String>::type texNames;
         texNames.push_back("BeachStones.jpg");
         texNames.push_back("BumpyMetal.jpg");
@@ -71,34 +69,38 @@ protected:
         texNames.push_back("Panels_reflection.png");
         texNames.push_back("RustedMetal.jpg");
         texNames.push_back("spacesky.jpg");
-        texNames.push_back("spacesky.jpg");
         texNames.push_back("terrain_texture.jpg");
         texNames.push_back("texmap2.jpg");
         texNames.push_back("Water01.jpg");
         texNames.push_back("Water02.jpg");
+        texNames.push_back("body.jpg");
+        texNames.push_back("stone1.jpg");
+        texNames.push_back("wall3.jpg");
+        texNames.push_back("sinbad_body.tga");
+        texNames.push_back("sinbad_clothes.tga");
+        texNames.push_back("stevecube_BK.jpg");
+
+        // create the 2d texture array (the depth is the size of the array - number of textures)
+        TexturePtr tex = TextureManager::getSingleton().createManual("TextureArrayTex", 
+            ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, 
+            TEX_TYPE_2D_ARRAY, 
+            512, 512, texNames.size(), 
+            0, 
+            PF_X8R8G8B8 );
 
 
-
+        // add all the textures to a 2d texture array
 		for (size_t i = 0; i < texNames.size(); i++)
 		{
             Image terrainTex;
             terrainTex.load(texNames[i], ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-            if (tex.isNull())
-            {
-                tex = TextureManager::getSingleton().createManual("TextureArrayTex", 
-                    ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, 
-                    TEX_TYPE_2D_ARRAY, 
-                    terrainTex.getWidth(),terrainTex.getHeight(), texNames.size(), 
-                    0, 
-                    PF_X8R8G8B8 );
-            }
             HardwarePixelBufferSharedPtr pixelBufferBuf = tex->getBuffer(0);
             const PixelBox&  currImage = pixelBufferBuf->lock(Box(0,0,i,terrainTex.getHeight(), terrainTex.getHeight(), i+1), HardwareBuffer::HBL_DISCARD);
             PixelUtil::bulkPixelConversion(terrainTex.getPixelBox(), currImage);
             pixelBufferBuf->unlock();
         }
 
-
+        // create material and set the texture unit to our texture
         MaterialManager& matMgr = MaterialManager::getSingleton();
 		MaterialPtr newMat = matMgr.create("TextureArray", ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
 		newMat->getTechnique(0)->getPass(0)->setLightingEnabled(false);
@@ -106,6 +108,7 @@ protected:
 		TextureUnitState* pState = pass->createTextureUnitState();
         pState->setTextureName(tex->getName());
 
+        // the the shader (you must use a shader with texture array - this doesn't work with the fixed func)
         GpuProgramPtr fragProg = GpuProgramManager::getSingleton().createProgram("TexArrayFrag", 
             ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
             "",
@@ -113,29 +116,29 @@ protected:
             "arbfp1");
         fragProg->setSource(lerp_fprog_code);
         fragProg->load();
-
         pass->setFragmentProgram(fragProg->getName());
 
+        // create a plain with float3 tex cord - the third value will be the texture index in our case
 	    ManualObject* textureArrayObject = mSceneMgr->createManualObject("TextureAtlasObject");
-
-	    textureArrayObject->begin(newMat->getName(), RenderOperation::OT_TRIANGLE_LIST);
-
+        
+        // create a quad that uses our material 
         int quadSize = 100;
-	    // triangle 0
-	    textureArrayObject->position(0, 0, 0); //Position
-    	textureArrayObject->textureCoord(0, 0, 0); //UV
-	    textureArrayObject->position(quadSize, 0, 0); //Position
-    	textureArrayObject->textureCoord(1, 0, 0); //UV
-	    textureArrayObject->position(quadSize, quadSize, 0); //Position
-    	textureArrayObject->textureCoord(1, 1, texNames.size()); //UV
+	    textureArrayObject->begin(newMat->getName(), RenderOperation::OT_TRIANGLE_LIST);
+	    // triangle 0 of the quad
+	    textureArrayObject->position(0, 0, 0);
+    	textureArrayObject->textureCoord(0, 0, 0);
+	    textureArrayObject->position(quadSize, 0, 0);
+    	textureArrayObject->textureCoord(1, 0, 0);
+	    textureArrayObject->position(quadSize, quadSize, 0);
+    	textureArrayObject->textureCoord(1, 1, texNames.size());
 
-	    // triangle 1
-	    textureArrayObject->position(0, 0, 0); //Position
-    	textureArrayObject->textureCoord(0, 0, 0); //UV
-	    textureArrayObject->position(quadSize, quadSize, 0); //Position
-    	textureArrayObject->textureCoord(1, 1, texNames.size()); //UV
-	    textureArrayObject->position(0, quadSize, 0); //Position
-    	textureArrayObject->textureCoord(0, 1, texNames.size()); //UV
+	    // triangle 1 of the quad
+	    textureArrayObject->position(0, 0, 0);
+    	textureArrayObject->textureCoord(0, 0, 0); 
+	    textureArrayObject->position(quadSize, quadSize, 0); 
+    	textureArrayObject->textureCoord(1, 1, texNames.size()); 
+	    textureArrayObject->position(0, quadSize, 0);
+    	textureArrayObject->textureCoord(0, 1, texNames.size());
 
 	    textureArrayObject->end();
 
