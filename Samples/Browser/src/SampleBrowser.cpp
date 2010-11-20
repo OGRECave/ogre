3825a403b95dfaa049717659b9eafd95b3b8e5fe
David Rogers <davidrogers@me.com>
2010-11-20 16:28:20 -0600

    OS X: Build fix when targeting 10.5 or earlier.

--- Samples/Browser/src/SampleBrowser.cpp
+++ Samples/Browser/src/SampleBrowser.cpp
@@ -105,7 +105,11 @@ int mainWithTrap()
 @end
 
 
+#if defined(MAC_OS_X_VERSION_10_6) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+#else
+@interface AppDelegate : NSObject
+#endif
 {
     NSTimer *mTimer;
     OgreBites::SampleBrowser sb;
