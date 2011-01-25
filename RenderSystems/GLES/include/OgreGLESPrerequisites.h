67bba8a6164411eff6fc958bbb123a2cd4194aa6
Mattan Furst <matanf@simigon.com>
2011-01-25 15:00:56 +0200

    Fixed GLES not compiling of visual studio compiler

--- RenderSystems/GLES/include/OgreGLESPrerequisites.h
+++ RenderSystems/GLES/include/OgreGLESPrerequisites.h
@@ -128,6 +128,11 @@ extern PFNGLUNMAPBUFFEROESPROC glUnmapBufferOES;
 #    define _OgreGLESExport
 #endif
 
+#if OGRE_COMPILER == OGRE_COMPILER_MSVC
+#define __PRETTY_FUNCTION__ __FUNCTION__
+#endif
+
+
 #define DEBUG_(text) \
     {\
         fprintf(stderr, "%s:%d: %s\n", __FUNCTION__, __LINE__, text); \
@@ -146,6 +151,7 @@ extern PFNGLUNMAPBUFFEROESPROC glUnmapBufferOES;
 #else
     #define GL_CHECK_ERROR {}
 #endif
+
 
 #if ENABLE_GL_CHECK
     #define EGL_CHECK_ERROR \
