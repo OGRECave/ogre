e078d18a5dc640faa76891e6c631d642799175e1
David Rogers <masterfalcon@ogre3d.org>
2011-04-10 20:46:10 -0500

    Bug 409 - System freezes in GLPixelUtil::getMaxMipmaps when width or height is 0.  Bug was reported for GL but could affect GL ES as well.

--- RenderSystems/GLES/src/OgreGLESPixelFormat.cpp
+++ RenderSystems/GLES/src/OgreGLESPixelFormat.cpp
@@ -310,32 +310,25 @@ namespace Ogre  {
     size_t GLESPixelUtil::getMaxMipmaps(size_t width, size_t height, size_t depth,
                                       PixelFormat format)
     {
-        size_t count = 0;
-
-        do {
-            if (width > 1)
-            {
-                width = width / 2;
-            }
-            if (height > 1)
-            {
-                height = height / 2;
-            }
-            if (depth > 1)
-            {
-                depth = depth / 2;
-            }
-            /*
-            NOT needed, compressed formats will have mipmaps up to 1x1
-            if(PixelUtil::isValidExtent(width, height, depth, format))
+		size_t count = 0;
+        if((width > 0) && (height > 0))
+        {
+            do {
+                if(width>1)		width = width/2;
+                if(height>1)	height = height/2;
+                if(depth>1)		depth = depth/2;
+                /*
+                 NOT needed, compressed formats will have mipmaps up to 1x1
+                 if(PixelUtil::isValidExtent(width, height, depth, format))
+                 count ++;
+                 else
+                 break;
+                 */
+                
                 count ++;
-            else
-                break;
-            */
-            count++;
-        } while (!(width == 1 && height == 1 && depth == 1));
-
-        return count;
+            } while(!(width == 1 && height == 1 && depth == 1));
+        }		
+		return count;
     }
 
     size_t GLESPixelUtil::optionalPO2(size_t value)
