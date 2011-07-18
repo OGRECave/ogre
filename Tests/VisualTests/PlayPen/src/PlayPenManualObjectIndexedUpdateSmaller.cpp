ecfbbe284287738600dd1b4fadc1b0df8a59fe88
Riley Adams <praetor57@gmail.com>
2011-07-18 14:52:32 -0700

    Converted material schemes, 6 variations of manual object, and simple multi SceneManager tests.

---
+++ Tests/VisualTests/PlayPen/src/PlayPenManualObjectIndexedUpdateSmaller.cpp
@@ -0,0 +1,62 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+Copyright (c) 2000-2009 Torus Knot Software Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+#include "PlayPenTests.h"
+
+PlayPen_ManualObjectIndexedUpdateSmaller::PlayPen_ManualObjectIndexedUpdateSmaller()
+{
+	mInfo["Title"] = "PlayPen_ManualObjectIndexedUpdateSmaller";
+	mInfo["Description"] = "Tests.";
+	addScreenshotFrame(250);
+}
+//----------------------------------------------------------------------------
+
+void PlayPen_ManualObjectIndexedUpdateSmaller::setupContent()
+{
+	PlayPen_ManualObjectIndexed::setupContent();
+	ManualObject* man = static_cast<ManualObject*>(
+	mSceneMgr->getMovableObject("test", ManualObjectFactory::FACTORY_TYPE_NAME));
+	
+	
+	man->beginUpdate(0);
+	// 1 tri smaller
+	man->position(-20, 20, 20);
+	man->normal(0, 0, 1);
+	man->textureCoord(0, 0);
+	
+	man->position(-20, -20, 20);
+	man->normal(0, 0, 1);
+	man->textureCoord(0, 1);
+	
+	man->position(20, -20, 20);
+	man->normal(0, 0, 1);
+	man->textureCoord(1, 1);
+	
+	man->triangle(0, 1, 2);
+	
+	man->end();
+	
+}
---
+++ Tests/VisualTests/PlayPen/src/PlayPenMaterialSchemes.cpp
@@ -0,0 +1,61 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+Copyright (c) 2000-2009 Torus Knot Software Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+#include "PlayPenTests.h"
+
+PlayPen_MaterialSchemes::PlayPen_MaterialSchemes()
+{
+	mInfo["Title"] = "PlayPen_MaterialSchemes";
+	mInfo["Description"] = "Tests.";
+	addScreenshotFrame(250);
+}
+//----------------------------------------------------------------------------
+
+void PlayPen_MaterialSchemes::setupContent()
+{
+	
+	Entity *ent = mSceneMgr->createEntity("robot", "robot.mesh");
+	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent);
+	mSceneMgr->setAmbientLight(ColourValue(0.8, 0.8, 0.8));
+	
+	MaterialPtr mat = MaterialManager::getSingleton().create("schemetest", 
+	ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+	// default scheme
+	mat->getTechnique(0)->getPass(0)->createTextureUnitState("GreenSkin.jpg");
+	
+	Technique* t = mat->createTechnique();
+	t->setSchemeName("newscheme");
+	t->createPass()->createTextureUnitState("rockwall.tga");
+	ent->setMaterialName("schemetest");
+	
+	// create a second viewport using alternate scheme
+	Viewport* vp = mWindow->addViewport(mCamera, 1, 0.75, 0, 0.25, 0.25);
+	vp->setMaterialScheme("newscheme");
+	vp->setOverlaysEnabled(false);
+
+	mCamera->setPosition(0,75,200);
+	
+}
