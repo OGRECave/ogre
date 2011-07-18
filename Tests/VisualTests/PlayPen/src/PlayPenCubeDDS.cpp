9f45a2d1b09321bf8d17eb567cb0b3705404efa4
Riley Adams <praetor57@gmail.com>
2011-07-18 16:33:52 -0700

    Converted billboard texture coord, texture blitting, DDS cube, and depth bias playpen tests.

---
+++ Tests/VisualTests/PlayPen/src/PlayPenCubeDDS.cpp
@@ -0,0 +1,56 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+Copyright (c) 2000-2009 Torus Knot Software Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+#include "PlayPenTests.h"
+
+PlayPen_CubeDDS::PlayPen_CubeDDS()
+{
+	mInfo["Title"] = "PlayPen_CubeDDS";
+	mInfo["Description"] = "Tests.";
+	addScreenshotFrame(250);
+}
+//----------------------------------------------------------------------------
+
+void PlayPen_CubeDDS::setupContent()
+{
+	Ogre::ResourceGroupManager::getSingleton().initialiseResourceGroup("Tests");
+	
+	MaterialPtr mat = MaterialManager::getSingleton().create("testcube", 
+	ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+	Pass* p = mat->getTechnique(0)->getPass(0);
+	p->setLightingEnabled(false);
+	TextureUnitState* t = p->createTextureUnitState();
+	t->setTextureName("grace_cube.dds", TEX_TYPE_CUBE_MAP);
+	t->setTextureAddressingMode(TextureUnitState::TAM_CLAMP);
+	t->setEnvironmentMap(true, TextureUnitState::ENV_REFLECTION);
+	Entity* e = mSceneMgr->createEntity("1", "sphere.mesh");
+	e->setMaterialName(mat->getName());
+	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(e);
+	
+	mCamera->setPosition(300,0,0);
+	mCamera->lookAt(Vector3::ZERO);
+	
+}
---
+++ Tests/VisualTests/PlayPen/src/PlayPenDepthBias.cpp
@@ -0,0 +1,85 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+Copyright (c) 2000-2009 Torus Knot Software Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+#include "PlayPenTests.h"
+
+PlayPen_DepthBias::PlayPen_DepthBias()
+{
+	mInfo["Title"] = "PlayPen_DepthBias";
+	mInfo["Description"] = "Tests.";
+	addScreenshotFrame(250);
+}
+//----------------------------------------------------------------------------
+
+void PlayPen_DepthBias::setupContent()
+{
+	Ogre::ResourceGroupManager::getSingleton().initialiseResourceGroup("Tests");
+	
+	mSceneMgr->setAmbientLight(ColourValue::White);
+	
+	MaterialPtr mat = MaterialManager::getSingleton().create("mat1", 
+	ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+	Pass* p = mat->getTechnique(0)->getPass(0);
+	p->createTextureUnitState("BumpyMetal.jpg");
+	
+	const String meshName("cube.mesh"); 
+	Entity* entity = mSceneMgr->createEntity("base", meshName);
+	entity->setMaterialName("mat1");
+	mSceneMgr->getRootSceneNode()->attachObject(entity);
+	
+	
+	entity = mSceneMgr->createEntity("base2", meshName);
+	entity->setMaterialName("Examples/SphereMappedRustySteel");
+	SceneNode* n = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+	n->setPosition(-30, 0, 0);
+	n->yaw(Degree(45));
+	n->attachObject(entity);
+	
+	for (size_t i = 0; i <= 6;++i)
+	{
+		String name("decal");
+		name += StringConverter::toString(i);
+		
+		MaterialPtr pMat = static_cast<MaterialPtr>(MaterialManager::getSingleton().create(
+		name, ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME));
+		
+		pMat->getTechnique(0)->getPass(0)->setLightingEnabled(false);
+		pMat->getTechnique(0)->getPass(0)->setAlphaRejectSettings(CMPF_GREATER_EQUAL, 128);
+		pMat->getTechnique(0)->getPass(0)->setDepthBias(i);
+		pMat->getTechnique(0)->getPass(0)->createTextureUnitState(name + ".png");
+		
+		entity = mSceneMgr->createEntity(name, meshName);
+		entity->setMaterialName(name);
+		mSceneMgr->getRootSceneNode()->attachObject(entity);
+	}
+	
+	
+	
+	mCamera->setPosition(0,0,200);
+	mCamera->lookAt(Vector3::ZERO);
+	
+	
+}
