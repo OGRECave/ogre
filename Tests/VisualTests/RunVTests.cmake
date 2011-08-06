b621185c898beb9e8343560fafad2b56a9b8a910
Riley Adams <praetor57@gmail.com>
2011-08-06 14:32:52 -0700

    Cleaned up CTest setup somewhat.

---
+++ Tests/VisualTests/RunVTests.cmake
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------
+# This file is part of the CMake build system for OGRE
+#     (Object-oriented Graphics Rendering Engine)
+# For the latest info, see http://www.ogre3d.org/
+#
+# The contents of this file are placed in the public domain. Feel
+# free to make use of it in any way you like.
+#-------------------------------------------------------------------
+
+# strip spaces from render system name
+string(REPLACE " " "" TEST_CONTEXT_RENDER_SYSTEM_NOSPACE ${TEST_CONTEXT_RENDER_SYSTEM})
+
+# delete any existing results first
+exec_program("cmake -E remove ${TEST_CONTEXT_RESULT_DIR}/TestResults_${TEST_CONTEXT_RENDER_SYSTEM_NOSPACE}.txt")
+
+# needs the ./ in Unix-like systems
+if (UNIX)
+  set (TEST_CONTEXT_EXECUTABLE ./${TEST_CONTEXT_EXECUTABLE})
+endif (UNIX)
+
+# now run the context (TODO: add commandline args (namely rendersystem...))
+exec_program("cmake -E chdir ${TEST_CONTEXT_PATH} ${TEST_CONTEXT_EXECUTABLE}")
