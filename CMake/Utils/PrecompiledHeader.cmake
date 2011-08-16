bab0fa8611e041ab7757ec027208214887e70a92
Riley Adams <praetor57@gmail.com>
2011-08-16 14:58:44 -0700

    Small cleanup, changed PCH config to be disabled for NMake builds (which are necessary for automated building in Windows).

--- CMake/Utils/PrecompiledHeader.cmake
+++ CMake/Utils/PrecompiledHeader.cmake
@@ -16,7 +16,8 @@
 macro(use_precompiled_header TARGET HEADER_FILE SRC_FILE)
   get_filename_component(HEADER ${HEADER_FILE} NAME)
 
-  if (MSVC AND NOT OGRE_UNITY_BUILD)
+  # Use MSVC_IDE to exclude NMake from using PCHs
+  if (MSVC_IDE AND NOT OGRE_UNITY_BUILD)
 	add_definitions(/Yu"${HEADER}")
     set_source_files_properties(${SRC_FILE}
       PPROPERTIES COMPILE_FLAGS /Yc"${HEADER}"
