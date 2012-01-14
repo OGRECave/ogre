70d2392d9ab734b6dd1a980edcdc0824577814fc
Holger Frydrych <h.frydrych@gmx.de>
2012-01-14 18:22:35 +0100

    Updated version number to 1.7.4

--- OgreMain/include/OgrePrerequisites.h
+++ OgreMain/include/OgrePrerequisites.h
@@ -55,7 +55,7 @@ namespace Ogre {
     // Define ogre version
     #define OGRE_VERSION_MAJOR 1
     #define OGRE_VERSION_MINOR 7
-    #define OGRE_VERSION_PATCH 3
+    #define OGRE_VERSION_PATCH 4
 	#define OGRE_VERSION_SUFFIX ""
     #define OGRE_VERSION_NAME "Cthugha"
 
