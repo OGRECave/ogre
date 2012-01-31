2377bf64628e7e5319dc27f031fe7d3400ed64cd
David Rogers <masterfalcon@ogre3d.org>
2012-01-31 01:53:06 -0600

    Backout changeset 8e86b8a79230562c7bd44081d7f5903f071c5de0

--- OgreMain/include/OgreScriptTranslator.h
+++ OgreMain/include/OgreScriptTranslator.h
@@ -248,6 +248,22 @@ namespace Ogre{
 		CompositionPassTranslator();
 		void translate(ScriptCompiler *compiler, const AbstractNodePtr &node);
 	};
+	class _OgreExport CompositionPassClearTranslator : public ScriptTranslator
+	{
+	protected:
+		CompositionPass *mPass;
+	public:
+		CompositionPassClearTranslator();
+		void translate(ScriptCompiler *compiler, const AbstractNodePtr &node);
+	};
+	class _OgreExport CompositionPassStencilTranslator : public ScriptTranslator
+	{
+	protected:
+		CompositionPass *mPass;
+	public:
+		CompositionPassStencilTranslator();
+		void translate(ScriptCompiler *compiler, const AbstractNodePtr &node);
+	};
 
 	/**************************************************************************
 	 * BuiltinScriptTranslatorManager
@@ -270,6 +286,8 @@ namespace Ogre{
 		CompositionTechniqueTranslator mCompositionTechniqueTranslator;
 		CompositionTargetPassTranslator mCompositionTargetPassTranslator;
 		CompositionPassTranslator mCompositionPassTranslator;
+		CompositionPassClearTranslator mCompositionPassClearTranslator;
+		CompositionPassStencilTranslator mCompositionPassStencilTranslator;
 	public:
 		BuiltinScriptTranslatorManager();
 		/// Returns the number of translators being managed
