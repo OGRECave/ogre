2c6da8c70abf558b14207ee5d23056e1a0252dcf
David Rogers <masterfalcon@ogre3d.org>
2012-02-23 23:12:47 -0600

    Add new search path for iOS SDK thanks to Xcode 4.3

--- CMake/Packages/FindiOSSDK.cmake
+++ CMake/Packages/FindiOSSDK.cmake
@@ -9,7 +9,7 @@ findpkg_begin(iOS\ SDK)
 # construct search paths
 set(iOSSDK_PREFIX_PATH ${iOSSDK_HOME} $ENV{iOSSDK_HOME}
   ${OGRE_HOME} $ENV{OGRE_HOME})
-create_search_paths(/Developer/Platforms)
+create_search_paths(/Developer/Platforms /Applications/Xcode.app/Contents/Developer/Platforms)
 # redo search if prefix path changed
 clear_if_changed(iOSSDK_PREFIX_PATH
   iOSSDK_LIBRARY_FWK
@@ -25,7 +25,7 @@ get_debug_names(iOSSDK_LIBRARY_NAMES)
 
 #findpkg_framework(UIKit)
 
-find_path(iOSSDK_INCLUDE_DIR iPhoneOS.platform /Developer/Platforms)
+find_path(iOSSDK_INCLUDE_DIR iPhoneOS.platform /Developer/Platforms /Applications/Xcode.app/Contents/Developer/Platforms)
 
 if (iOSSDK_INCLUDE_DIR)
   unset(iOSSDK_INCLUDE_DIR)
