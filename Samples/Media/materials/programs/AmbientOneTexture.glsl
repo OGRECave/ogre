ad5fa4344011e24ef33f504ba12f16d4986a61ec
David Rogers <masterfalcon@ogre3d.org>
2012-04-10 22:18:34 -0500

    Fix a few more sample issues in both DX and GL

--- Samples/Media/materials/programs/AmbientOneTexture.glsl
+++ Samples/Media/materials/programs/AmbientOneTexture.glsl
@@ -1,14 +1,11 @@
 uniform vec4 ambient;
-uniform mat4 worldViewProj;
-
-attribute vec4 position;
 
 /*
   Basic ambient lighting vertex program
 */
 void main()
 {
-	gl_Position = worldViewProj * position;
+	gl_Position = ftransform();
 	gl_TexCoord[0] = gl_MultiTexCoord0;
 	gl_FrontColor = ambient;
 }
