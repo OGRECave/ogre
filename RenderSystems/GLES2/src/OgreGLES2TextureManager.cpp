d368901ae47170d7f2fcaf5f44451d4ec13d7924
wolfmanfx <unknown>
2012-08-04 13:38:39 +0200

    GLES/GLES2 move error checks into the right place

--- RenderSystems/GLES2/src/OgreGLES2TextureManager.cpp
+++ RenderSystems/GLES2/src/OgreGLES2TextureManager.cpp
@@ -34,8 +34,7 @@ THE SOFTWARE.
 namespace Ogre {
     GLES2TextureManager::GLES2TextureManager(GLES2Support& support)
         : TextureManager(), mGLSupport(support), mWarningTextureID(0)
-    {
-        GL_CHECK_ERROR;
+    {        
         // Register with group manager
         ResourceGroupManager::getSingleton()._registerResourceManager(mResourceType, this);
     }
@@ -76,6 +75,7 @@ namespace Ogre {
             }
         }
 
+		GL_CHECK_ERROR;
         // Create GL resource
         glGenTextures(1, &mWarningTextureID);
         GL_CHECK_ERROR;
