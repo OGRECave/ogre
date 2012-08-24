0556a09dd3c250c7299414eaaf49662601ae7c89
philiplb <philip@philiplb.de>
2012-08-24 12:52:28 +0200

    added missing include

--- RenderSystems/Direct3D9/src/OgreD3D9HardwareBufferManager.cpp
+++ RenderSystems/Direct3D9/src/OgreD3D9HardwareBufferManager.cpp
@@ -25,6 +25,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 -----------------------------------------------------------------------------
 */
+#include "OgreD3D9RenderSystem.h"
 #include "OgreD3D9HardwareBufferManager.h"
 #include "OgreD3D9HardwareVertexBuffer.h"
 #include "OgreD3D9HardwareIndexBuffer.h"
