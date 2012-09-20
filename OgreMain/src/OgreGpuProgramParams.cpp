6158fd954ba37bfafa86e4d7ad5f2b454467a71c
lunkhound <unknown>
2012-09-20 13:37:41 -0700

    bugfix: matrix arrays in shared params are broken (only copied the first element if matrix transposition was enabled)

--- OgreMain/src/OgreGpuProgramParams.cpp
+++ OgreMain/src/OgreGpuProgramParams.cpp
@@ -634,9 +634,15 @@ namespace Ogre
 				if (mParams->getTransposeMatrices() && (e.dstDefinition->constType == GCT_MATRIX_4X4 ||
                                                         e.dstDefinition->constType == GCT_MATRIX_DOUBLE_4X4))
 				{
-					for (int row = 0; row < 4; ++row)
-						for (int col = 0; col < 4; ++col)
-							pDst[row * 4 + col] = pSrc[col * 4 + row];
+                    // for each matrix that needs to be transposed and copied,
+                    for (size_t iMat = 0; iMat < e.dstDefinition->arraySize; ++iMat)
+                    {
+                        for (int row = 0; row < 4; ++row)
+                            for (int col = 0; col < 4; ++col)
+                                pDst[row * 4 + col] = pSrc[col * 4 + row];
+                        pSrc += 16;
+                        pDst += 16;
+                    }
 				}
 				else
 				{
