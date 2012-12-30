bd50d67a153d1cfafcee72c9c2fa6d9aa71cdc96
David Rogers <masterfalcon@ogre3d.org>
2012-12-30 17:15:45 -0600

    [OGRE-53] Correct FrameListener documentation regarding when frame events are raised.

--- OgreMain/include/OgreFrameListener.h
+++ OgreMain/include/OgreFrameListener.h
@@ -73,11 +73,8 @@ namespace Ogre {
             Root::addFrameListener passing an instance of this class.
             There is no limit to the number of frame listeners you can register,
             allowing you to register multiple listeners for different purposes.
-            Frame events only occur when Ogre is in continuous rendering mode,
-            i.e. after Root::startRendering is called. If the application is
-            doing ad-hoc rendering without entering a rendering loop, frame
-            events are not generated. Note that a frame event occurs once for
-            all rendering targets, not once per target.
+            Note that a frame event occurs once for all rendering targets,
+            not once per target.
     */
     class _OgreExport FrameListener
     {
