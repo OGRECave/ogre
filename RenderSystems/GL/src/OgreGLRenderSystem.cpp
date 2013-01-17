c49d05578b22175be5f4adf0a24b46d658b03071
David Rogers <masterfalcon@ogre3d.org>
2013-01-16 23:25:18 -0600

    [OGRE-129] Fix building on OS X with C++11

--- RenderSystems/GL/src/OgreGLRenderSystem.cpp
+++ RenderSystems/GL/src/OgreGLRenderSystem.cpp
@@ -1048,7 +1048,7 @@ namespace Ogre {
 		if(miscParams)
 		{
 			ss << " miscParams: ";
-			NameValuePairList::const_iterator it;
+			NameValuePairList::const_iterator it(NULL);
 			for(it=miscParams->begin(); it!=miscParams->end(); ++it)
 			{
 				ss << it->first << "=" << it->second << " ";
