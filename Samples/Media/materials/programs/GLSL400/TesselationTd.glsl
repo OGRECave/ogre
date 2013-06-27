c367cf2b6f07985a11722f129b6bc8036e9efc38
Jesse Johnson <holocronweaver@gmail.com>
2013-06-26 22:40:34 -0400

    [GL3+] Minor tessellation sample changes.

--- Samples/Media/materials/programs/GLSL400/TesselationTd.glsl
+++ Samples/Media/materials/programs/GLSL400/TesselationTd.glsl
@@ -9,6 +9,6 @@ layout(triangles, equal_spacing, cw) in;
 void main()
 {
     // Baricentric interpolation
-    vec3 finalPos = (gl_TessCoord.x * gl_in[0].gl_Position) + (gl_TessCoord.y * gl_in[1].gl_Position) + (gl_TessCoord.z * gl_in[2].gl_Position);
+    vec3 finalPos = (gl_TessCoord.x * gl_in[0].gl_Position + gl_TessCoord.y * gl_in[1].gl_Position + gl_TessCoord.z * gl_in[2].gl_Position).xyz;
     gl_Position = g_mWorldViewProjection * vec4(finalPos, 1.0);
 }
