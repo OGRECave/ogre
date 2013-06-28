1c30ea09eba1073bdeaed21fe573e1c0ae9d98cc
Jesse Johnson <holocronweaver@gmail.com>
2013-06-27 22:02:14 -0400

    [GL3+] Cleanup isosurface sample and add debugging mesh.

--- Samples/Media/materials/programs/GLSL150/MetaballFP.glsl
+++ Samples/Media/materials/programs/GLSL150/MetaballFP.glsl
@@ -11,18 +11,19 @@
 // Copyright (c) NVIDIA Corporation. All rights reserved.
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-out vec4 fragColour;
 in vec3 oNormal;
+out vec4 fragColour;
 
 // Pixel shader
 void main()
 {
-	// Sanitize input
-	vec3 N = normalize(oNormal);
-	vec3 L = vec3(0, 0, 1);
+    // Sanitize input
+    vec3 N = normalize(oNormal);
+    vec3 L = vec3(0, 0, 1);
     float nDotL = dot(N, L);
 
     vec3 materials[2] = vec3[](vec3(1, 1, 1), vec3(0, 0, 0.5));
 
-	fragColour = vec4(abs(nDotL) * materials[int(nDotL < 0.0)], 0.1);
+    fragColour = vec4(abs(nDotL) * materials[int(nDotL < 0.0)], 0.1);
+    fragColour = vec4(1, 1, 1, 1);
 }
--- Samples/Media/materials/programs/GLSL150/TessellateTetrahedraGS.glsl
+++ Samples/Media/materials/programs/GLSL150/TessellateTetrahedraGS.glsl
@@ -29,16 +29,22 @@ layout(triangle_strip, max_vertices = 4) out;
 
 // Estimate where isosurface intersects grid edge with endpoints v0, v1
 void CalcIntersection(vec4 Pos0,
-					  vec3 N0,
-					  vec2 Field0,
-					  vec4 Pos1,
-					  vec3 N1,
-					  vec2 Field1)
+                      vec3 N0,
+                      vec2 Field0,
+                      vec4 Pos1,
+                      vec3 N1,
+                      vec2 Field1)
 {
-	float t = (IsoValue - Field0.x) / (Field1.x - Field0.x);
-	gl_Position = mix(Pos0, Pos1, t);
-	oNormal = mix(N0, N1, t);
-    EmitVertex();
+    float t = (IsoValue - Field0.x) / (Field1.x - Field0.x);
+    if ((Field0.x < IsoValue) && (Field1.x > Field0.x))
+    {
+        if (t > 0 && t < 1)
+        {
+            gl_Position = mix(Pos0, Pos1, t);
+            oNormal = mix(N0, N1, t);
+            EmitVertex();
+        }
+    }
 }
 
 // Geometry shader
@@ -46,38 +52,38 @@ void CalcIntersection(vec4 Pos0,
 // outputs: zero, one or two triangles depending if isosurface intersects tetrahedron
 void main()
 {
-	// construct index for this tetrahedron
-	uint index = uint((int(VertexIn[0].Field.y) << 3) |
+    // construct index for this tetrahedron
+    uint index = uint((int(VertexIn[0].Field.y) << 3) |
                       (int(VertexIn[1].Field.y) << 2) |
                       (int(VertexIn[2].Field.y) << 1) |
-                       int(VertexIn[3].Field.y));
-	
-	// don't bother if all vertices out or all vertices inside isosurface
-	if (index > uint(0) && index < uint(15))
-	{
-		// Uber-compressed version of the edge table.
-		uint edgeListHex[8] = 
-			uint[8](uint(0x0001cde0), uint(0x98b08c9d), uint(0x674046ce), uint(0x487bc480), 
+                      int(VertexIn[3].Field.y));
+        
+    // don't bother if all vertices out or all vertices inside isosurface
+    if (index > uint(0) && index < uint(15))
+    {
+        // Uber-compressed version of the edge table.
+        uint edgeListHex[8] = 
+            uint[8](uint(0x0001cde0), uint(0x98b08c9d), uint(0x674046ce), uint(0x487bc480), 
                     uint(0x21301d2e), uint(0x139bd910), uint(0x26376e20), uint(0x3b700000));
 
-		uint edgeValFull = edgeListHex[index/uint(2)];
-		uint three = uint(0x3);
-		uint edgeVal = (index % uint(2) == uint(1)) ? (edgeValFull & uint(0xFFFF)) : ((edgeValFull >> 16) & uint(0xFFFF));
-		ivec4 e0 = ivec4((edgeVal >> 14) & three, (edgeVal >> 12) & three, (edgeVal >> 10) & three, (edgeVal >> 8) & three);
-		ivec4 e1 = ivec4((edgeVal >> 6) & three, (edgeVal >> 4) & three, (edgeVal >> 2) & three, (edgeVal >> 0) & three);
+        uint edgeValFull = edgeListHex[index/uint(2)];
+        uint three = uint(0x3);
+        uint edgeVal = (index % uint(2) == uint(1)) ? (edgeValFull & uint(0xFFFF)) : ((edgeValFull >> 16) & uint(0xFFFF));
+        ivec4 e0 = ivec4((edgeVal >> 14) & three, (edgeVal >> 12) & three, (edgeVal >> 10) & three, (edgeVal >> 8) & three);
+        ivec4 e1 = ivec4((edgeVal >> 6) & three, (edgeVal >> 4) & three, (edgeVal >> 2) & three, (edgeVal >> 0) & three);
 
-		CalcIntersection(gl_in[e0.x].gl_Position, VertexIn[e0.x].N, VertexIn[e0.x].Field,
-		                 gl_in[e0.y].gl_Position, VertexIn[e0.y].N, VertexIn[e0.y].Field);
-		CalcIntersection(gl_in[e0.z].gl_Position, VertexIn[e0.z].N, VertexIn[e0.z].Field,
-		                 gl_in[e0.w].gl_Position, VertexIn[e0.w].N, VertexIn[e0.w].Field);
-		CalcIntersection(gl_in[e1.x].gl_Position, VertexIn[e1.x].N, VertexIn[e1.x].Field,
-		                 gl_in[e1.y].gl_Position, VertexIn[e1.y].N, VertexIn[e1.y].Field);
+        CalcIntersection(gl_in[e0.x].gl_Position, VertexIn[e0.x].N, VertexIn[e0.x].Field,
+                         gl_in[e0.y].gl_Position, VertexIn[e0.y].N, VertexIn[e0.y].Field);
+        CalcIntersection(gl_in[e0.z].gl_Position, VertexIn[e0.z].N, VertexIn[e0.z].Field,
+                         gl_in[e0.w].gl_Position, VertexIn[e0.w].N, VertexIn[e0.w].Field);
+        CalcIntersection(gl_in[e1.x].gl_Position, VertexIn[e1.x].N, VertexIn[e1.x].Field,
+                         gl_in[e1.y].gl_Position, VertexIn[e1.y].N, VertexIn[e1.y].Field);
 
-		// Emit additional triangle, if necessary
-		if (e1.z != -1) {
-			CalcIntersection(gl_in[e1.z].gl_Position, VertexIn[e1.z].N, VertexIn[e1.z].Field,
-			                 gl_in[e1.w].gl_Position, VertexIn[e1.w].N, VertexIn[e1.w].Field);
-		}
+        // Emit additional triangle, if necessary
+        if (e1.z != -1) {
+            CalcIntersection(gl_in[e1.z].gl_Position, VertexIn[e1.z].N, VertexIn[e1.z].Field,
+                             gl_in[e1.w].gl_Position, VertexIn[e1.w].N, VertexIn[e1.w].Field);
+        }
         EndPrimitive();
-	}
+    }
 }
