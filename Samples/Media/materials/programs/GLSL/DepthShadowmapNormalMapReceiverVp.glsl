9e3fd63dd2995d188dce9c19a049507986524fd4
Murat Sari <wolfmanfx@gmail.com>
2013-07-29 07:20:14 +0200

    * Work on shadow sample. HLSL shadow shader works again

--- Samples/Media/materials/programs/GLSL/DepthShadowmapNormalMapReceiverVp.glsl
+++ Samples/Media/materials/programs/GLSL/DepthShadowmapNormalMapReceiverVp.glsl
@@ -4,7 +4,6 @@ uniform mat4 world;
 uniform mat4 worldViewProj;
 uniform mat4 texViewProj;
 uniform vec4 lightPosition; // object space
-uniform vec4 shadowDepthRange;
 
 varying vec3 tangentLightDir;
 
@@ -20,10 +19,6 @@ void main()
 
 	// calculate shadow map coords
 	gl_TexCoord[0] = texViewProj * worldPos;
-#if LINEAR_RANGE
-	// adjust by fixed depth bias, rescale into range
-	gl_TexCoord[0].z = (gl_TexCoord[0].z - shadowDepthRange.x) * shadowDepthRange.w;
-#endif
 
 	// pass the main uvs straight through unchanged 
 	gl_TexCoord[1] = gl_MultiTexCoord0;
@@ -37,6 +32,5 @@ void main()
     
 	// Transform the light vector according to this matrix 
 	tangentLightDir = normalize(rotation * lightDir); 
-
 }
 
