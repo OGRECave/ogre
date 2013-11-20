2fa1f77139698fbef79a19263321f48654ea40f3
Jesse Johnson <holocronweaver@gmail.com>
2013-11-20 18:46:34 -0500

    [vtests] Fix SerialisedColour test

--- Samples/Media/materials/programs/GLSL150/PassthroughVP.glsl
+++ Samples/Media/materials/programs/GLSL150/PassthroughVP.glsl
@@ -2,12 +2,8 @@
 
 in vec4 vertex;
 
-// uniform mat4 WorldViewProj;
-
 void main()
 {
     // Transform the vertex (ModelViewProj matrix).
-    // gl_Position = ftransform();
-    // gl_Position = WorldViewProj * vertex;
     gl_Position = vertex;
 }
