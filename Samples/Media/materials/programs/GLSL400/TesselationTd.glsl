e6ffbfd741dbc5e04acc643ad19ec5ce3e60cfb6
Jesse Johnson <holocronweaver@gmail.com>
2013-11-22 16:38:00 -0500

    [samples] Fix implicit vec 3 to vec4 conversion in Tessellation sample GLSL shader

--- Samples/Media/materials/programs/GLSL400/TesselationTd.glsl
+++ Samples/Media/materials/programs/GLSL400/TesselationTd.glsl
@@ -7,6 +7,8 @@ layout(triangles, equal_spacing, cw) in;
 void main()
 {
     // Baricentric interpolation
-    vec3 finalPos = (gl_TessCoord.x * gl_in[0].gl_Position + gl_TessCoord.y * gl_in[1].gl_Position + gl_TessCoord.z * gl_in[2].gl_Position).xyz;
+    vec3 finalPos = vec3(gl_TessCoord.x * gl_in[0].gl_Position + 
+                         gl_TessCoord.y * gl_in[1].gl_Position + 
+                         gl_TessCoord.z * gl_in[2].gl_Position);
     gl_Position = g_mWorldViewProjection * vec4(finalPos, 1.0);
 }
