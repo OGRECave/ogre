06175ac96f253a2672a164d9ceb29c274f77402f
dark_sylinc <unknown>
2013-11-23 15:07:12 -0300

    Fixed calling convention warnings on x64 builds.

--- OgreMain/include/Threading/OgreThreads.h
+++ OgreMain/include/Threading/OgreThreads.h
@@ -31,18 +31,18 @@ THE SOFTWARE.
 
 #include "OgreSharedPtr.h"
 
-#if OGRE_CPU == OGRE_CPU_ARM
-    // Don't need calling conventions for ARM CPU's
-    #define OGRE_THREAD_CALL_CONVENTION
-#else
+#if defined(__i386) || defined(_M_IX86)
+	// Calling conventions are needed for x86 (32-bit ONLY) CPUs
     #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
         #define OGRE_THREAD_CALL_CONVENTION __stdcall
-    #else
+	#elif
         #if OGRE_COMPILER == OGRE_COMPILER_GNUC || OGRE_COMPILER == OGRE_COMPILER_CLANG
             #define __cdecl __attribute__((__cdecl__))
         #endif
         #define OGRE_THREAD_CALL_CONVENTION __cdecl
     #endif
+#else
+	#define OGRE_THREAD_CALL_CONVENTION
 #endif
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
