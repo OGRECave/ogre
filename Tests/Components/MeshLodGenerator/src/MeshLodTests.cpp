01315612efa9aecbe6714740f81b0158cb934957
David Rogers <masterfalcon@ogre3d.org>
2013-12-01 22:35:59 -0600

    Fix up new MeshLodTests, make sure a render system is loaded before initializing root.

--- Tests/Components/MeshLodGenerator/src/MeshLodTests.cpp
+++ Tests/Components/MeshLodGenerator/src/MeshLodTests.cpp
@@ -28,11 +28,11 @@ void MeshLodTests::setUp()
 {
 	OGRE_DELETE LogManager::getSingletonPtr();
 	mLogManager = OGRE_NEW LogManager();
-	mLogManager->createLog("MeshWithoutIndexDataTests.log", false);
+	mLogManager->createLog("MeshLodTests.log", false);
 	mLogManager->setLogDetail(LL_LOW);
 
 #if OGRE_STATIC
-        mStaticPluginLoader = OGRE_NEW StaticPluginLoader();
+    mStaticPluginLoader = OGRE_NEW StaticPluginLoader();
 #endif
 
 #ifdef OGRE_STATIC_LIB
@@ -41,7 +41,17 @@ void MeshLodTests::setUp()
 	mStaticPluginLoader.load();
 #else
 	Root* root = OGRE_NEW Root();
-	
+
+    // Try to load a Rendersystem
+    try
+	{
+        root->loadPlugin("RenderSystem_GL");
+	}
+	catch(std::exception&)
+    {
+        root->loadPlugin("RenderSystem_Direct3D9");
+    }
+
 #endif
 	CPPUNIT_ASSERT(!root->getAvailableRenderers().empty());
 	root->setRenderSystem(root->getAvailableRenderers().back());
