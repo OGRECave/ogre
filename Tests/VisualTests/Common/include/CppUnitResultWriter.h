d8a2238ed2e46ecef1680d65fde4bc0ae6975b57
David Rogers <masterfalcon@ogre3d.org>
2013-12-09 22:18:03 -0600

    Make the Cppunit result writer start counting test id’s at 10000.

--- Tests/VisualTests/Common/include/CppUnitResultWriter.h
+++ Tests/VisualTests/Common/include/CppUnitResultWriter.h
@@ -66,7 +66,8 @@ protected:
                 {
                     numFailed++;
 
-                    out << "    <FailedTest id=\"" << i << "\">" << std::endl;
+                    // Start counting at 10000 to make sure that they are unique test id's.
+                    out << "    <FailedTest id=\"" << i + 10000 << "\">" << std::endl;
                     out << "      <Name>" << test << "</Name>" << std::endl;
                     out << "      <FailureType>Error</FailureType>" << std::endl;
                     out << "      <Message>Visual Test Image Mismatch</Message>" << std::endl;
