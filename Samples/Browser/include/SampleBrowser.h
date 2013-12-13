913084044d986af525cdcc64c89c783ba8b59ce3
Jesse Johnson <holocronweaver@gmail.com>
2013-12-13 00:07:01 -0500

    [vtests] Allow vtests to link successfully with mesh LOD when CppUnit is absent

--- Samples/Browser/include/SampleBrowser.h
+++ Samples/Browser/include/SampleBrowser.h
@@ -114,7 +114,7 @@ typedef std::map<String, OgreBites::SdkSample *> PluginMap;
 #ifdef INCLUDE_RTSHADER_SYSTEM
 // Remove the comment below in order to make the RTSS use valid path for writing down the generated shaders.
 // If cache path is not set - all shaders are generated to system memory.
-//#define _RTSS_WRITE_SHADERS_TO_DISK
+#define _RTSS_WRITE_SHADERS_TO_DISK
 #endif // INCLUDE_RTSHADER_SYSTEM   
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_APPLE_IOS
