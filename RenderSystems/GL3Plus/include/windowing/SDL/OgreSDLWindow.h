03b3f643098ad1f6c29fdcce4ba691e1c3838455
dark_sylinc <dark_sylinc@yahoo.com.ar>
2013-12-18 02:35:20 -0300

    [GL3+] For real move the windowing code to its own folder (stripped Jabberwocky's files)

---
+++ RenderSystems/GL3Plus/include/windowing/SDL/OgreSDLWindow.h
@@ -0,0 +1,94 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2009 Torus Knot Software Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+
+#ifndef __SDLWindow_H__
+#define __SDLWindow_H__
+
+#include "OgreSDLPrerequisites.h"
+#include "OgreRenderWindow.h"
+
+namespace Ogre {
+    class _OgrePrivate SDLWindow : public RenderWindow
+    {
+    private:
+        SDL_Surface* mScreen;
+        bool mActive;
+        bool mClosed;
+        bool mVSync;
+
+        // Process pending events
+        void processEvents(void);
+
+        int (*glXGetVideoSyncSGI) (unsigned int *);
+        int (*glXWaitVideoSyncSGI) (int, int, unsigned int *);
+
+    public:
+        SDLWindow();
+        ~SDLWindow();
+
+		void create(const String& name, unsigned int width, unsigned int height,
+	            bool fullScreen, const NameValuePairList *miscParams);
+        /** Overridden - see RenderWindow */
+        void destroy(void);
+        /** Overridden - see RenderWindow */
+        bool isActive(void) const;
+        /** Overridden - see RenderWindow */
+        bool isClosed(void) const;
+        /** Overridden - see RenderWindow */
+        void reposition(int left, int top);
+        /** Overridden - see RenderWindow */
+        void resize(unsigned int width, unsigned int height);
+        /** Overridden - see RenderWindow */
+        void swapBuffers();
+
+        /** Overridden - see RenderTarget. */
+        void copyContentsToMemory(const PixelBox &dst, FrameBuffer buffer);
+
+        /** @copydoc see RenderWindow::setVSyncEnabled */
+        void setVSyncEnabled(bool vsync);
+        /** @copydoc see RenderWindow::isVSyncEnabled */
+        bool isVSyncEnabled() const;
+
+        /** Overridden - see RenderTarget.
+        */
+        void getCustomAttribute( const String& name, void* pData )
+        {
+            // NOOP
+        }
+
+        bool requiresTextureFlipping() const { return false; }
+
+        bool isFullScreen(void) const
+        {
+            return ( mScreen->flags & SDL_FULLSCREEN ) == SDL_FULLSCREEN;
+        }
+    };
+}
+
+#endif
+
---
+++ RenderSystems/GL3Plus/src/windowing/SDL/OgreSDLGLSupport.cpp
@@ -0,0 +1,173 @@
+#include "OgreException.h"
+#include "OgreLogManager.h"
+#include "OgreStringConverter.h"
+
+#include "OgreSDLGLSupport.h"
+
+#include "OgreSDLWindow.h"
+
+using namespace Ogre;
+
+SDLGLSupport::SDLGLSupport()
+{
+
+    SDL_Init(SDL_INIT_VIDEO);
+}
+
+SDLGLSupport::~SDLGLSupport()
+{
+}
+
+void SDLGLSupport::addConfig(void)
+{
+    mVideoModes = SDL_ListModes(NULL, SDL_FULLSCREEN | SDL_OPENGL);
+    
+    if (mVideoModes == (SDL_Rect **)0)
+    {
+        OGRE_EXCEPT(Exception::ERR_RENDERINGAPI_ERROR, "Unable to load video modes",
+                "SDLRenderSystem::initConfigOptions");
+    }
+
+    ConfigOption optFullScreen;
+    ConfigOption optVideoMode;
+    ConfigOption optFSAA;
+	ConfigOption optRTTMode;
+	ConfigOption optEnableFixedPipeline;
+
+    // FS setting possibilities
+    optFullScreen.name = "Full Screen";
+    optFullScreen.possibleValues.push_back("Yes");
+    optFullScreen.possibleValues.push_back("No");
+    optFullScreen.currentValue = "Yes";
+    optFullScreen.immutable = false;
+
+    // Video mode possibilities
+    optVideoMode.name = "Video Mode";
+    optVideoMode.immutable = false;
+    for (size_t i = 0; mVideoModes[i]; i++)
+    {
+        char szBuf[16];
+		snprintf(szBuf, 16, "%d x %d", mVideoModes[i]->w, mVideoModes[i]->h);
+        optVideoMode.possibleValues.push_back(szBuf);
+        // Make the first one default
+        if (i == 0)
+        {
+            optVideoMode.currentValue = szBuf;
+        }
+    }
+    
+    //FSAA possibilities
+    optFSAA.name = "FSAA";
+    optFSAA.possibleValues.push_back("0");
+    optFSAA.possibleValues.push_back("2");
+    optFSAA.possibleValues.push_back("4");
+    optFSAA.possibleValues.push_back("6");
+    optFSAA.currentValue = "0";
+    optFSAA.immutable = false;
+
+	optRTTMode.name = "RTT Preferred Mode";
+	optRTTMode.possibleValues.push_back("FBO");
+	optRTTMode.possibleValues.push_back("PBuffer");
+	optRTTMode.possibleValues.push_back("Copy");
+	optRTTMode.currentValue = "FBO";
+	optRTTMode.immutable = false;
+
+		optEnableFixedPipeline.name = "Fixed Pipeline Enabled";
+		optEnableFixedPipeline.possibleValues.push_back( "Yes" );
+		optEnableFixedPipeline.possibleValues.push_back( "No" );
+		optEnableFixedPipeline.currentValue = "Yes";
+		optEnableFixedPipeline.immutable = false;
+
+    mOptions[optFullScreen.name] = optFullScreen;
+    mOptions[optVideoMode.name] = optVideoMode;
+    mOptions[optFSAA.name] = optFSAA;
+	mOptions[optRTTMode.name] = optRTTMode;
+		mOptions[optEnableFixedPipeline.name] = optEnableFixedPipeline;
+
+}
+
+String SDLGLSupport::validateConfig(void)
+{
+    return String("");
+}
+
+RenderWindow* SDLGLSupport::createWindow(bool autoCreateWindow, GL3PlusRenderSystem* renderSystem, const String& windowTitle)
+{
+	if (autoCreateWindow)
+    {
+        ConfigOptionMap::iterator opt = mOptions.find("Full Screen");
+        if (opt == mOptions.end())
+            OGRE_EXCEPT(Exception::ERR_RENDERINGAPI_ERROR, "Can't find full screen options!", "SDLGLSupport::createWindow");
+        bool fullscreen = (opt->second.currentValue == "Yes");
+
+        opt = mOptions.find("Video Mode");
+        if (opt == mOptions.end())
+            OGRE_EXCEPT(Exception::ERR_RENDERINGAPI_ERROR, "Can't find video mode options!", "SDLGLSupport::createWindow");
+        String val = opt->second.currentValue;
+        String::size_type pos = val.find('x');
+        if (pos == String::npos)
+            OGRE_EXCEPT(Exception::ERR_RENDERINGAPI_ERROR, "Invalid Video Mode provided", "SDLGLSupport::createWindow");
+
+		// Parse FSAA config
+		NameValuePairList winOptions;
+		winOptions["title"] = windowTitle;
+        int fsaa_x_samples = 0;
+        opt = mOptions.find("FSAA");
+        if(opt != mOptions.end())
+        {
+			winOptions["FSAA"] = opt->second.currentValue;
+        }
+
+			opt = mOptions.find("Fixed Pipeline Enabled");
+			if (opt == mOptions.end())
+				OGRE_EXCEPT(Exception::ERR_INVALIDPARAMS, "Can't find Fixed Pipeline enabled options!", "Win32GLSupport::createWindow");
+			bool enableFixedPipeline = (opt->second.currentValue == "Yes");
+			renderSystem->setFixedPipelineEnabled(enableFixedPipeline);
+
+        unsigned int w = StringConverter::parseUnsignedInt(val.substr(0, pos));
+        unsigned int h = StringConverter::parseUnsignedInt(val.substr(pos + 1));
+
+        const SDL_VideoInfo* videoInfo = SDL_GetVideoInfo();
+        return renderSystem->createRenderWindow(windowTitle, w, h, fullscreen, &winOptions);
+    }
+    else
+    {
+        // XXX What is the else?
+		return NULL;
+    }
+
+}
+
+
+RenderWindow* SDLGLSupport::newWindow(const String &name, unsigned int width, unsigned int height, 
+	bool fullScreen, const NameValuePairList *miscParams)
+{
+	SDLWindow* window = new SDLWindow();
+	window->create(name, width, height, fullScreen, miscParams);
+	return window;
+}
+
+void SDLGLSupport::start()
+{
+    LogManager::getSingleton().logMessage(
+        "******************************\n"
+        "*** Starting SDL Subsystem ***\n"
+        "******************************");
+
+    SDL_Init(SDL_INIT_VIDEO);
+}
+
+void SDLGLSupport::stop()
+{
+    LogManager::getSingleton().logMessage(
+        "******************************\n"
+        "*** Stopping SDL Subsystem ***\n"
+        "******************************");
+
+    SDL_Quit();
+}
+
+void* SDLGLSupport::getProcAddress(const String& procname)
+{
+    return SDL_GL_GetProcAddress(procname.c_str());
+}
