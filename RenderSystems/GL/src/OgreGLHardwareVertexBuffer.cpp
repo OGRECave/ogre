a323cba94073c1f8194bbefdac93ed14c6736240
Murat Sari <wolfmanfx@gmail.com>
2013-12-18 20:34:07 +0100

    Recover GLHardwareVertexBuffer if glBufferDataARB fails (happens on OSX Mavericks)

--- RenderSystems/GL/src/OgreGLHardwareVertexBuffer.cpp
+++ RenderSystems/GL/src/OgreGLHardwareVertexBuffer.cpp
@@ -109,7 +109,29 @@ namespace Ogre {
 				// Discard the buffer
 				glBufferDataARB(GL_ARRAY_BUFFER_ARB, mSizeInBytes, NULL, 
 					GLHardwareBufferManager::getGLUsage(mUsage));
+                
+                GLenum error = glGetError();
+                if(error != 0)
+                {
+                    String glErrDesc;
+                    const char* glerrStr = (const char*)gluErrorString(error);
+                    if (glerrStr)
+                        glErrDesc = glerrStr;
+                    
+                    LogManager::getSingleton().logMessage("GLHardwareVertexBuffer::lock - Error: failed to Discard the buffer. Try to recreate the buffer", LML_CRITICAL);
+                    
+                    static_cast<GLHardwareBufferManagerBase*>(mMgr)->getStateCacheManager()->deleteGLBuffer(GL_ARRAY_BUFFER_ARB, mBufferId);
+                    mBufferId = 0;
+                    
+                    glGenBuffersARB( 1, &mBufferId );
+                    
+                    static_cast<GLHardwareBufferManagerBase*>(mMgr)->getStateCacheManager()->bindGLBuffer(GL_ARRAY_BUFFER_ARB, mBufferId);
+                    
+                    glBufferDataARB(GL_ARRAY_BUFFER_ARB, mSizeInBytes, NULL,
+                                    GLHardwareBufferManager::getGLUsage(mUsage));
+                }
 			}
+            
 			if (mUsage & HBU_WRITE_ONLY)
 				access = GL_WRITE_ONLY_ARB;
 			else if (options == HBL_READ_ONLY)
