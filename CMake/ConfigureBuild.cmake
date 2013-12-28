b51c94bb14ea8c6edf3b195bd520488d5840a9e3
Joel Croteau <jcroteau@gmail.com>
2013-12-27 23:43:54 -0800

    Fix spelling error in CMake file.

--- CMake/ConfigureBuild.cmake
+++ CMake/ConfigureBuild.cmake
@@ -276,7 +276,7 @@ if (UNIX)
 endif ()
 
 if(OGRE_CONFIG_STATIC_LINK_CRT)
-#We statically link to reduce dependancies
+#We statically link to reduce dependencies
 foreach(flag_var CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
     if(${flag_var} MATCHES "/MD")
         string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
