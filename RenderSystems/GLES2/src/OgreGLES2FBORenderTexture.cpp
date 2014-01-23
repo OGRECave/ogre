7dd5ce0db00ba82d9d0bd4f97c694e8bab7d2fd0
Assaf Raman <assaf@assaf.co.il>
2014-01-23 11:00:11 +0200

    GLES2 render system - change a static buffer to a dynamic one - as you can't alloc a static buffer with a dynamic size.

--- RenderSystems/GLES2/src/OgreGLES2FBORenderTexture.cpp
+++ RenderSystems/GLES2/src/OgreGLES2FBORenderTexture.cpp
@@ -198,10 +198,11 @@ namespace Ogre {
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 #if OGRE_NO_GLES3_SUPPORT == 0
-            unsigned char data[PROBE_SIZE * PROBE_SIZE * PixelUtil::getNumElemBytes(pixFmt)];
+            unsigned char * data = new unsigned char[PROBE_SIZE * PROBE_SIZE * PixelUtil::getNumElemBytes(pixFmt)];
 
             glTexStorage2D(GL_TEXTURE_2D, 1, internalFormat, PROBE_SIZE, PROBE_SIZE);
             glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, PROBE_SIZE, PROBE_SIZE, fmt, GL_UNSIGNED_BYTE, data);
+			delete [] data;
 #else
             glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, PROBE_SIZE, PROBE_SIZE, 0, fmt, type, 0);
 #endif
