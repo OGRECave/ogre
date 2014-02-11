8b0ddbf1010d195ea817dc1497f71e9e2508631b
Murat Sari <wolfmanfx@gmail.com>
2014-02-11 14:51:46 +0100

    Patch: Bug inside Math::intersects(ray,AABB)

--- OgreMain/src/OgreMath.cpp
+++ OgreMain/src/OgreMath.cpp
@@ -532,7 +532,7 @@ namespace Ogre
                 hitpoint = rayorig + raydir * t;
                 if (hitpoint.y >= min.y && hitpoint.y <= max.y &&
                     hitpoint.z >= min.z && hitpoint.z <= max.z &&
-                    t < lowt)
+					(!hit || t < lowt))
                 {
                     hit = true;
                     lowt = t;
