36597dba32cb79bd0075e70fd530c33b8337e285
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2014-03-23 02:31:08 -0300

    Moved ShadowsV2 sample from "Showcase" to "API Usage" category, as it is much more appropiate (it's very simple, and lacks enough GUI interactivity)

---
+++ Samples/ApiUsage/Lights/ShadowsV2/src/ShadowsV2Plugin.cpp
@@ -0,0 +1,47 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2014 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+You may use this sample code for anything you like, it is not covered by the
+same license as the rest of the engine.
+-----------------------------------------------------------------------------
+*/
+
+/**
+    @file 
+        Shadows.cpp
+    @brief
+        Shows a few ways to use Ogre's shadowing techniques
+*/
+
+#include "SamplePlugin.h"
+#include "ShadowsV2.h"
+
+using namespace Ogre;
+using namespace OgreBites;
+
+#ifndef OGRE_STATIC_LIB
+
+SamplePlugin* sp;
+Sample* s;
+
+extern "C" _OgreSampleExport void dllStartPlugin()
+{
+    s = new Sample_ShadowsV2;
+    sp = OGRE_NEW SamplePlugin(s->getInfo()["Title"] + " Sample");
+    sp->addSample(s);
+    Root::getSingleton().installPlugin(sp);
+}
+
+extern "C" _OgreSampleExport void dllStopPlugin()
+{
+    Root::getSingleton().uninstallPlugin(sp); 
+    OGRE_DELETE sp;
+    delete s;
+}
+#endif
