3d387824fe6f3694f2d4687fb54df7346d5df87d
Eugene Golushkov <eugene@belightsoft.com>
2014-09-02 15:32:43 +0300

    [D3D11] Fixed adaptive_tessellation.hlsl: "error X4576: Input signature parameter SV_Position (1-based Entry 0) type must be float32 and mask must be xyzw."

--- Samples/Media/materials/programs/HLSL/adaptive_tessellation.hlsl
+++ Samples/Media/materials/programs/HLSL/adaptive_tessellation.hlsl
@@ -17,7 +17,7 @@ cbuffer TessellationBuffer
 //////////////
 struct VertexInputType2
 {
-    float3 position 		: POSITION;
+    float4 position 		: POSITION;
     float2 texCoord     	: TEXCOORD0;
     float4 color 			: COLOR;
 };
@@ -25,7 +25,7 @@ struct VertexInputType2
 
 struct HullInputType
 {
-    float3 position : POSITION;
+    float4 position : POSITION;
     float2 texCoord : TEXCOORD0;
     float4 color 	: COLOR;
 };
@@ -42,7 +42,7 @@ struct ConstantOutputType
 
 struct HullOutputType
 {
-    float3 position : POSITION;
+    float4 position : POSITION;
     float2 texCoord : TEXCOORD0;
     float4 color 	: COLOR;
 };
