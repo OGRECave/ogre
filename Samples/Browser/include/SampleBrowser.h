780b631d098c8604e27f36fe7c8d72bd8a7f4e01
Pavel Rojtberg <rojtberg@gmail.com>
2014-09-03 18:55:50 +0200

    improve readability of RTSS Shader Generation code in SampleBrowser

--- Samples/Browser/include/SampleBrowser.h
+++ Samples/Browser/include/SampleBrowser.h
@@ -167,42 +167,41 @@ namespace OgreBites
                                                       const Ogre::String& schemeName, Ogre::Material* originalMaterial, unsigned short lodIndex,
                                                       const Ogre::Renderable* rend)
         {
-            Ogre::Technique* generatedTech = NULL;
-
-            // Case this is the default shader generator scheme.
-            if (schemeName == Ogre::RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME)
+            if (schemeName != Ogre::RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME)
             {
-                bool techniqueCreated;
+                return NULL;
+            }
+            // Case this is the default shader generator scheme.
 
-                // Create shader generated technique for this material.
-                techniqueCreated = mShaderGenerator->createShaderBasedTechnique(
-                    originalMaterial->getName(),
-                    Ogre::MaterialManager::DEFAULT_SCHEME_NAME,
-                    schemeName);
+            // Create shader generated technique for this material.
+            bool techniqueCreated = mShaderGenerator->createShaderBasedTechnique(
+                originalMaterial->getName(),
+                Ogre::MaterialManager::DEFAULT_SCHEME_NAME,
+                schemeName);
 
-                // Case technique registration succeeded.
-                if (techniqueCreated)
+            if (!techniqueCreated)
+            {
+                return NULL;
+            }
+            // Case technique registration succeeded.
+
+            // Force creating the shaders for the generated technique.
+            mShaderGenerator->validateMaterial(schemeName, originalMaterial->getName());
+
+            // Grab the generated technique.
+            Ogre::Material::TechniqueIterator itTech = originalMaterial->getTechniqueIterator();
+
+            while (itTech.hasMoreElements())
+            {
+                Ogre::Technique* curTech = itTech.getNext();
+
+                if (curTech->getSchemeName() == schemeName)
                 {
-                    // Force creating the shaders for the generated technique.
-                    mShaderGenerator->validateMaterial(schemeName, originalMaterial->getName());
-
-                    // Grab the generated technique.
-                    Ogre::Material::TechniqueIterator itTech = originalMaterial->getTechniqueIterator();
-
-                    while (itTech.hasMoreElements())
-                    {
-                        Ogre::Technique* curTech = itTech.getNext();
-
-                        if (curTech->getSchemeName() == schemeName)
-                        {
-                            generatedTech = curTech;
-                            break;
-                        }
-                    }
+                    return curTech;
                 }
             }
 
-            return generatedTech;
+            return NULL;
         }
 
     protected:
