82c34b5aaef8ba29e9ae6773aefd4516671206e1
tiggilyboo <simonwillshire@gmail.com>
2014-10-07 19:00:14 +0000

    OgreTerrainPagedWorldSection.cpp edited online with Bitbucket - Fixed formatting

--- Components/Terrain/src/OgreTerrainPagedWorldSection.cpp
+++ Components/Terrain/src/OgreTerrainPagedWorldSection.cpp
@@ -309,19 +309,19 @@ namespace Ogre
     //---------------------------------------------------------------------
     void TerrainPagedWorldSection::handleResponse(const WorkQueue::Response* res, const WorkQueue* srcQ)
     {
-		if(!mPagesInLoading.empty())
+        if(!mPagesInLoading.empty())
         {
-			PageID pageID = mPagesInLoading.front();
+            PageID pageID = mPagesInLoading.front();
 
-			// trigger terrain load
-			long x, y;
-			// pageID is the same as a packed index
-			mTerrainGroup->unpackIndex(pageID, &x, &y);
-			mTerrainGroup->loadTerrain(x, y, false);
-			mPagesInLoading.pop_front();
+            // trigger terrain load
+            long x, y;
+            // pageID is the same as a packed index
+            mTerrainGroup->unpackIndex(pageID, &x, &y);
+            mTerrainGroup->loadTerrain(x, y, false);
+            mPagesInLoading.pop_front();
 
-			unsigned long currentTime = Root::getSingletonPtr()->getTimer()->getMilliseconds();
-			mNextLoadingTime = currentTime + mLoadingIntervalMs;
+            unsigned long currentTime = Root::getSingletonPtr()->getTimer()->getMilliseconds();
+            mNextLoadingTime = currentTime + mLoadingIntervalMs;
 
             // Continue loading other pages
             Root::getSingleton().getWorkQueue()->addRequest(
