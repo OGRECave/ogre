500ea0989b0587c86ae900342436e98d8f9f16d5
Jannik Heller <scrawl@baseoftrash.de>
2014-10-13 16:26:17 +0200

    Disable restrict qualifier on return types for GCC only

--- OgreMain/include/OgreRawPtr.h
+++ OgreMain/include/OgreRawPtr.h
@@ -114,8 +114,8 @@ namespace Ogre
             }
         }
 
-        T * get()                               { return mPtr; }
-        const T * get() const                   { return mPtr; }
+        T * RESTRICT_ALIAS_RETURN get()                               { return mPtr; }
+        const T * RESTRICT_ALIAS_RETURN get() const                   { return mPtr; }
         size_t size() const                     { return mNumElements; }
     };
 }
