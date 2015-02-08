5f9194f375aca08db0a442262477eaf94a640fcb
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2015-02-08 16:49:08 -0300

    Fixed compile error when compiling with anything other than MSVC 2008

--- OgreMain/include/Hash/MurmurHash3.h
+++ OgreMain/include/Hash/MurmurHash3.h
@@ -30,11 +30,11 @@ namespace Ogre
 
 namespace Ogre
 {
-    void _OgreExport MurmurHash3_x86_32  ( const void * key, int len, Ogre::uint32_t seed, void * out );
+    void _OgreExport MurmurHash3_x86_32  ( const void * key, int len, uint32_t seed, void * out );
 
-    void _OgreExport MurmurHash3_x86_128 ( const void * key, int len, Ogre::uint32_t seed, void * out );
+    void _OgreExport MurmurHash3_x86_128 ( const void * key, int len, uint32_t seed, void * out );
 
-    void _OgreExport MurmurHash3_x64_128 ( const void * key, int len, Ogre::uint32_t seed, void * out );
+    void _OgreExport MurmurHash3_x64_128 ( const void * key, int len, uint32_t seed, void * out );
 }
 
 //-----------------------------------------------------------------------------
