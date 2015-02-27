964b2d9b29b4f325a4a176e4d5b311ce9317f255
Pavel Rojtberg <rojtberg@gmail.com>
2015-02-27 12:31:20 +0100

    focus Compute example on compute shaders and make it compute something useful

--- Samples/Compute/src/Compute.cpp
+++ Samples/Compute/src/Compute.cpp
@@ -31,42 +31,24 @@ class _OgreSampleClassExport Sample_Compute : public SdkSample
 
  public:
         
-    Sample_Compute() 
+    Sample_Compute() : mOgreEnt(NULL)
     { 
         mInfo["Title"] = "Compute";
         mInfo["Description"] = "A basic example of the compute shader.";
         mInfo["Thumbnail"] = "thumb_compute.png";
         mInfo["Category"] = "Tests";
+        mInfo["Help"] = "The shader is executed in groups of 16x16 workers\n"
+                "in total there are 16x16 groups forming a grid of 256x256\n"
+                "each worker writes the color based on the local id\n"
+                "the sine overlay is based on the global id";
     }
 
     void testCapabilities(const RenderSystemCapabilities* caps)
     {
-        OGRE_EXCEPT(Exception::ERR_NOT_IMPLEMENTED, 
-                    "Sample currently under construction.  Try again soon!",
-                    "Sample_Compute::testCapabilities");
-
-        if (!caps->hasCapability(RSC_ATOMIC_COUNTERS))
-        {
-            OGRE_EXCEPT(Exception::ERR_NOT_IMPLEMENTED, "Your render system / hardware does not support atomic counters, "
-                        "so you cannot run this sample. Sorry!",
-                        "Sample_Compute::testCapabilities");
-        }
-        else if (!caps->hasCapability(RSC_COMPUTE_PROGRAM))
+        if (!caps->hasCapability(RSC_COMPUTE_PROGRAM))
         {
             OGRE_EXCEPT(Exception::ERR_NOT_IMPLEMENTED, "Your render system / hardware does not support compute programs, "
                         "so you cannot run this sample. Sorry!",
-                        "Sample_Compute::testCapabilities");
-        }
-        else if (!caps->hasCapability(RSC_TESSELLATION_HULL_PROGRAM) || !caps->hasCapability(RSC_TESSELLATION_DOMAIN_PROGRAM))
-        {
-            OGRE_EXCEPT(Exception::ERR_NOT_IMPLEMENTED, "Your render system / hardware does not support tesselation programs, "
-                        "so you cannot run this sample. Sorry!",
-                        "Sample_Compute::testCapabilities");
-        }
-        else if (!caps->hasCapability(RSC_GEOMETRY_PROGRAM))
-        {
-            OGRE_EXCEPT(Exception::ERR_NOT_IMPLEMENTED, "Your render system / hardware does not support geometry programs, "
-                        "so you cannot run this sample. Sorry!", 
                         "Sample_Compute::testCapabilities");
         }
     }
@@ -79,11 +61,14 @@ class _OgreSampleClassExport Sample_Compute : public SdkSample
         // mCamera->setNearClipDistance(0.1);
         // mCamera->setFarClipDistance(100);
         
-        mOgreEnt = mSceneMgr->createEntity("PlainHead", "ogrehead.mesh");
+        Ogre::MovablePlane plane(Ogre::Vector3::UNIT_Z, 10);
+
+        Ogre::MeshManager::getSingleton().createPlane("PlaneMesh", "General", plane, 20, 20);
+        mOgreEnt = mSceneMgr->createEntity("Plane", "PlaneMesh");
         mOgreEnt->setMaterialName("Compute");
         //mOgreEnt->setMaterialName("BaseWhiteNoLighting");
         SceneNode* ogre = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-        ogre->setPosition(50, -50, 140);
+        ogre->setPosition(0, 0, 0);
         ogre->setDirection(0,0,1);
         ogre->attachObject(mOgreEnt);
 
@@ -97,8 +82,8 @@ class _OgreSampleClassExport Sample_Compute : public SdkSample
             TEX_TYPE_2D, // Texture type
             256, 256, 1, // Width, Height, Depth
             0,           // Number of mipmaps
-            PF_X8R8G8B8  // Pixel format  //TODO support formats from GL3+
-            //TU_DYNAMIC   // usage
+            PF_B8G8R8A8,  // Pixel format  //TODO support formats from GL3+
+            TU_DYNAMIC   // usage
         );
 
         mImage->createShaderAccessPoint(0);
@@ -121,8 +106,7 @@ class _OgreSampleClassExport Sample_Compute : public SdkSample
             for(size_t x = 0; x < width; ++x)
             {
                 // 0xXXRRGGBB -> fill the buffer with yellow pixels
-                // data[pitch * y + x] = 0x00FFFF00;
-                data[pitch * y + x] = 0x0087CEEB;
+                data[pitch * y + x] = 0x00FFFF00;
             }
         }
 
@@ -157,38 +141,13 @@ class _OgreSampleClassExport Sample_Compute : public SdkSample
 
     bool frameRenderingQueued(const FrameEvent& evt)
     {
+        /*
         Real seconds = (Real)(Root::getSingleton().getTimer()->getMilliseconds()) / 1000.0;
-        Pass* renderPass = mOgreEnt->getSubEntity(0)->getMaterial()->getTechnique(0)->getPass(0);
-        if (renderPass->hasFragmentProgram())
-        {
-            Ogre::GpuProgramParametersSharedPtr pParams = renderPass->getFragmentProgramParameters();
-            if ( pParams.isNull() )
-            {
-                //printf("SAD PANDA!");
-            }
-            else
-            { 
-                if ( pParams->_findNamedConstantDefinition( "ColourMe[0]" ) )
-                {
-                    Vector4 constParam = Ogre::Vector4(0.5, 0.1, 0.0, 1.0);
-                    renderPass->getFragmentProgramParameters()->setNamedConstant("ColourMe[0]", constParam);
-                
-                    Vector4 timeParam = Ogre::Vector4(
-                        Ogre::Math::Sin(seconds)*0.5, 0.0, Ogre::Math::Cos(seconds)*0.5, 0.0);
-                    renderPass->getFragmentProgramParameters()->setNamedConstant("ColourMe[1]", timeParam);
-                }
-                const Ogre::GpuConstantDefinition* atom_counter_def;
-                if ( (atom_counter_def = &pParams->getConstantDefinition("atom_counter")) )
-                {
-                    //TODO lock buffer, retrieve counter value similar to compute above
-                    //const uint* counter = pParams->getUnsignedIntPointer(atom_counter_def->physicalIndex);
-                    //const uint* counter2 = ;
-                    //std::cout << "FOUND THE ATOMS: " << *counter << " " << std::endl; //<< *counter2 << std::endl;
-                }
-            }
-        }
+        Pass* renderPass = mOgreEnt->getSubEntity(0)->getMaterial()->getBestTechnique()->getPass(0);
 
-        // renderPass->getFragmentProgramParameters()->getConstantDefinition("atom_counter").getValue();
+        Ogre::GpuProgramParametersSharedPtr pParams = renderPass->getComputeProgramParameters();
+        pParams->setNamedConstant("roll", Real(fmod(seconds, 1.0)));
+         */
         return SdkSample::frameRenderingQueued(evt); 
     }
 };
--- Samples/Media/materials/programs/GLSL400/ComputeCS.glsl
+++ Samples/Media/materials/programs/GLSL400/ComputeCS.glsl
@@ -1,33 +1,17 @@
 #version 430
 
-uniform float multiplier;
+uniform float roll;
 
-// layout(binding = 0, r32ui) coherent uniform uimage2D image_data;
 layout(binding = 0, rgba8) coherent uniform image2D image_data;
 
+layout (local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
-// struct particle{
-//     vec4 currentPos;
-//     vec4 oldPos;
-// };
-
-// layout(std430, binding=0) buffer particles{
-//     struct particle p[];
-// };
-
-layout (local_size_x = 8, local_size_y = 1, local_size_z = 1) in;
-void main() 
+// source/ details: http://wili.cc/blog/opengl-cs.html
+void main()
 {
-    // uint gid = gl_GlobalInvocationID.x;
-    // p[gid].currentPos.x += 100;
-    
     ivec2 storePos = ivec2(gl_GlobalInvocationID.xy);
-    
-    float localCoef = length(vec2(ivec2(gl_LocalInvocationID.xy)-8)/8.0);
-    float globalCoef = sin(float(gl_WorkGroupID.x+gl_WorkGroupID.y)*0.1)*0.5;
 
-    // imageStore(image_data, storePos,
-    //            multiplier * vec4(1.0 - globalCoef * localCoef, 0.0, 0.0, 1.0));
-    imageStore(image_data, ivec2(0), vec4(0,1,0,1));
-    // imageStore(image_data, ivec2(0), vec4(gl_GlobalInvocationID.x / 256, gl_GlobalInvocationID.y / 256, gl_GlobalInvocationID.z / 256, 1));
+    float localCoef = length(vec2(ivec2(gl_LocalInvocationID.xy)-8)/8.0);
+    float globalCoef = sin(float(gl_WorkGroupID.x+gl_WorkGroupID.y)*0.1 + roll)*0.5;
+    imageStore(image_data, storePos, vec4(vec2(1.0-globalCoef*localCoef),1.0,1.0));
 }
