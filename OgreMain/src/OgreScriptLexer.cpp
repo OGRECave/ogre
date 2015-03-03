33a50122c7d1a889c374168f0f459426f0608cf3
Philip Allgaier <mail@spacegaier.de>
2015-03-03 23:01:46 +0100

    Corrected spaces/tabs of script lexer code for future merge

--- OgreMain/src/OgreScriptLexer.cpp
+++ OgreMain/src/OgreScriptLexer.cpp
@@ -31,257 +31,257 @@ THE SOFTWARE.
 
 namespace Ogre{
 
-	ScriptLexer::ScriptLexer()
-	{
-	}
+    ScriptLexer::ScriptLexer()
+    {
+    }
 
-	ScriptTokenListPtr ScriptLexer::tokenize(const String &str, const String &source)
-	{
-		// State enums
-		enum{ READY = 0, COMMENT, MULTICOMMENT, WORD, QUOTE, VAR, POSSIBLECOMMENT };
+    ScriptTokenListPtr ScriptLexer::tokenize(const String &str, const String &source)
+    {
+        // State enums
+        enum{ READY = 0, COMMENT, MULTICOMMENT, WORD, QUOTE, VAR, POSSIBLECOMMENT };
 
-		// Set up some constant characters of interest
-		const wchar_t varopener = '$', quote = '\"', slash = '/', backslash = '\\', openbrace = '{', closebrace = '}', colon = ':', star = '*', cr = '\r', lf = '\n';
-		char c = 0, lastc = 0;
+        // Set up some constant characters of interest
+        const wchar_t varopener = '$', quote = '\"', slash = '/', backslash = '\\', openbrace = '{', closebrace = '}', colon = ':', star = '*', cr = '\r', lf = '\n';
+        char c = 0, lastc = 0;
 
-		String lexeme;
-		uint32 line = 1, state = READY, lastQuote = 0;
-		ScriptTokenListPtr tokens(OGRE_NEW_T(ScriptTokenList, MEMCATEGORY_GENERAL)(), SPFM_DELETE_T);
+        String lexeme;
+        uint32 line = 1, state = READY, lastQuote = 0;
+        ScriptTokenListPtr tokens(OGRE_NEW_T(ScriptTokenList, MEMCATEGORY_GENERAL)(), SPFM_DELETE_T);
 
-		// Iterate over the input
-		String::const_iterator i = str.begin(), end = str.end();
-		while(i != end)
-		{
-			lastc = c;
-			c = *i;
+        // Iterate over the input
+        String::const_iterator i = str.begin(), end = str.end();
+        while(i != end)
+        {
+            lastc = c;
+            c = *i;
 
-			if(c == quote)
-				lastQuote = line;
+            if(c == quote)
+                lastQuote = line;
 
-			switch(state)
-			{
-			case READY:
-				if(c == slash && lastc == slash)
-				{
-					// Comment start, clear out the lexeme
-					lexeme = "";
-					state = COMMENT;
-				}
-				else if(c == star && lastc == slash)
-				{
-					lexeme = "";
-					state = MULTICOMMENT;
-				}
-				else if(c == quote)
-				{
-					// Clear out the lexeme ready to be filled with quotes!
-					lexeme = c;
-					state = QUOTE;
-				}
-				else if(c == varopener)
-				{
-					// Set up to read in a variable
-					lexeme = c;
-					state = VAR;
-				}
-				else if(isNewline(c))
-				{
-					lexeme = c;
-					setToken(lexeme, line, source, tokens.get());
-				}
-				else if(!isWhitespace(c))
-				{
-					lexeme = c;
-					if(c == slash)
-						state = POSSIBLECOMMENT;
-					else
-						state = WORD;
-				}
-				break;
-			case COMMENT:
-				if(isNewline(c))
-				{
-					lexeme = c;
-					setToken(lexeme, line, source, tokens.get());
-					state = READY;
-				}
-				break;
-			case MULTICOMMENT:
-				if(c == slash && lastc == star)
-					state = READY;
-				break;
-			case POSSIBLECOMMENT:
-				if(c == slash && lastc == slash)
-				{
-					lexeme = "";
-					state = COMMENT;
-					break;	
-				}
-				else if(c == star && lastc == slash)
-				{
-					lexeme = "";
-					state = MULTICOMMENT;
-					break;
-				}
-				else
-				{
-					state = WORD;
-				}
-			case WORD:
-				if(isNewline(c))
-				{
-					setToken(lexeme, line, source, tokens.get());
-					lexeme = c;
-					setToken(lexeme, line, source, tokens.get());
-					state = READY;
-				}
-				else if(isWhitespace(c))
-				{
-					setToken(lexeme, line, source, tokens.get());
-					state = READY;
-				}
-				else if(c == openbrace || c == closebrace || c == colon)
-				{
-					setToken(lexeme, line, source, tokens.get());
-					lexeme = c;
-					setToken(lexeme, line, source, tokens.get());
-					state = READY;
-				}
-				else
-				{
-					lexeme += c;
-				}
-				break;
-			case QUOTE:
-				if(c != backslash)
-				{
-					// Allow embedded quotes with escaping
-					if(c == quote && lastc == backslash)
-					{
-						lexeme += c;
-					}
-					else if(c == quote)
-					{
-						lexeme += c;
-						setToken(lexeme, line, source, tokens.get());
-						state = READY;
-					}
-					else
-					{
-						// Backtrack here and allow a backslash normally within the quote
-						if(lastc == backslash)
-							lexeme = lexeme + "\\" + c;
-						else
-							lexeme += c;
-					}
-				}
-				break;
-			case VAR:
-				if(isNewline(c))
-				{
-					setToken(lexeme, line, source, tokens.get());
-					lexeme = c;
-					setToken(lexeme, line, source, tokens.get());
-					state = READY;
-				}
-				else if(isWhitespace(c))
-				{
-					setToken(lexeme, line, source, tokens.get());
-					state = READY;
-				}
-				else if(c == openbrace || c == closebrace || c == colon)
-				{
-					setToken(lexeme, line, source, tokens.get());
-					lexeme = c;
-					setToken(lexeme, line, source, tokens.get());
-					state = READY;
-				}
-				else
-				{
-					lexeme += c;
-				}
-				break;
-			}
+            switch(state)
+            {
+            case READY:
+                if(c == slash && lastc == slash)
+                {
+                    // Comment start, clear out the lexeme
+                    lexeme = "";
+                    state = COMMENT;
+                }
+                else if(c == star && lastc == slash)
+                {
+                    lexeme = "";
+                    state = MULTICOMMENT;
+                }
+                else if(c == quote)
+                {
+                    // Clear out the lexeme ready to be filled with quotes!
+                    lexeme = c;
+                    state = QUOTE;
+                }
+                else if(c == varopener)
+                {
+                    // Set up to read in a variable
+                    lexeme = c;
+                    state = VAR;
+                }
+                else if(isNewline(c))
+                {
+                    lexeme = c;
+                    setToken(lexeme, line, source, tokens.get());
+                }
+                else if(!isWhitespace(c))
+                {
+                    lexeme = c;
+                    if(c == slash)
+                        state = POSSIBLECOMMENT;
+                    else
+                        state = WORD;
+                }
+                break;
+            case COMMENT:
+                if(isNewline(c))
+                {
+                    lexeme = c;
+                    setToken(lexeme, line, source, tokens.get());
+                    state = READY;
+                }
+                break;
+            case MULTICOMMENT:
+                if(c == slash && lastc == star)
+                    state = READY;
+                break;
+            case POSSIBLECOMMENT:
+                if(c == slash && lastc == slash)
+                {
+                    lexeme = "";
+                    state = COMMENT;
+                    break;	
+                }
+                else if(c == star && lastc == slash)
+                {
+                    lexeme = "";
+                    state = MULTICOMMENT;
+                    break;
+                }
+                else
+                {
+                    state = WORD;
+                }
+            case WORD:
+                if(isNewline(c))
+                {
+                    setToken(lexeme, line, source, tokens.get());
+                    lexeme = c;
+                    setToken(lexeme, line, source, tokens.get());
+                    state = READY;
+                }
+                else if(isWhitespace(c))
+                {
+                    setToken(lexeme, line, source, tokens.get());
+                    state = READY;
+                }
+                else if(c == openbrace || c == closebrace || c == colon)
+                {
+                    setToken(lexeme, line, source, tokens.get());
+                    lexeme = c;
+                    setToken(lexeme, line, source, tokens.get());
+                    state = READY;
+                }
+                else
+                {
+                    lexeme += c;
+                }
+                break;
+            case QUOTE:
+                if(c != backslash)
+                {
+                    // Allow embedded quotes with escaping
+                    if(c == quote && lastc == backslash)
+                    {
+                        lexeme += c;
+                    }
+                    else if(c == quote)
+                    {
+                        lexeme += c;
+                        setToken(lexeme, line, source, tokens.get());
+                        state = READY;
+                    }
+                    else
+                    {
+                        // Backtrack here and allow a backslash normally within the quote
+                        if(lastc == backslash)
+                            lexeme = lexeme + "\\" + c;
+                        else
+                            lexeme += c;
+                    }
+                }
+                break;
+            case VAR:
+                if(isNewline(c))
+                {
+                    setToken(lexeme, line, source, tokens.get());
+                    lexeme = c;
+                    setToken(lexeme, line, source, tokens.get());
+                    state = READY;
+                }
+                else if(isWhitespace(c))
+                {
+                    setToken(lexeme, line, source, tokens.get());
+                    state = READY;
+                }
+                else if(c == openbrace || c == closebrace || c == colon)
+                {
+                    setToken(lexeme, line, source, tokens.get());
+                    lexeme = c;
+                    setToken(lexeme, line, source, tokens.get());
+                    state = READY;
+                }
+                else
+                {
+                    lexeme += c;
+                }
+                break;
+            }
 
-			// Separate check for newlines just to track line numbers
-			if(c == cr || (c == lf && lastc != cr))
-				line++;
-			
-			i++;
-		}
+            // Separate check for newlines just to track line numbers
+            if(c == cr || (c == lf && lastc != cr))
+                line++;
 
-		// Check for valid exit states
-		if(state == WORD || state == VAR)
-		{
-			if(!lexeme.empty())
-				setToken(lexeme, line, source, tokens.get());
-		}
-		else
-		{
-			if(state == QUOTE)
-			{
-				OGRE_EXCEPT(Exception::ERR_INVALID_STATE, 
-					Ogre::String("no matching \" found for \" at line ") + 
-						Ogre::StringConverter::toString(lastQuote),
-					"ScriptLexer::tokenize");
-			}
-		}
+            i++;
+        }
 
-		return tokens;
-	}
+        // Check for valid exit states
+        if(state == WORD || state == VAR)
+        {
+            if(!lexeme.empty())
+                setToken(lexeme, line, source, tokens.get());
+        }
+        else
+        {
+            if(state == QUOTE)
+            {
+                OGRE_EXCEPT(Exception::ERR_INVALID_STATE, 
+                    Ogre::String("no matching \" found for \" at line ") + 
+                    Ogre::StringConverter::toString(lastQuote),
+                    "ScriptLexer::tokenize");
+            }
+        }
 
-	void ScriptLexer::setToken(const Ogre::String &lexeme, Ogre::uint32 line, const String &source, Ogre::ScriptTokenList *tokens)
-	{
-		const char openBracket = '{', closeBracket = '}', colon = ':',
-			quote = '\"', var = '$';
+        return tokens;
+    }
 
-		ScriptTokenPtr token(OGRE_NEW_T(ScriptToken, MEMCATEGORY_GENERAL)(), SPFM_DELETE_T);
-		token->lexeme = lexeme;
-		token->line = line;
-		token->file = source;
-		bool ignore = false;
+    void ScriptLexer::setToken(const Ogre::String &lexeme, Ogre::uint32 line, const String &source, Ogre::ScriptTokenList *tokens)
+    {
+        const char openBracket = '{', closeBracket = '}', colon = ':',
+            quote = '\"', var = '$';
 
-		// Check the user token map first
-		if(lexeme.size() == 1 && isNewline(lexeme[0]))
-		{
-			token->type = TID_NEWLINE;
-			if(!tokens->empty() && tokens->back()->type == TID_NEWLINE)
-				ignore = true;
-		}
-		else if(lexeme.size() == 1 && lexeme[0] == openBracket)
-			token->type = TID_LBRACKET;
-		else if(lexeme.size() == 1 && lexeme[0] == closeBracket)
-			token->type = TID_RBRACKET;
-		else if(lexeme.size() == 1 && lexeme[0] == colon)
-			token->type = TID_COLON;
-		else if(lexeme[0] == var)
-			token->type = TID_VARIABLE;
-		else
-		{
-			// This is either a non-zero length phrase or quoted phrase
-			if(lexeme.size() >= 2 && lexeme[0] == quote && lexeme[lexeme.size() - 1] == quote)
-			{
-				token->type = TID_QUOTE;
-			}
-			else
-			{
-				token->type = TID_WORD;
-			}
-		}
+        ScriptTokenPtr token(OGRE_NEW_T(ScriptToken, MEMCATEGORY_GENERAL)(), SPFM_DELETE_T);
+        token->lexeme = lexeme;
+        token->line = line;
+        token->file = source;
+        bool ignore = false;
 
-		if(!ignore)
-			tokens->push_back(token);
-	}
+        // Check the user token map first
+        if(lexeme.size() == 1 && isNewline(lexeme[0]))
+        {
+            token->type = TID_NEWLINE;
+            if(!tokens->empty() && tokens->back()->type == TID_NEWLINE)
+                ignore = true;
+        }
+        else if(lexeme.size() == 1 && lexeme[0] == openBracket)
+            token->type = TID_LBRACKET;
+        else if(lexeme.size() == 1 && lexeme[0] == closeBracket)
+            token->type = TID_RBRACKET;
+        else if(lexeme.size() == 1 && lexeme[0] == colon)
+            token->type = TID_COLON;
+        else if(lexeme[0] == var)
+            token->type = TID_VARIABLE;
+        else
+        {
+            // This is either a non-zero length phrase or quoted phrase
+            if(lexeme.size() >= 2 && lexeme[0] == quote && lexeme[lexeme.size() - 1] == quote)
+            {
+                token->type = TID_QUOTE;
+            }
+            else
+            {
+                token->type = TID_WORD;
+            }
+        }
 
-	bool ScriptLexer::isWhitespace(Ogre::String::value_type c) const
-	{
-		return c == ' ' || c == '\r' || c == '\t';
-	}
+        if(!ignore)
+            tokens->push_back(token);
+    }
 
-	bool ScriptLexer::isNewline(Ogre::String::value_type c) const
-	{
-		return c == '\n' || c == '\r';
-	}
+    bool ScriptLexer::isWhitespace(Ogre::String::value_type c) const
+    {
+        return c == ' ' || c == '\r' || c == '\t';
+    }
+
+    bool ScriptLexer::isNewline(Ogre::String::value_type c) const
+    {
+        return c == '\n' || c == '\r';
+    }
 
 }
 
