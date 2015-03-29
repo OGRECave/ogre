78eb33f748d482f72752d631a285a39f4a9e6e13
Pavel Rojtberg <rojtberg@gmail.com>
2015-03-29 16:58:31 +0200

    unify empty ConfigDialog implementations beneath OgreConfigDialogNoOp

--- OgreMain/include/OgreConfigDialog.h
+++ OgreMain/include/OgreConfigDialog.h
@@ -32,28 +32,64 @@ THE SOFTWARE.
 #include "OgrePlatform.h"
 
 // Bring in the specific platform's header file: first allow forced override
-#if defined OGRE_GUI_WIN32
+#if defined(OGRE_GUI_WIN32) || OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 # include "WIN32/OgreConfigDialogImp.h"
 #elif defined OGRE_GUI_gtk
 # include "gtk/OgreConfigDialogImp.h"
-#elif defined OGRE_GUI_GLX
-# include "GLX/OgreConfigDialogImp.h"
-#elif OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-# include "WIN32/OgreConfigDialogImp.h"
-#elif OGRE_PLATFORM == OGRE_PLATFORM_WINRT
-# include "WIN32/OgreConfigDialogImpWinRT.h"
-#elif OGRE_PLATFORM == OGRE_PLATFORM_LINUX
-# include "GLX/OgreConfigDialogImp.h"
-#elif OGRE_PLATFORM == OGRE_PLATFORM_NACL
-# include "NaCl/OgreConfigDialogImp.h"
 #elif OGRE_PLATFORM == OGRE_PLATFORM_APPLE
 # include "OSX/OgreConfigDialogImp.h"
-#elif OGRE_PLATFORM == OGRE_PLATFORM_APPLE_IOS
-# include "iOS/OgreConfigDialogImp.h"
-#elif OGRE_PLATFORM == OGRE_PLATFORM_ANDROID
-# include "Android/OgreConfigDialogImp.h"
-#elif OGRE_PLATFORM == OGRE_PLATFORM_EMSCRIPTEN
-# include "Emscripten/OgreConfigDialogImp.h"
+#else
+namespace Ogre
+{
+    /** \addtogroup Core
+    *  @{
+    */
+    /** \addtogroup General
+    *  @{
+    */
+
+    /** Defines the behaviour of an automatic renderer configuration dialog.
+    @remarks
+        OGRE comes with it's own renderer configuration dialog, which
+        applications can use to easily allow the user to configure the
+        settings appropriate to their machine. This class defines the
+        interface to this standard dialog. Because dialogs are inherently
+        tied to a particular platform's windowing system, there will be a
+        different subclass for each platform.
+    @author
+        Steven J. Streeting
+    */
+    class _OgreExport ConfigDialog : public UtilityAlloc
+    {
+    public:
+        ConfigDialog();
+
+        /** Displays the dialog.
+        @remarks
+            This method displays the dialog and from then on the dialog
+            interacts with the user independently. The dialog will be
+            calling the relevant OGRE rendering systems to query them for
+            options and to set the options the user selects. The method
+            returns when the user closes the dialog.
+        @returns
+            If the user accepted the dialog, <b>true</b> is returned.
+        @par
+            If the user cancelled the dialog (indicating the application
+            should probably terminate), <b>false</b> is returned.
+        @see
+            RenderSystem
+        */
+        bool display();
+
+    protected:
+        // platform specific implementation
+        // TODO: use this on remaining platforms instead of duplicating whole header
+        struct PrivateData;
+        PrivateData* mImpl;
+    };
+    /** @} */
+    /** @} */
+}
 #endif
 
 #endif
