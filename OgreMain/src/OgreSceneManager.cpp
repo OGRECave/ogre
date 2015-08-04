6d4f9ecd2cae7f7a0e26744107350793d0e32eb0
Lior Lahav <unknown>
2015-08-04 15:12:43 +0300

    Fixed: unbind geometry program before rendering volume shadows

--- OgreMain/src/OgreSceneManager.cpp
+++ OgreMain/src/OgreSceneManager.cpp
@@ -5570,6 +5570,10 @@ void SceneManager::renderShadowVolumesToStencil(const Light* light,
     {
         mDestRenderSystem->unbindGpuProgram(GPT_VERTEX_PROGRAM);
     }
+    if (mDestRenderSystem->getCapabilities()->hasCapability(RSC_GEOMETRY_PROGRAM))
+    {
+        mDestRenderSystem->unbindGpuProgram(GPT_GEOMETRY_PROGRAM);
+    }
 
     // Turn off colour writing and depth writing
     mDestRenderSystem->_setColourBufferWriteEnabled(false, false, false, false);
