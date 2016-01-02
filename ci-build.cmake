31dc061b2a11bbca1ccda1b8887228337660db52
Pavel Rojtberg <rojtberg@gmail.com>
2016-01-02 14:35:30 +0100

    update CI scripts for iOS

--- ci-build.cmake
+++ ci-build.cmake
@@ -2,6 +2,8 @@ set(GENERATOR)
 set(OTHER)
 set(BUILD_OGREDEPS FALSE)
 
+set(CROSS)
+
 set(CMAKE_BUILD_TYPE Debug)
 
 set(RENDERSYSTEMS
@@ -10,11 +12,29 @@ set(RENDERSYSTEMS
     -DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=$ENV{GL3ES}
     -DOGRE_BUILD_RENDERSYSTEM_GLES2=$ENV{GL3ES})
 
-if("$ENV{TRAVIS_OS_NAME}" STREQUAL "osx")
+if(DEFINED ENV{IOS})
     set(GENERATOR -G Xcode)
     set(RENDERSYSTEMS
-        -DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=TRUE
-        -DOGRE_BUILD_RENDERSYSTEM_GL=FALSE)
+        -DOGRE_BUILD_RENDERSYSTEM_GLES2=TRUE
+        -DOGRE_CONFIG_ENABLE_GLES3_SUPPORT=TRUE)
+    set(CROSS
+        -DIOS_PLATFORM=SIMULATOR
+        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_CURRENT_SOURCE_DIR}/CMake/toolchain/ios.toolchain.xcode.cmake)
+
+    set(OTHER
+        -DOGRE_DEPENDENCIES_DIR=${CMAKE_CURRENT_SOURCE_DIR}/ogredeps
+        ${CROSS})
+
+    set(BUILD_OGREDEPS TRUE)
+elseif("$ENV{TRAVIS_OS_NAME}" STREQUAL "osx")
+    set(GENERATOR -G Xcode)
+    set(RENDERSYSTEMS
+        -DOGRE_BUILD_RENDERSYSTEM_GL=FALSE
+        -DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=TRUE)
+
+    set(OTHER
+        -DOGRE_DEPENDENCIES_DIR=${CMAKE_CURRENT_SOURCE_DIR}/ogredeps
+        ${CROSS})
 endif()
 
 if(DEFINED ENV{APPVEYOR})
@@ -31,7 +51,7 @@ if(DEFINED ENV{APPVEYOR})
 endif()
 
 if(DEFINED ENV{ANDROID})
-    set(ANDROID_FLAGS
+    set(CROSS
         -DANDROID_NATIVE_API_LEVEL=21
         -DANDROID_NDK=${CMAKE_CURRENT_SOURCE_DIR}/android-ndk-r10e
         -DCMAKE_TOOLCHAIN_FILE=${CMAKE_CURRENT_SOURCE_DIR}/CMake/toolchain/android.toolchain.cmake)
@@ -41,7 +61,7 @@ if(DEFINED ENV{ANDROID})
         -DOGRE_CONFIG_ENABLE_GLES3_SUPPORT=FALSE)
 
     set(OTHER
-        ${ANDROID_FLAGS}
+        ${CROSS}
         -DOGRE_DEPENDENCIES_DIR=${CMAKE_CURRENT_SOURCE_DIR}/ogredeps)
 
     message(STATUS "Downloading Android NDK")
@@ -60,10 +80,16 @@ if(BUILD_OGREDEPS)
         http://download.savannah.gnu.org/releases/freetype/freetype-2.6.2.tar.gz
         ./freetype-2.6.2.tar.gz)
     execute_process(COMMAND cmake -E tar xf freetype-2.6.2.tar.gz)
+    # patch toolchain for iOS
+    execute_process(COMMAND cmake -E copy
+        ${CMAKE_CURRENT_SOURCE_DIR}/CMake/toolchain/ios.toolchain.xcode.cmake
+        freetype-2.6.2/builds/cmake/iOS.cmake)
     execute_process(COMMAND cmake
         -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_SOURCE_DIR}/ogredeps
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        ${ANDROID_FLAGS}
+        # workaround for broken iOS toolchain in freetype
+        -DPROJECT_SOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR}/freetype-2.6.2
+        ${CROSS}
         ${GENERATOR}
         ..
         WORKING_DIRECTORY freetype-2.6.2/objs)
@@ -77,7 +103,7 @@ if(BUILD_OGREDEPS)
     execute_process(COMMAND cmake
         -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_SOURCE_DIR}/ogredeps
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        ${ANDROID_FLAGS}
+        ${CROSS}
         ${GENERATOR}
         .
         WORKING_DIRECTORY ZZIPlib-master)
