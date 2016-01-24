73aa4993b3202e60e823a270408bf7a82538e9d8
Pavel Rojtberg <rojtberg@gmail.com>
2016-01-24 18:52:23 +0100

    EGL: allow querying EGL version and extensions

--- RenderSystems/GLES2/include/EGL/OgreEGLSupport.h
+++ RenderSystems/GLES2/include/EGL/OgreEGLSupport.h
@@ -63,7 +63,11 @@ namespace Ogre {
             VideoMode mCurrentMode;
             StringVector mSampleLevels;
 
+            EGLint mEGLMajor, mEGLMinor;
+
             //virtual EGLWindow* createEGLWindow( EGLSupport * support) = 0;
+
+            void initialiseExtensions();
         public:
             EGLSupport();
             virtual ~EGLSupport();
