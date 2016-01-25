b69299d6db2a291208f0c89ed6a7877dbeda2060
Pavel Rojtberg <rojtberg@gmail.com>
2016-01-25 16:18:10 +0100

    GLSupport: getProcAddress: use const char* instead of String

--- RenderSystems/GLSupport/src/GLX/OgreGLXGLSupport.cpp
+++ RenderSystems/GLSupport/src/GLX/OgreGLXGLSupport.cpp
@@ -441,8 +441,8 @@ namespace Ogre
     }
 
     //-------------------------------------------------------------------------------------------------//
-    void* GLXGLSupport::getProcAddress(const String& procname) {
-        return (void*)glXGetProcAddressARB((const GLubyte*)procname.c_str());
+    void* GLXGLSupport::getProcAddress(const char* procname) {
+        return (void*)glXGetProcAddressARB((const GLubyte*)procname);
     }
 
     //-------------------------------------------------------------------------------------------------//
