88db1e762a682a3030a29ba919cc9040d7310ba2
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2016-02-20 12:18:57 -0300

    [D3D11] Fixed HLSL compiler errors in PBS when using shadows.

--- Samples/Media/Hlms/Common/HLSL/RenderDepthOnly_piece_ps.hlsl
+++ Samples/Media/Hlms/Common/HLSL/RenderDepthOnly_piece_ps.hlsl
@@ -1,15 +1,13 @@
 
-@property( !hlms_shadowcaster )
-	@property( !hlms_render_depth_only )
-		@property( !hlms_shadowcaster )
-			@piece( output_type )float4@end
-		@end @property( hlms_shadowcaster )
-			@piece( output_type )float@end
-		@end
-		@piece( output_type_sv ): SV_Target0@end
-	@end @property( hlms_render_depth_only )
-		@piece( output_type )void@end
+@property( !hlms_render_depth_only )
+	@property( !hlms_shadowcaster )
+		@piece( output_type )float4@end
+	@end @property( hlms_shadowcaster )
+		@piece( output_type )float@end
 	@end
+	@piece( output_type_sv ): SV_Target0@end
+@end @property( hlms_render_depth_only )
+	@piece( output_type )void@end
 @end
 
 @property( hlms_render_depth_only && !alpha_test )
