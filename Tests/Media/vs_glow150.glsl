11a0f0dcfe5190c222d679cdadd7495cd9c21593
Murat Sari <wolfmanfx@gmail.com>
2016-03-23 22:53:23 +0100

    Fix playpen stencil_glow for gl3plus Use ENABLE_GL_DEBUG_OUTPUT to control GL_DEBUG_OUTPUT Disable GL_CHECK & ENABLE_GL_DEBUG_OUTPUT

--- Tests/Media/vs_glow150.glsl
+++ Tests/Media/vs_glow150.glsl
@@ -9,7 +9,7 @@ in vec3 normal;
 
 void main(void)
 {
-    vec3 Pos = vertex.xyz + (size_value * (1.0 + (sin(time * 5.0) + 1.0) / 15.0 ) * normal );
+    vec3 Pos = vertex.xyz + (size_value * (1.0 + (sin(time * 5.0) + 1.0) / 15.0 ) * normal);
 
     gl_Position = worldViewProjMatrix * vec4(Pos,1.0);
 }
