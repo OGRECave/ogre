f8a3ffd806d7b6ae80d8f212b033fcb24d893a47
Eugene Golushkov <eugene@REMOVESPAMbelightsoft.com>
2016-03-28 15:10:39 +0300

    [iOS] Non-fragile OpenGLES.framework loading (fixed crash on iOS sim 9.x)

--- RenderSystems/GLES2/src/gles2w.cpp
+++ RenderSystems/GLES2/src/gles2w.cpp
@@ -53,38 +53,11 @@ CFURLRef bundleURL;
 
 static void open_libgl(void)
 {
-    CFStringRef frameworkPath = CFSTR("/System/Library/Frameworks/OpenGLES.framework");
-    NSString *sysVersion = [UIDevice currentDevice].systemVersion;
-    NSArray *sysVersionComponents = [sysVersion componentsSeparatedByString:@"."];
-
-    BOOL isSimulator = ([[UIDevice currentDevice].model rangeOfString:@"Simulator"].location != NSNotFound);
-    if(isSimulator)
-    {
-        // Ask where Xcode is installed
-        std::string xcodePath = "/Applications/Xcode.app/Contents/Developer\n";
-
-        // The result contains an end line character. Remove it.
-        size_t pos = xcodePath.find("\n");
-        xcodePath.erase(pos);
-
-        char tempPath[PATH_MAX];
-        sprintf(tempPath,
-                "%s/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator%s.%s.sdk/System/Library/Frameworks/OpenGLES.framework",
-                xcodePath.c_str(),
-                [[sysVersionComponents objectAtIndex:0] cStringUsingEncoding:NSUTF8StringEncoding],
-                [[sysVersionComponents objectAtIndex:1] cStringUsingEncoding:NSUTF8StringEncoding]);
-        frameworkPath = CFStringCreateWithCString(kCFAllocatorDefault, tempPath, kCFStringEncodingUTF8);
-    }
-
-    bundleURL = CFURLCreateWithFileSystemPath(kCFAllocatorDefault,
-                                              frameworkPath,
-                                              kCFURLPOSIXPathStyle, true);
-
-    CFRelease(frameworkPath);
-
-    bundle = CFBundleCreate(kCFAllocatorDefault, bundleURL);
-
+    bundle = CFBundleGetBundleWithIdentifier(CFSTR("com.apple.opengles")); // we are always linked to OpenGLES.framework statically, so it is already loaded and could be found by id
     assert(bundle != NULL);
+    
+    CFRetain(bundle);
+    bundleURL = CFBundleCopyBundleURL(bundle);
 }
 
 static void close_libgl(void)
