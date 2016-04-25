f5a15c1a9639828a56ebb03476e011a3edc2e085
Matías N. Goldberg <dark_sylinc@yahoo.com.ar>
2016-04-25 19:28:08 -0300

    Fixed crash when rendering v1 meshes that have a valid indexData pointer with a null index buffer. This is what v1 Mesh does BY DEFAULT and unfortunately a lot of old code relies on that behavior. Gee, whoever wrote that... what was he thinking.

--- OgreMain/src/OgreRenderQueue.cpp
+++ OgreMain/src/OgreRenderQueue.cpp
@@ -722,7 +722,7 @@ namespace Ogre
                     lastRenderOp = renderOp;
                 }
 
-                if( renderOp.indexData )
+                if( renderOp.useIndexes )
                 {
                     v1::CbDrawCallIndexed *drawCall =
                             mCommandBuffer->addCommand<v1::CbDrawCallIndexed>();
--- RenderSystems/GL3Plus/src/OgreGL3PlusRenderSystem.cpp
+++ RenderSystems/GL3Plus/src/OgreGL3PlusRenderSystem.cpp
@@ -2877,7 +2877,7 @@ namespace Ogre {
             ++itor;
         }
 
-        if( cmd->indexData )
+        if( cmd->indexData && !cmd->indexData->indexBuffer.isNull() )
         {
             v1::GL3PlusHardwareIndexBuffer *indexBuffer = static_cast<v1::GL3PlusHardwareIndexBuffer*>(
                                                                     cmd->indexData->indexBuffer.get() );
