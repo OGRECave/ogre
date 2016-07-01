630da15a411da86461904a1a7ffdc8800eb780d2
Pavel Rojtberg <rojtberg@gmail.com>
2016-07-02 01:26:57 +0200

    port Emscripten Sample to OgreBites

--- Samples/Emscripten/build.sh
+++ Samples/Emscripten/build.sh
@@ -7,7 +7,7 @@ cp ../Media/packs/SdkTrays.zip media/
 cp ../Media/packs/Sinbad.zip media/
 cp -R ../Media/RTShaderLib/GLSLES media/RTShaderLib/
 
-INCLUDES="-I$OGRE_SDK/include/OGRE/RTShaderSystem -I$OGRE_SDK/include/OGRE/RenderSystems/GLES2 -I$OGRE_SDK/include/OGRE/Overlay -I$OGRE_SDK/include/OGRE"
-LIBRARIES="-L$OGRE_SDK/lib/ -lz -lOgreMain -lOgreOverlay -lOgreRTShaderSystem -lOGRE/RenderSystem_GLES2"
+INCLUDES="-I$OGRE_SDK/include/OGRE/Bites -I$OGRE_SDK/include/OGRE/RTShaderSystem -I$OGRE_SDK/include/OGRE/RenderSystems/GLES2 -I$OGRE_SDK/include/OGRE/Overlay -I$OGRE_SDK/include/OGRE"
+LIBRARIES="-L$OGRE_SDK/lib/ -lz -lOgreMain -lOgreOverlay -lOgreRTShaderSystem -lOgreBites -lOGRE/RenderSystem_GLES2"
 
 em++ -std=c++11 -O3 Main.cpp Sample.cpp $INCLUDES $LIBRARIES -o release.html --preload-file media/@. -s EXPORTED_FUNCTIONS="['_passAssetAsArrayBuffer', '_clearScene', '_main']" -s ALLOW_MEMORY_GROWTH=1 --closure 1
