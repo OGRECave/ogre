b4792ae2eb2c949689e971bb4e6d394245c08772
Pavel Rojtberg <rojtberg@gmail.com>
2016-08-31 12:08:13 +0200

    Threading: fixup for STD thread provider
    - static templated member belongs in the header
    - simplify threading options logic

--- CMake/PrepareThreadingOptions.cmake
+++ CMake/PrepareThreadingOptions.cmake
@@ -12,8 +12,6 @@
 # build, depending on the dependencies found.
 #######################################################################
 
-#always available via std
-set(OGRE_THREAD_SUPPORT_AVAILABLE TRUE)
 set(OGRE_THREAD_DEFAULT_PROVIDER "none")
 set(OGRE_THREAD_TYPE "0")
 
@@ -22,19 +20,19 @@ if (Boost_THREAD_FOUND AND Boost_DATE_TIME_FOUND)
 endif ()
 
 if (Boost_THREADING AND NOT OGRE_THREAD_SUPPORT_AVAILABLE)
-	#set(OGRE_THREAD_SUPPORT_AVAILABLE TRUE)
+	set(OGRE_THREAD_SUPPORT_AVAILABLE TRUE)
 	set(OGRE_THREAD_DEFAULT_PROVIDER "boost")
 	set(OGRE_THREAD_TYPE "2")
 endif ()
 
 if (POCO_FOUND AND NOT OGRE_THREAD_SUPPORT_AVAILABLE)
-	#set(OGRE_THREAD_SUPPORT_AVAILABLE TRUE)
+	set(OGRE_THREAD_SUPPORT_AVAILABLE TRUE)
 	set(OGRE_THREAD_DEFAULT_PROVIDER "poco")
 	set(OGRE_THREAD_TYPE "2")
 endif ()
 
 if (TBB_FOUND AND NOT OGRE_THREAD_SUPPORT_AVAILABLE)
-	#set(OGRE_THREAD_SUPPORT_AVAILABLE TRUE)
+	set(OGRE_THREAD_SUPPORT_AVAILABLE TRUE)
 	set(OGRE_THREAD_DEFAULT_PROVIDER "tbb")
 	set(OGRE_THREAD_TYPE "2")
 endif ()
@@ -44,25 +42,19 @@ if (OGRE_CONFIG_THREAD_PROVIDER STREQUAL "std")
 	set(OGRE_THREAD_TYPE "2")
 endif ()
 
-if (OGRE_THREAD_SUPPORT_AVAILABLE)
-	set(OGRE_CONFIG_THREADS ${OGRE_THREAD_TYPE} CACHE STRING 
-		"Enable Ogre thread support for background loading. Possible values:
-		0 - Threading off.
-		1 - Full background loading.
-		2 - Background resource preparation."
-	)
-	set(OGRE_CONFIG_THREAD_PROVIDER ${OGRE_THREAD_DEFAULT_PROVIDER} CACHE STRING
-		"Select the library to use for thread support. Possible values:
-		boost - Boost thread library.
-		poco  - Poco thread library.
-		tbb   - ThreadingBuildingBlocks library.
-		std   - STL thread library (requires compiler support)."
-	)
-else ()
-	set(OGRE_CONFIG_THREADS 0)
-	set(OGRE_CONFIG_THREAD_PROVIDER "none")
-endif ()
-
+set(OGRE_CONFIG_THREADS ${OGRE_THREAD_TYPE} CACHE STRING 
+	"Enable Ogre thread support for background loading. Possible values:
+	0 - Threading off.
+	1 - Full background loading.
+	2 - Background resource preparation."
+)
+set(OGRE_CONFIG_THREAD_PROVIDER ${OGRE_THREAD_DEFAULT_PROVIDER} CACHE STRING
+	"Select the library to use for thread support. Possible values:
+	boost - Boost thread library.
+	poco  - Poco thread library.
+	tbb   - ThreadingBuildingBlocks library.
+	std   - STL thread library (requires compiler support)."
+)
 
 # sanitise threading choices
 if (NOT OGRE_CONFIG_THREADS)
@@ -86,7 +78,6 @@ else ()
 		endif ()
 	elseif (OGRE_CONFIG_THREAD_PROVIDER STREQUAL "std")
 		#Potentially add test for compiler support
-		set(OGRE_CONFIG_THREAD_PROVIDER ${OGRE_THREAD_DEFAULT_PROVIDER})
 	else ()
 		message(STATUS "Warning: Unknown thread provider chosen. Defaulting to ${OGRE_THREAD_DEFAULT_PROVIDER}.")
 	endif ()	
