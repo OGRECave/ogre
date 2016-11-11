b7e47bbf1304bc65d2a40bcf2fc0ef5fa8435eb0
Eugene Golushkov <e.golushkov@nospam_gmail.com>
2016-11-11 14:17:19 +0200

    [D3D11] Memory corruption in D3D11HardwareIndexBuffer::copyData fixed

--- RenderSystems/Direct3D11/src/OgreD3D11HardwareUniformBuffer.cpp
+++ RenderSystems/Direct3D11/src/OgreD3D11HardwareUniformBuffer.cpp
@@ -73,10 +73,10 @@ namespace Ogre {
 	void D3D11HardwareUniformBuffer::copyData(HardwareBuffer& srcBuffer, size_t srcOffset, 
 		size_t dstOffset, size_t length, bool discardWholeBuffer)
 	{
-		// check if the other buffer is also a D3D11HardwareVertexBuffer
+		// check if the other buffer is also a D3D11HardwareUniformBuffer
 		if (srcBuffer.isSystemMemory())
 		{
-			// src is not a D3D11HardwareVertexBuffer - use default copy
+			// src is not a D3D11HardwareUniformBuffer - use default copy
 			HardwareBuffer::copyData(srcBuffer, srcOffset, dstOffset, length, discardWholeBuffer);
 		}
 		else
