f50ebc2bb27ab5df7fb533c0de974ae9e26f59aa
Pavel Rojtberg <rojtberg@gmail.com>
2016-12-09 00:46:29 +0100

    RTShader: FFPLib - GLSL add some missing functions

--- Samples/Media/RTShaderLib/GLSL/FFPLib_Common.glsl
+++ Samples/Media/RTShaderLib/GLSL/FFPLib_Common.glsl
@@ -251,3 +251,9 @@ void FFP_DotProduct(in vec4 vIn0, in vec4 vIn1, out vec4 vOut)
 {
 	vOut = vec4(dot(vIn0, vIn1), 1.0, 1.0, 1.0);
 }
+
+//-----------------------------------------------------------------------------
+void FFP_Normalize(inout vec3 vIn)
+{
+    vIn = normalize(vIn);
+}
