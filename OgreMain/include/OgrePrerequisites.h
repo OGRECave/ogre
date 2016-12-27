0b37a49a342fd0f328aa1207d92dbaedfa9e328d
Pavel Rojtberg <rojtberg@gmail.com>
2016-12-27 15:19:29 +0100

    bump patch level

--- OgreMain/include/OgrePrerequisites.h
+++ OgreMain/include/OgrePrerequisites.h
@@ -50,7 +50,7 @@ namespace Ogre {
     // Define ogre version
     #define OGRE_VERSION_MAJOR 1
     #define OGRE_VERSION_MINOR 10
-    #define OGRE_VERSION_PATCH 0
+    #define OGRE_VERSION_PATCH 1
     #define OGRE_VERSION_SUFFIX "unstable"
     #define OGRE_VERSION_NAME "Xalafu"
 
