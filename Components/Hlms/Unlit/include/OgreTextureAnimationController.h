4c530bd2dc898e61c685430a0262859e2ec7ed57
MisterKH <misterk1@outlook.de>
2017-02-15 11:20:57 -0300

    - Fix name collision with old TexCoordModifierControllerValue - Fix export definition - dark_sylinc: Changed code formatting to comply with Ogre standards.

--- Components/Hlms/Unlit/include/OgreTextureAnimationController.h
+++ Components/Hlms/Unlit/include/OgreTextureAnimationController.h
@@ -1,85 +1,89 @@
-#pragma once
 
+#ifndef _OgreTextureAnimationControllerValue_H_
+#define _OgreTextureAnimationControllerValue_H_
+
+#include "OgreHlmsUnlitPrerequisites.h"
 #include "OgreController.h"
 #include "OgreMatrix4.h"
 
-namespace Ogre {
+namespace Ogre
+{
+    /** Predefined controller value for getting / setting a texture coordinate
+        modifications (scales and translates).
+    @remarks
+        Effects can be applied to the scale or the offset of the u or v coordinates, or both.
+        If separate modifications are required to u and v then 2 instances are required to
+        control both independently, or 4 if you want separate u and v scales as well as
+        separate u and v offsets.
+    @par
+        Because of the nature of this value, it can accept values outside the 0..1 parametric range.
+    */
+    class _OgreHlmsUnlitExport TextureAnimationControllerValue : public Ogre::ControllerValue<Ogre::Real>
+    {
+    protected:
+        bool mTransU, mTransV;
+        bool mScaleU, mScaleV;
+        bool mRotate;
 
-	//-----------------------------------------------------------------------
-	/** Predefined controller value for getting / setting a texture coordinate modifications (scales and translates).
-	@remarks
-	Effects can be applied to the scale or the offset of the u or v coordinates, or both. If separate
-	modifications are required to u and v then 2 instances are required to control both independently, or 4
-	if you want separate u and v scales as well as separate u and v offsets.
-	@par
-	Because of the nature of this value, it can accept values outside the 0..1 parametric range.
-	*/
-	class _OgreExport TexCoordModifierControllerValue : public Ogre::ControllerValue<Ogre::Real>
-	{
-	protected:
-		bool mTransU, mTransV;
-		bool mScaleU, mScaleV;
-		bool mRotate;
-		
-		Ogre::HlmsDatablock *mdatablock;
-		Ogre::uint8 mtextureUnit;
+        HlmsDatablock   *mDatablock;
+        uint8           mTextureUnit;
 
-		Ogre::Real mUMod, mVMod;
-		Ogre::Real mUScale, mVScale;
-		Ogre::Radian mRotation;
-		mutable Ogre::Matrix4 mTexModMatrix;
+        Real    mUMod, mVMod;
+        Real    mUScale, mVScale;
+        Radian  mRotation;
+        mutable Matrix4 mTexModMatrix;
 
-		//---- Tiled Texture
-		int mNumFramesHorizontal, mNumFramesVertical;
-		Ogre::uint16 mLastFrame, mCurrentVerticalFrame;
+        //---- Tiled Texture
+        int mNumFramesHorizontal, mNumFramesVertical;
+        uint16 mLastFrame, mCurrentVerticalFrame;
 
-	public:
-		/** Constructor.
-		@param
-		datablock HlmsDatablock to apply the modification to.
-		@param
-		textureUnit  to apply the modification to.
-		*/
-		TexCoordModifierControllerValue(Ogre::HlmsDatablock *datablock, Ogre::uint8 textureUnit);
+    public:
+        /** Constructor.
+        @param datablock
+            HlmsDatablock to apply the modification to.
+        @param textureUnit
+            textureUnit to apply the modification to.
+        */
+        TextureAnimationControllerValue(Ogre::HlmsDatablock *datablock, Ogre::uint8 textureUnit);
 
-		Ogre::Real getValue(void) const;
+        Ogre::Real getValue(void) const;
 
-		void setValue(Ogre::Real value);
+        void setValue(Ogre::Real value);
 
-		/** scaleAnimation.
-		@param
-		scaleU If true, the u coordinates will be scaled by the modification.
-		@param
-		scaleV If true, the v coordinates will be scaled by the modification.
-		*/
-		void scaleAnimation(bool scaleU, bool scaleV);
+        /** scaleAnimation.
+        @param scaleU
+            If true, the u coordinates will be scaled by the modification.
+        @param scaleV
+            If true, the v coordinates will be scaled by the modification.
+        */
+        void scaleAnimation(bool scaleU, bool scaleV);
 
-		/** scrollAnimation.
-		@param
-		textureUnit  to apply the modification to.
-		@param
-		translateV If true, the v coordinates will be translated by the modification.
-		*/
-		void scrollAnimation(bool translateU, bool translateV);
-		
-		/** rotationAnimation.
-		@param
-		rotate If true, the texture will be rotated by the modification.
-		*/
-		void rotationAnimation(bool rotate);
+        /** scrollAnimation.
+        @param textureUnit
+            textureUnit to apply the modification to.
+        @param translateV
+            If true, the v coordinates will be translated by the modification.
+        */
+        void scrollAnimation(bool translateU, bool translateV);
 
-		/** tiledAnimation.
-		@param
-		numFramesHorizontal number of horizontal tiles.
-		@param
-		numFramesVertical number of vertical tiles.
-			*/
-		void tiledAnimation(Ogre::uint16 numFramesHorizontal, Ogre::uint16 numFramesVertical);
+        /** rotationAnimation.
+        @param rotate
+            If true, the texture will be rotated by the modification.
+        */
+        void rotationAnimation(bool rotate);
 
+        /** tiledAnimation.
+        @param numFramesHorizontal
+            number of horizontal tiles.
+        @param numFramesVertical
+            number of vertical tiles.
+        */
+        void tiledAnimation(Ogre::uint16 numFramesHorizontal, Ogre::uint16 numFramesVertical);
 
-	private:
-		void recalcTextureMatrix() const;
-
-	};
+    private:
+        void recalcTextureMatrix() const;
+    };
 
 }
+
+#endif
--- Components/Hlms/Unlit/src/OgreTextureAnimationController.cpp
+++ Components/Hlms/Unlit/src/OgreTextureAnimationController.cpp
@@ -1,175 +1,178 @@
+
+#include "OgreStableHeaders.h"
+
 #include "OgreTextureAnimationController.h"
 #include "OgreMath.h"
 #include "OgreHlmsUnlitDatablock.h"
 
 namespace Ogre {
 
-	void TexCoordModifierControllerValue::recalcTextureMatrix() const
-	{
-		// Assumption: 2D texture coords
-		Ogre::Matrix4 xform;
+    //-----------------------------------------------------------------------
+    // TexCoordModifierControllerValue
+    //-----------------------------------------------------------------------
+    TextureAnimationControllerValue::TextureAnimationControllerValue( HlmsDatablock *datablock,
+                                                                      uint8 textureUnit )
+        : mDatablock(datablock)
+        , mTextureUnit(textureUnit)
+        , mTransU(false)
+        , mTransV(false)
+        , mScaleU(false)
+        , mScaleV(false)
+        , mRotate(false)
+        , mUMod(0)
+        , mVMod(0)
+        , mUScale(1)
+        , mVScale(1)
+        , mRotation(0)
+        , mTexModMatrix(Matrix4::IDENTITY)
+        , mLastFrame(0)
+        , mNumFramesHorizontal(0)
+        , mNumFramesVertical(0)
+        , mCurrentVerticalFrame(0)
+    {
+    }
+    //-----------------------------------------------------------------------------------
+    Real TextureAnimationControllerValue::getValue() const
+    {
+        if (mTransU)
+        {
+            return mTexModMatrix[0][3];
+        }
+        else if (mTransV)
+        {
+            return mTexModMatrix[1][3];
+        }
+        else if (mScaleU)
+        {
+            return mTexModMatrix[0][0];
+        }
+        else if (mScaleV)
+        {
+            return mTexModMatrix[1][1];
+        }
+        // Shouldn't get here
+        return 0;
+    }
+    //-----------------------------------------------------------------------------------
+    void TextureAnimationControllerValue::setValue(Real value)
+    {
+        if (mNumFramesHorizontal > 0) // tex tile animation
+        {
+            // The current animation frame.
+            unsigned int currentFrame = ((int)(value * mNumFramesHorizontal) % mNumFramesHorizontal);
 
-		xform = Ogre::Matrix4::IDENTITY;
-		if (mUScale != 1 || mVScale != 1)
-		{
-			// Offset to center of texture
-			xform[0][0] = 1 / mUScale;
-			xform[1][1] = 1 / mVScale;
+            if (mLastFrame == 0 && currentFrame == (mNumFramesHorizontal - 1))
+            {
+                if (mCurrentVerticalFrame == (mNumFramesVertical - 1))
+                    mCurrentVerticalFrame = 0;
+                else
+                    mCurrentVerticalFrame++;
+            }
 
-			if (mNumFramesHorizontal == 0) // no tex tile animation
-			{
-				//// Skip matrix concat since first matrix update
-				xform[0][3] = (-0.5f * xform[0][0]) + 0.5f;
-				xform[1][3] = (-0.5f * xform[1][1]) + 0.5f;
-			}
-		}
+            mUMod = (1 / (Real)mNumFramesHorizontal) * (Real)currentFrame;
+            mVMod = (1 / (Real)mNumFramesVertical) * (Real)mCurrentVerticalFrame;
 
-		if (mUMod || mVMod)
-		{
-			Ogre::Matrix4 xlate = Ogre::Matrix4::IDENTITY;
+            mLastFrame = currentFrame;
 
-			xlate[0][3] = mUMod;
-			xlate[1][3] = mVMod;
+            mUScale = mNumFramesHorizontal;
+            mVScale = mNumFramesVertical;
+        }
+        else
+        {
+            if (mTransU)
+                mUMod = value;
 
-			xform = xlate * xform;
-		}
+            if (mTransV)
+                mVMod = value;
 
-		if (mRotation != Ogre::Radian(0))
-		{
-			Ogre::Matrix4 rot = Ogre::Matrix4::IDENTITY;
-			Ogre::Radian theta(mRotation);
-			Ogre::Real cosTheta = Ogre::Math::Cos(theta);
-			Ogre::Real sinTheta = Ogre::Math::Sin(theta);
+            if (mScaleU)
+                mUScale = value;
 
-			rot[0][0] = cosTheta;
-			rot[0][1] = -sinTheta;
-			rot[1][0] = sinTheta;
-			rot[1][1] = cosTheta;
-			// Offset center of rotation to center of texture
-			rot[0][3] = 0.5f + ((-0.5f * cosTheta) - (-0.5f * sinTheta));
-			rot[1][3] = 0.5f + ((-0.5f * sinTheta) + (-0.5f * cosTheta));
+            if (mScaleV)
+                mVScale = value;
 
-			xform = rot * xform;
-		}
+            if (mRotate)
+                mRotation = Radian(value * Math::TWO_PI);
+        }
 
-		mTexModMatrix = xform;
-		
-		Ogre::HlmsUnlitDatablock *datablockUnlit = (Ogre::HlmsUnlitDatablock*)mdatablock;
-		datablockUnlit->setAnimationMatrix(mtextureUnit, mTexModMatrix);
-	}
+        recalcTextureMatrix();
+    }
+    //-----------------------------------------------------------------------------------
+    void TextureAnimationControllerValue::scaleAnimation(bool scaleU, bool scaleV)
+    {
+         mScaleU = scaleU;
+         mScaleV = scaleV;
+    }
+    //-----------------------------------------------------------------------------------
+    void TextureAnimationControllerValue::scrollAnimation(bool translateU, bool translateV)
+    {
+        mTransU = translateU;
+        mTransV = translateV;
+    }
+    //-----------------------------------------------------------------------------------
+    void TextureAnimationControllerValue::rotationAnimation(bool rotate)
+    {
+        mRotate = rotate;
+    }
+    //-----------------------------------------------------------------------------------
+    void TextureAnimationControllerValue::tiledAnimation( uint16 numFramesHorizontal,
+                                                          uint16 numFramesVertical )
+    {
+        mNumFramesHorizontal = numFramesHorizontal;
+        mNumFramesVertical = numFramesVertical;
+    }
+    //-----------------------------------------------------------------------------------
+    void TextureAnimationControllerValue::recalcTextureMatrix() const
+    {
+        // Assumption: 2D texture coords
+        Matrix4 xform;
 
-	//-----------------------------------------------------------------------
-	// TexCoordModifierControllerValue
-	//-----------------------------------------------------------------------
-	TexCoordModifierControllerValue::TexCoordModifierControllerValue(Ogre::HlmsDatablock *datablock, Ogre::uint8 textureUnit)
-		: mdatablock(datablock)
-		, mtextureUnit(textureUnit)
-		, mTransU(false)
-		, mTransV(false)
-		, mScaleU(false)
-		, mScaleV(false)
-		, mRotate(false)
-		, mUMod(0)
-		, mVMod(0)
-		, mUScale(1)
-		, mVScale(1)
-		, mRotation(0)
-		, mTexModMatrix(Ogre::Matrix4::IDENTITY)
-		, mLastFrame(0)
-		, mNumFramesHorizontal(0)
-		, mNumFramesVertical(0)
-		, mCurrentVerticalFrame(0)
-	{
+        xform = Matrix4::IDENTITY;
+        if (mUScale != 1 || mVScale != 1)
+        {
+            // Offset to center of texture
+            xform[0][0] = 1 / mUScale;
+            xform[1][1] = 1 / mVScale;
 
-	}
+            if (mNumFramesHorizontal == 0) // no tex tile animation
+            {
+                //// Skip matrix concat since first matrix update
+                xform[0][3] = (-0.5f * xform[0][0]) + 0.5f;
+                xform[1][3] = (-0.5f * xform[1][1]) + 0.5f;
+            }
+        }
 
-	//-----------------------------------------------------------------------
-	Ogre::Real TexCoordModifierControllerValue::getValue() const
-	{
-		if (mTransU)
-		{
-			return mTexModMatrix[0][3];
-		}
-		else if (mTransV)
-		{
-			return mTexModMatrix[1][3];
-		}
-		else if (mScaleU)
-		{
-			return mTexModMatrix[0][0];
-		}
-		else if (mScaleV)
-		{
-			return mTexModMatrix[1][1];
-		}
-		// Shouldn't get here
-		return 0;
-	}
-	//-----------------------------------------------------------------------
-	void TexCoordModifierControllerValue::setValue(Ogre::Real value)
-	{
-		if (mNumFramesHorizontal > 0) // tex tile animation
-		{
-			unsigned int currentFrame = ((int)(value * mNumFramesHorizontal) % mNumFramesHorizontal); // The current animation frame.
+        if (mUMod || mVMod)
+        {
+            Matrix4 xlate = Matrix4::IDENTITY;
 
-			if (mLastFrame == 0 && currentFrame == (mNumFramesHorizontal - 1))
-			{
-				if (mCurrentVerticalFrame == (mNumFramesVertical - 1))
-					mCurrentVerticalFrame = 0;
-				else
-					mCurrentVerticalFrame++;
-			}
+            xlate[0][3] = mUMod;
+            xlate[1][3] = mVMod;
 
-			mUMod = (1 / (Ogre::Real)mNumFramesHorizontal) * (Ogre::Real)currentFrame;
-			mVMod = (1 / (Ogre::Real)mNumFramesVertical) * (Ogre::Real)mCurrentVerticalFrame;
+            xform = xlate * xform;
+        }
 
-			mLastFrame = currentFrame;
+        if (mRotation != Radian(0))
+        {
+            Matrix4 rot = Matrix4::IDENTITY;
+            Radian theta(mRotation);
+            Real cosTheta = Math::Cos(theta);
+            Real sinTheta = Math::Sin(theta);
 
-			mUScale = mNumFramesHorizontal;
-			mVScale = mNumFramesVertical;
-		}
-		else 
-		{
-			if (mTransU)
-				mUMod = value;
+            rot[0][0] = cosTheta;
+            rot[0][1] = -sinTheta;
+            rot[1][0] = sinTheta;
+            rot[1][1] = cosTheta;
+            // Offset center of rotation to center of texture
+            rot[0][3] = 0.5f + ((-0.5f * cosTheta) - (-0.5f * sinTheta));
+            rot[1][3] = 0.5f + ((-0.5f * sinTheta) + (-0.5f * cosTheta));
 
-			if (mTransV)
-				mVMod = value;
+            xform = rot * xform;
+        }
 
-			if (mScaleU)
-				mUScale = value;
+        mTexModMatrix = xform;
 
-			if (mScaleV)
-				mVScale = value;
-
-			if (mRotate)
-				mRotation = Ogre::Radian(value * Ogre::Math::TWO_PI);
-		}
-
-		recalcTextureMatrix();
-	}
-
-
-	void TexCoordModifierControllerValue::scaleAnimation(bool scaleU, bool scaleV)
-	{
-		 mScaleU = scaleU;
-		 mScaleV = scaleV;
-	}
-
-	void TexCoordModifierControllerValue::scrollAnimation(bool translateU, bool translateV)
-	{
-		mTransU = translateU;
-		mTransV = translateV;
-	}
-
-	void TexCoordModifierControllerValue::rotationAnimation(bool rotate)
-	{
-		mRotate = rotate;
-	}
-
-	void TexCoordModifierControllerValue::tiledAnimation(Ogre::uint16 numFramesHorizontal, Ogre::uint16 numFramesVertical)
-	{
-		mNumFramesHorizontal = numFramesHorizontal;
-		mNumFramesVertical = numFramesVertical;
-	}
+        HlmsUnlitDatablock *datablockUnlit = (HlmsUnlitDatablock*)mDatablock;
+        datablockUnlit->setAnimationMatrix(mTextureUnit, mTexModMatrix);
+    }
 }
