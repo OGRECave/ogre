166428fae71a5c81ebf1d9f7684e63f66055f79d
Matías N. Goldberg <dark_sylinc@yahoo.com.ar>
2017-03-19 23:55:09 -0300

    Directional lights DO need to disable auto aspect ratio, but non-directional lights need it off.

--- OgreMain/src/Compositor/OgreCompositorShadowNode.cpp
+++ OgreMain/src/Compositor/OgreCompositorShadowNode.cpp
@@ -93,7 +93,6 @@ namespace Ogre
                                                 StringConverter::toString( id ) + " Map " +
                                                 StringConverter::toString( shadowMapIdx ), false );
             shadowMapCamera.camera->setFixedYawAxis( false );
-            shadowMapCamera.camera->setAutoAspectRatio( true );
             shadowMapCamera.minDistance = 0.0f;
             shadowMapCamera.maxDistance = 100000.0f;
 
@@ -464,12 +463,18 @@ namespace Ogre
 
                 // set base
                 if( light->getType() != Light::LT_POINT )
-                {
                     texCamera->setOrientation( light->getParentNode()->_getDerivedOrientation() );
-                }
+                else
+                    texCamera->setOrientation( Quaternion::IDENTITY );
+
                 if( light->getType() != Light::LT_DIRECTIONAL )
                 {
                     texCamera->setPosition( light->getParentNode()->_getDerivedPosition() );
+                    texCamera->setAutoAspectRatio( true );
+                }
+                else
+                {
+                    texCamera->setAutoAspectRatio( false );
                 }
 
                 if( itor->shadowMapTechnique == SHADOWMAP_PSSM )
