3092338d5fa790cd0c7f5b7c3c8fa0e0ce447d6d
0xc0deface <unknown>
2017-04-19 14:23:40 +0100

    Made sure the existing binding method will be used in all compilers except VS2017

--- OgreMain/src/OgreZip.cpp
+++ OgreMain/src/OgreZip.cpp
@@ -291,7 +291,11 @@ namespace Ogre {
             cleanName = tokens[tokens.size() - 1];
         }
 
-        return std::find_if (mFileList.begin(), mFileList.end(), std::bind(FileNameCompare(), std::placeholders::_1, cleanName)) != mFileList.end();
+#       if OGRE_COMPILER == OGRE_COMPILER_MSVC && OGRE_COMP_VER >= 1910
+        return std::find_if( mFileList.begin(), mFileList.end(), std::bind( FileNameCompare(), std::placeholders::_1, cleanName ) ) != mFileList.end();
+#       else
+        return std::find_if( mFileList.begin(), mFileList.end(), std::bind2nd<FileNameCompare>( FileNameCompare(), cleanName ) ) != mFileList.end();
+#       endif
     }
     //---------------------------------------------------------------------
     time_t ZipArchive::getModifiedTime(const String& filename)
