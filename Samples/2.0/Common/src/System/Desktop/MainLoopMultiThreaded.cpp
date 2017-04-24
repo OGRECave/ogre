754f2884472eba6e34fce1a321cd3d1645fe960b
Matías N. Goldberg <dark_sylinc@yahoo.com.ar>
2017-04-23 21:31:05 -0300

    Fixed Windows issues if building with UNICODE.

--- Samples/2.0/Common/src/System/Desktop/MainLoopMultiThreaded.cpp
+++ Samples/2.0/Common/src/System/Desktop/MainLoopMultiThreaded.cpp
@@ -173,8 +173,8 @@ unsigned long renderThread( Ogre::ThreadHandle *threadHandle )
     catch( Ogre::Exception& e )
     {
    #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-        MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!",
-                    MB_OK | MB_ICONERROR | MB_TASKMODAL );
+        MessageBoxA( NULL, e.getFullDescription().c_str(), "An exception has occured!",
+                     MB_OK | MB_ICONERROR | MB_TASKMODAL );
    #else
         std::cerr << "An exception has occured: " <<
                      e.getFullDescription().c_str() << std::endl;
