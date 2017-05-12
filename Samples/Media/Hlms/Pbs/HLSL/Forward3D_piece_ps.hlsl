8b4c38b442b1583850d91d2189c2ec3f3e0b12f6
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2017-05-12 15:30:23 -0300

    Ported OGRE_CONFIG_ENABLE_FINE_LIGHT_MASK_GRANULARITY to D3D11

--- Samples/Media/Hlms/Pbs/HLSL/Forward3D_piece_ps.hlsl
+++ Samples/Media/Hlms/Pbs/HLSL/Forward3D_piece_ps.hlsl
@@ -1,5 +1,11 @@
 @property( hlms_forwardplus )
+@property( hlms_forwardplus_fine_light_mask )
+	@piece( andObjLightMaskCmp )&& ((objLightMask & asuint( lightDiffuse.w )) != 0u)@end
+@end
 @piece( forward3dLighting )
+	@property( hlms_forwardplus_fine_light_mask )
+		uint objLightMask = worldMaterialIdx[inPs.drawId].z;
+	@end
 	@property( hlms_forwardplus == forward3d )
 		float f3dMinDistance	= passBuf.f3dData.x;
 		float f3dInvMaxDistance	= passBuf.f3dData.y;
@@ -59,14 +65,18 @@
 		//Get the light
 		float4 posAndType = f3dLightList.Load( int(idx) );
 
+	@property( !hlms_forwardplus_fine_light_mask )
 		float3 lightDiffuse	= f3dLightList.Load( int(idx + 1u) ).xyz;
+	@end @property( hlms_forwardplus_fine_light_mask )
+		float4 lightDiffuse	= f3dLightList.Load( int(idx + 1u) ).xyw;
+	@end
 		float3 lightSpecular= f3dLightList.Load( int(idx + 2u) ).xyz;
 		float4 attenuation	= f3dLightList.Load( int(idx + 3u) ).xyzw;
 
 		float3 lightDir	= posAndType.xyz - inPs.pos;
 		float fDistance	= length( lightDir );
 
-		[branch]if( fDistance <= attenuation.x )
+		[branch]if( fDistance <= attenuation.x @insertpiece( andObjLightMaskCmp ) )
 		{
 			lightDir *= 1.0 / fDistance;
 			float atten = 1.0 / (0.5 + (attenuation.y + attenuation.z * fDistance) * fDistance );
@@ -75,7 +85,7 @@
 			@end
 
 			//Point light
-			float3 tmpColour = BRDF( lightDir, viewDir, NdotV, lightDiffuse, lightSpecular, material, nNormal @insertpiece( brdfExtraParams ) );
+			float3 tmpColour = BRDF( lightDir, viewDir, NdotV, lightDiffuse.xyz, lightSpecular, material, nNormal @insertpiece( brdfExtraParams ) );
 			finalColour += tmpColour * atten;
 		}
 	}
@@ -93,7 +103,11 @@
 		//Get the light
 		float4 posAndType = f3dLightList.Load( int(idx) );
 
+	@property( !hlms_forwardplus_fine_light_mask )
 		float3 lightDiffuse	= f3dLightList.Load( int(idx + 1u) ).xyz;
+	@end @property( hlms_forwardplus_fine_light_mask )
+		float4 lightDiffuse	= f3dLightList.Load( int(idx + 1u) ).xyw;
+	@end
 		float3 lightSpecular= f3dLightList.Load( int(idx + 2u) ).xyz;
 		float4 attenuation	= f3dLightList.Load( int(idx + 3u) ).xyzw;
 		float3 spotDirection= f3dLightList.Load( int(idx + 4u) ).xyz;
@@ -102,7 +116,7 @@
 		float3 lightDir	= posAndType.xyz - inPs.pos;
 		float fDistance	= length( lightDir );
 
-		[branch]if( fDistance <= attenuation.x )
+		[branch]if( fDistance <= attenuation.x @insertpiece( andObjLightMaskCmp ) )
 		{
 			lightDir *= 1.0 / fDistance;
 			float atten = 1.0 / (0.5 + (attenuation.y + attenuation.z * fDistance) * fDistance );
@@ -123,7 +137,7 @@
 
 			if( spotCosAngle >= spotParams.y )
 			{
-				float3 tmpColour = BRDF( lightDir, viewDir, NdotV, lightDiffuse, lightSpecular, material, nNormal @insertpiece( brdfExtraParams ) );
+				float3 tmpColour = BRDF( lightDir, viewDir, NdotV, lightDiffuse.xyz, lightSpecular, material, nNormal @insertpiece( brdfExtraParams ) );
 				finalColour += tmpColour * atten;
 			}
 		}
