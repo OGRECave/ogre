9f9883c7871e1fe095fdf2dd082a0b115a1890bb
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2017-07-18 00:49:07 -0300

    Fixed one more Metal leak.

--- RenderSystems/Metal/src/OgreMetalDevice.mm
+++ RenderSystems/Metal/src/OgreMetalDevice.mm
@@ -61,12 +61,15 @@ namespace Ogre
     //-------------------------------------------------------------------------
     void MetalDevice::init(void)
     {
-        mDevice = MTLCreateSystemDefaultDevice();
-        mMainCommandQueue = [mDevice newCommandQueue];
-        mCurrentCommandBuffer = [mMainCommandQueue commandBuffer];
-        mBlitEncoder = 0;
-        mComputeEncoder = 0;
-        mRenderEncoder = 0;
+        @autoreleasepool
+        {
+            mDevice = MTLCreateSystemDefaultDevice();
+            mMainCommandQueue = [mDevice newCommandQueue];
+            mCurrentCommandBuffer = [mMainCommandQueue commandBuffer];
+            mBlitEncoder = 0;
+            mComputeEncoder = 0;
+            mRenderEncoder = 0;
+        }
     }
     //-------------------------------------------------------------------------
     void MetalDevice::endBlitEncoder(void)
