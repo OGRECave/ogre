e7884a8205c4c9e63ce9fd3c6b4892a44aff1fe0
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2017-11-04 15:21:39 -0300

    Finally got SMAA right! (after comparing against Reshade 3.0 as ground truth):  * Despite the stencil optimization, edgesTex still needed to be cleared.  * blendTex must PFG_RGBA8_UNORM, not PFG_RGBA8_UNORM_SRGB.  * blendTex alpha must be cleared to 0, not 1!  * SMAA works in sRGB space. Which means when we sample from a PFG_RGBA8_UNORM_SRGB image, the GPU converts from sRGB space to linear for us; and we need to convert it back to sRGB space. We were doing the opposite: from linear to linear^2

--- Samples/Media/2.0/scripts/materials/Tutorial_SMAA/GLSL/SMAA_GLSL.glsl
+++ Samples/Media/2.0/scripts/materials/Tutorial_SMAA/GLSL/SMAA_GLSL.glsl
@@ -43,6 +43,11 @@ float fromSRGB( float x )
 	return (x <= 0.040449907) ? x / 12.92 : pow( (x + 0.055) / 1.055, 2.4 );
 }
 
+vec4 toSRGB( vec4 x )
+{
+	return vec4( toSRGB( x.x ), toSRGB( x.y ), toSRGB( x.z ), x.w );
+}
+
 vec4 fromSRGB( vec4 x )
 {
 	return vec4( fromSRGB( x.x ), fromSRGB( x.y ), fromSRGB( x.z ), x.w );
