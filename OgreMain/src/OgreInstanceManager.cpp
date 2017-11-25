45f497995744aa2b98af81bf74514ba0caab2682
Erik Ogenvik <erik@ogenvik.org>
2017-11-25 13:11:59 +0100

    Fixed issue with indices.
    Depending on the compiler, inserting into a map while reading the size
    of in the same line produces different results. So on some machines the
    indices would be messed up if a mesh with shared vertex data was used.

--- OgreMain/src/OgreInstanceManager.cpp
+++ OgreMain/src/OgreInstanceManager.cpp
@@ -528,7 +528,9 @@ namespace Ogre
             TIndexType index = data[i];
             if (indicesMap.find(index) == indicesMap.end()) 
             {
-                indicesMap[index] = (uint32)(indicesMap.size());
+                //We need to guarantee that the size is read before an entry is added, hence these are on separate lines.
+                uint32 size = (uint32)(indicesMap.size());
+                indicesMap[index] = size;
             }
         }
 
