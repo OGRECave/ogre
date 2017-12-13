22ee375608e863ea3694bc61917355159a4cc97c
Matías N. Goldberg <dark_sylinc@yahoo.com.ar>
2017-12-13 20:30:19 -0300

    Bugfix: HLSL Compilation error in D3D11 when using Blinn Phong

--- Samples/Media/Hlms/Pbs/HLSL/BRDFs_piece_ps.hlsl
+++ Samples/Media/Hlms/Pbs/HLSL/BRDFs_piece_ps.hlsl
@@ -71,7 +71,7 @@ float3 BRDF( float3 lightDir, float3 viewDir, float NdotV, float3 lightDiffuse,
 		//Note: For blinn-phong we use larger denominators otherwise specular blows out of proportion
 		@insertpiece( FresnelType ) Rs = ( fresnelS * blinnPhong ) / max( 4.0 * NdotV * NdotL, 0.75 );
 		//Make diffuse look closer to Default.
-		fresnelD *= mix( 1.0, 1.0 / 1.51, ROUGHNESS );
+		fresnelD *= lerp( 1.0, 1.0 / 1.51, ROUGHNESS );
 	@end @property( legacy_math_brdf )
 		float Rs = blinnPhong;
 		float fresnelD = 1.0;
