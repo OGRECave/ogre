572eb527295094603ced70bfa7ede561da347526
Pavel Rojtberg <rojtberg@gmail.com>
2017-12-17 03:29:17 +0100

    Revert "Prefer std::unordered_xxx rather than versions from tr1,stdext,__gnu_cxx,boost namespaces. Note: tr1 namespace is deprecated in VS2017.5"
    This reverts commit 77107af3330565a164bc2b85605a8c67850f30c2.

--- OgreMain/include/OgreStdHeaders.h
+++ OgreMain/include/OgreStdHeaders.h
@@ -37,10 +37,9 @@
 #include <queue>
 #include <bitset>
 
-#if __cplusplus >= 201103L || OGRE_COMPILER_MIN_VERSION(OGRE_COMPILER_MSVC, 1600) // C++11 or VC++ 2010+
-#       include <unordered_map>
-#       include <unordered_set>
-#elif (OGRE_COMPILER == OGRE_COMPILER_GNUC) && !defined(STLPORT)
+// Note - not in the original STL, but exists in SGI STL and STLport
+// For gcc 4.3 see http://gcc.gnu.org/gcc-4.3/changes.html
+#if (OGRE_COMPILER == OGRE_COMPILER_GNUC) && !defined(STLPORT)
 #   if OGRE_COMP_VER >= 430
 #       include <tr1/unordered_map>
 #       include <tr1/unordered_set> 
