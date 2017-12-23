576573a7a36ddc008db818c8b6e130b2a0219b6b
Pavel Rojtberg <rojtberg@gmail.com>
2017-12-23 18:45:40 +0100

    RTSS: SampleLib_ReflectionMap - drop now obsolete flip for GL

--- Samples/Media/RTShaderLib/GLSL/SampleLib_ReflectionMap.glsl
+++ Samples/Media/RTShaderLib/GLSL/SampleLib_ReflectionMap.glsl
@@ -40,7 +40,6 @@ void SGX_ApplyReflectionMap(in sampler2D maskSampler,
 						    out vec3 vOut)
 {
 	vec3 maskTexel	   = texture2D(maskSampler, maskSamplerTexCoord).xyz;
-	reflectionSamplerTexCoord.y = -reflectionSamplerTexCoord.y; // Hack for gl 
 	vec3 reflectionTexel = texture2D(reflectionSampler, reflectionSamplerTexCoord).xyz;
 	
 	vOut = baseColor + reflectionTexel.xyz*maskTexel.xyz*reflectionPower;
@@ -56,7 +55,6 @@ void SGX_ApplyReflectionMap(in sampler2D maskSampler,
 						    out vec3 vOut)
 {
 	vec3 maskTexel	   = texture2D(maskSampler, maskSamplerTexCoord).xyz;
-	reflectionSamplerTexCoord.z = -reflectionSamplerTexCoord.z; // Hack for gl 
 	vec3 reflectionTexel = textureCube(reflectionSampler, reflectionSamplerTexCoord).xyz;
 	
 	vOut = baseColor + reflectionTexel.xyz*maskTexel.xyz*reflectionPower;
