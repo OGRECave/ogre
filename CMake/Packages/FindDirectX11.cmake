47887f1be33566ca8c69bff027e3258ea199efb5
Eugene Golushkov <e.golushkov@nospam_gmail.com>
2017-12-28 16:17:51 +0200

    FindDirectX11: Try harder to find DX11 SDK as part of Modern SDK, use version passed by CMake or highest available, search in standard and obtaines from registry location, restored support for ARM and ARM64 library archs.

--- CMake/Packages/FindDirectX11.cmake
+++ CMake/Packages/FindDirectX11.cmake
@@ -23,48 +23,46 @@ if(WIN32) # The only platform it makes sense to check for DirectX11 SDK
 		# Windows Phone 8.1 SDK
 		if(WINDOWS_PHONE AND MSVC12)
 			find_path(DirectX11_INCLUDE_DIR NAMES d3d11.h HINTS "C:/Program Files (x86)/Windows Phone Kits/8.1/include" "C:/Program Files/Windows Phone Kits/8.1/include")
-			set(DirectX11_LIBRARY d3d11.lib dxgi.lib dxguid.lib) # in "C:/Program Files (x86)/Windows Phone Kits/8.1/lib/${MSVC_CXX_ARCHITECTURE_ID}/"
 
 		# Windows Phone 8.0 SDK
 		elseif(WINDOWS_PHONE AND MSVC11)
 			find_path(DirectX11_INCLUDE_DIR NAMES d3d11.h HINTS "C:/Program Files (x86)/Windows Phone Kits/8.0/include" "C:/Program Files/Windows Phone Kits/8.0/include")
-			set(DirectX11_LIBRARY d3d11.lib dxgi.lib dxguid.lib) # in "C:/Program Files (x86)/Windows Phone Kits/8.0/lib/${MSVC_CXX_ARCHITECTURE_ID}/"
 
 		endif()
 	elseif( NOT MSVC90 )
 		# Windows 10.x SDK
-		if(CMAKE_CL_64)
-			set(DirectX11_LIBPATH_SUFFIX "x64")
-		else(CMAKE_CL_64)
-			set(DirectX11_LIBPATH_SUFFIX "x86")
-		endif(CMAKE_CL_64)
 		get_filename_component(kit10_dir "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot10]" REALPATH)
-		file(GLOB kit10_list ${kit10_dir}/Include/10.*)
-		foreach(tmp_elem ${kit10_list})
-			if(IS_DIRECTORY ${tmp_elem})
-				get_filename_component(kit10_ver ${tmp_elem} NAME)
-				set(DirectX11_LIB_SEARCH_PATH "${kit10_dir}/Lib/${kit10_ver}/um")
-				find_path(DirectX11_INCLUDE_DIR NAMES d3d11.h HINTS "${tmp_elem}/um")
-				find_library(DirectX11_DXGUID_LIBRARY NAMES dxguid HINTS ${DirectX11_LIB_SEARCH_PATH} PATH_SUFFIXES ${DirectX11_LIBPATH_SUFFIX})
-				find_library(DirectX11_DXGI_LIBRARY NAMES dxgi HINTS ${DirectX11_LIB_SEARCH_PATH} PATH_SUFFIXES ${DirectX11_LIBPATH_SUFFIX})
-				find_library(DirectX11_D3D11_LIBRARY NAMES d3d11 HINTS ${DirectX11_LIB_SEARCH_PATH} PATH_SUFFIXES ${DirectX11_LIBPATH_SUFFIX})
-				set(DirectX11_LIBRARY
-					${DirectX11_D3D11_LIBRARY}
-					${DirectX11_DXGI_LIBRARY}
-					${DirectX11_DXGUID_LIBRARY}
-				)
-			endif()
+		file(GLOB W10SDK_VERSIONS RELATIVE ${kit10_dir}/Include ${kit10_dir}/Include/10.*) 			# enumerate pre-release and not yet known release versions
+		list(APPEND W10SDK_VERSIONS "10.0.10240.0" "10.0.14393.0" "10.0.15063.0" "10.0.16299.0")	# enumerate well known release versions
+		list(REMOVE_DUPLICATES W10SDK_VERSIONS)
+		list(SORT W10SDK_VERSIONS)
+		list(REVERSE W10SDK_VERSIONS)																# sort from high to low
+		if(CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION)
+			list(INSERT W10SDK_VERSIONS 0 ${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION})				# prefer version passed by CMake, if any
+		endif()
+		foreach(W10SDK_VER ${W10SDK_VERSIONS})
+			find_path(DirectX11_INCLUDE_DIR NAMES d3d11.h HINTS "${kit10_dir}/Include/${W10SDK_VER}/um" "C:/Program Files (x86)/Windows Kits/10/Include/${W10SDK_VER}/um" "C:/Program Files/Windows Kits/10/Include/${W10SDK_VER}/um")
 		endforeach()
 		# Windows 8.1 SDK
 		if(NOT DirectX11_INCLUDE_DIR)
 			find_path(DirectX11_INCLUDE_DIR NAMES d3d11.h HINTS "C:/Program Files (x86)/Windows Kits/8.1/include/um" "C:/Program Files/Windows Kits/8.1/include/um")
-			set(DirectX11_LIBRARY d3d11.lib dxgi.lib dxguid.lib) # in "C:/Program Files (x86)/Windows Kits/8.1/lib/winv6.3/um/${MSVC_CXX_ARCHITECTURE_ID}/"
 		endif()
 		# Windows 8.0 SDK
 		if(NOT DirectX11_INCLUDE_DIR)
 			find_path(DirectX11_INCLUDE_DIR NAMES d3d11.h HINTS "C:/Program Files (x86)/Windows Kits/8.0/include/um" "C:/Program Files/Windows Kits/8.0/include/um")
-			set(DirectX11_LIBRARY d3d11.lib dxgi.lib dxguid.lib) # in "C:/Program Files (x86)/Windows Kits/8.0/lib/win8/um/${MSVC_CXX_ARCHITECTURE_ID}/"
 		endif()
+	endif()
+
+	if(DirectX11_INCLUDE_DIR)
+		# No need to specify full path to libraries, proper version would be found as part of SDK, in one of the following places
+		# "C:/Program Files (x86)/Windows Kits/10/lib/${W10SDK_VER}/um/${MSVC_CXX_ARCHITECTURE_ID}/"
+		# "C:/Program Files (x86)/Windows Kits/8.1/lib/winv6.3/um/${MSVC_CXX_ARCHITECTURE_ID}/"
+		# "C:/Program Files (x86)/Windows Kits/8.0/lib/win8/um/${MSVC_CXX_ARCHITECTURE_ID}/"
+		# "C:/Program Files (x86)/Windows Phone Kits/8.1/lib/${MSVC_CXX_ARCHITECTURE_ID}/"
+		# "C:/Program Files (x86)/Windows Phone Kits/8.0/lib/${MSVC_CXX_ARCHITECTURE_ID}/"
+		set(DirectX11_LIBRARY d3d11.lib dxgi.lib dxguid.lib) 
+		# but it is usefull to output selected version to the log
+		message(STATUS "Found DirectX11 headers: ${DirectX11_INCLUDE_DIR}")
 	endif()
 
 	# Legacy Direct X SDK
