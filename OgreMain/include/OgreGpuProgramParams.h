d3aa314d119e6dc7caf16ff74cf38f36ec79ad6b
Pavel Rojtberg <rojtberg@gmail.com>
2018-02-13 15:51:25 +0100

    MSVC: fix narrowing conversion warnings

--- OgreMain/include/OgreGpuProgramParams.h
+++ OgreMain/include/OgreGpuProgramParams.h
@@ -1501,8 +1501,8 @@ namespace Ogre {
         typedef vector<GpuSharedParametersUsage>::type GpuSharedParamUsageList;
 
         // Map that store subroutines associated with slots
-        typedef OGRE_HashMap<unsigned int, String> SubroutineMap;
-        typedef OGRE_HashMap<unsigned int, String>::const_iterator SubroutineIterator;
+        typedef OGRE_HashMap<size_t, String> SubroutineMap;
+        typedef OGRE_HashMap<size_t, String>::const_iterator SubroutineIterator;
 
     protected:
         SubroutineMap mSubroutineMap;
