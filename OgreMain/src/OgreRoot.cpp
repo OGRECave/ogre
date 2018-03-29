34848dfcd71e45f2de3f46415ddcd93787c7e571
Pavel Rojtberg <rojtberg@gmail.com>
2018-03-29 23:23:01 +0200

    Main: Root - skip FrameListeners that are removed during event dispatch
    allows listeners to destroy other listeners

--- OgreMain/src/OgreRoot.cpp
+++ OgreMain/src/OgreRoot.cpp
@@ -853,6 +853,9 @@ namespace Ogre {
         // Tell all listeners
         for (set<FrameListener*>::type::iterator i = mFrameListeners.begin(); i != mFrameListeners.end(); ++i)
         {
+            if(mRemovedFrameListeners.find(*i) != mRemovedFrameListeners.end())
+                continue;
+
             if (!(*i)->frameStarted(evt))
                 return false;
         }
@@ -869,6 +872,9 @@ namespace Ogre {
         // Tell all listeners
         for (set<FrameListener*>::type::iterator i = mFrameListeners.begin(); i != mFrameListeners.end(); ++i)
         {
+            if(mRemovedFrameListeners.find(*i) != mRemovedFrameListeners.end())
+                continue;
+
             if (!(*i)->frameRenderingQueued(evt))
                 return false;
         }
@@ -884,6 +890,9 @@ namespace Ogre {
         bool ret = true;
         for (set<FrameListener*>::type::iterator i = mFrameListeners.begin(); i != mFrameListeners.end(); ++i)
         {
+            if(mRemovedFrameListeners.find(*i) != mRemovedFrameListeners.end())
+                continue;
+
             if (!(*i)->frameEnded(evt))
             {
                 ret = false;
