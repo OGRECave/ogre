246214371c8518de5a95f5a491c77ca837da0b0b
marko213 <markokene312@gmail.com>
2018-03-31 17:06:18 +0300

    Revamp SceneLoaderManager * Register name, supported extensions and the SceneLoader * More errors and warnings * Loading is now possible with either DataStreamPtr or filename, without needing the SceneLoader pointer itself

--- OgreMain/include/OgreSceneLoaderManager.h
+++ OgreMain/include/OgreSceneLoaderManager.h
@@ -21,19 +21,27 @@ namespace Ogre {
         virtual ~SceneLoaderManager();
         /** Register a new SceneLoader
             @param name The name for the SceneLoader for lookup.
+            @param ext A StringVector containing the supported extensions for the
+                SceneLoader being registered.
             @param sl Pointer to the SceneLoader instance.
         */
-        void registerSceneLoader(const String& name, SceneLoader *sl);
+        void registerSceneLoader(const String& name, StringVectorPtr ext, SceneLoader *sl);
         /** Load a scene from a SceneLoader
-        @remarks This is usually equivalent to calling
-            SceneLoader::load(stream, groupName, rootNode)
-        @param loaderName The name of the SceneLoader that will be used for loading.
-        @param stream Weak reference to a data stream which is the source of the scene
+        @param filename The name (and path) of the file to be loaded.
+            This is also used to determine the SceneLoader to use by the file extension.
         @param groupName The name of a resource group which should be used if any resources
             are created during the parse of this script.
-        @param rootNode The root node for the scene being created
+        @param rootNode The root node for the scene being loaded.
         */
-        void load(const String& loaderName, DataStreamPtr& stream, const String& groupName, SceneNode *rootNode);
+        void load(const String& filename, const String& groupName, SceneNode *rootNode);
+        /** Load a scene from a SceneLoader
+        @param stream Weak reference to a data stream which is the source of the scene.
+            This is also used to determine the SceneLoader to use by the file extension.
+        @param groupName The name of a resource group which should be used if any resources
+            are created during the parse of this script.
+        @param rootNode The root node for the scene being loaded.
+        */
+        void load(DataStreamPtr& stream, const String& groupName, SceneNode *rootNode);
         
         /// @copydoc Singleton::getSingleton()
         static SceneLoaderManager& getSingleton(void);
@@ -41,8 +49,15 @@ namespace Ogre {
         static SceneLoaderManager* getSingletonPtr(void);
         
     protected:
-        /// Map from scene loader names to SceneLoaders
-        typedef map<String, SceneLoader*>::type SceneLoaderMap;
+        /// Struct for storing data about SceneLoaders internally
+        struct SceneLoaderInfo
+        {
+            SceneLoaderInfo(SceneLoader *l, StringVectorPtr ext);
+            SceneLoader* loader;
+            StringVectorPtr supportedExt;
+        };
+        /// Map from scene loader names to SceneLoaderInfo
+        typedef map<String, SceneLoaderInfo>::type SceneLoaderMap;
         SceneLoaderMap mSceneLoaders;
     };
 }
--- OgreMain/src/OgreSceneLoaderManager.cpp
+++ OgreMain/src/OgreSceneLoaderManager.cpp
@@ -1,55 +1,124 @@
 #include "OgreSceneLoaderManager.h"
 #include "OgreException.h"
+#include "OgreStringVector.h"
+#include "OgreRoot.h"
+#include "OgreLogManager.h"
 
 namespace Ogre {
+
+template<> SceneLoaderManager* Singleton<SceneLoaderManager>::msSingleton = 0;
+
+SceneLoaderManager *SceneLoaderManager::getSingletonPtr(void)
+{
+    return msSingleton;
+}
+
+SceneLoaderManager& SceneLoaderManager::getSingleton(void)
+{
+    assert(msSingleton);
+    return *msSingleton;
+}
+
+SceneLoaderManager::SceneLoaderManager()
+{
+
+}
+
+SceneLoaderManager::~SceneLoaderManager()
+{
+
+}
+
+SceneLoaderManager::SceneLoaderInfo::SceneLoaderInfo(SceneLoader *l, StringVectorPtr ext)
+{
+    loader = l;
+    supportedExt = ext;
+}
+
+void SceneLoaderManager::registerSceneLoader(const String& name, StringVectorPtr ext, SceneLoader *sl)
+{
+    LogManager::getSingleton().logMessage("Registering SceneLoader " + name);
+    if(mSceneLoaders.find(name) != mSceneLoaders.end())
+        OGRE_EXCEPT(Exception::ERR_DUPLICATE_ITEM,
+            String("a SceneLoader with the name \"") +
+            name + String("\" already exists"),
+            "SceneLoaderManager::registerSceneLoader");
     
-    template<> SceneLoaderManager* Singleton<SceneLoaderManager>::msSingleton = 0;
-    
-    SceneLoaderManager *SceneLoaderManager::getSingletonPtr(void)
+    unsigned int shadow = 0;
+    for(StringVector::iterator s = (*ext).begin(); s != (*ext).end(); ++s)
     {
-        return msSingleton;
+        bool skipCurrent = false;
+        for(SceneLoaderMap::iterator l = mSceneLoaders.begin();
+            l != mSceneLoaders.end() && !skipCurrent; ++l)
+        {
+            StringVectorPtr ep = l->second.supportedExt;
+            for(StringVector::iterator e = ep->begin(); e != ep->end(); ++e)
+            {
+                if((*s).compare(*e) == 0)
+                {
+                    LogManager::getSingleton().logWarning("support for file extension \"" +
+                        (*s) + "\" has already been registered by the SceneLoader " +
+                        l->first + ". This extension will be shadowed");
+                    ++shadow;
+                    skipCurrent = true;
+                    break;
+                }
+            }
+        }
     }
     
-    SceneLoaderManager& SceneLoaderManager::getSingleton(void)
+    if(shadow)
+        LogManager::getSingleton().logWarning("registering with " + 
+            StringConverter::toString(shadow) + " shadowed extension(s) out of " + 
+            StringConverter::toString((*ext).size()));
+        
+        
+    
+    mSceneLoaders.insert(std::pair<String, SceneLoaderInfo>(name, SceneLoaderInfo(sl, ext)));
+}
+
+void SceneLoaderManager::load(const String& filename, const String& groupName, SceneNode *rootNode)
+{
+    DataStreamPtr stream(Root::openFileStream(filename, groupName));
+    load(stream, groupName, rootNode);
+}
+
+void SceneLoaderManager::load(DataStreamPtr& stream, const String& groupName, SceneNode *rootNode)
+{
+    String ext = "";
+    String filename = stream -> getName();
+    for(unsigned int i = filename.length() - 2; i > 0; --i)
     {
-        assert(msSingleton);
-        return *msSingleton;
+        if(filename[i] == '.')
+        {
+            ext = filename.substr(i, filename.length() - i);
+            break;
+        }
     }
     
-    SceneLoaderManager::SceneLoaderManager()
+    if (!ext.length())
+        OGRE_EXCEPT(Exception::ERR_INVALIDPARAMS,
+            String("could not get file extension from filename \"") + 
+            filename + String("\""), "SceneLoaderManager::load");
+    
+    for(SceneLoaderMap::iterator i = mSceneLoaders.begin();
+        i != mSceneLoaders.end(); ++i)
     {
-        
+        StringVectorPtr ep = i->second.supportedExt;
+        for(StringVector::iterator s = ep->begin(); s != ep -> end(); ++s)
+        {
+            if(!ext.compare(*s))
+            {
+                i->second.loader->load (stream, groupName, rootNode);
+                return;
+            }
+        }
     }
     
-    SceneLoaderManager::~SceneLoaderManager()
-    {
-        
-    }
-    
-    void SceneLoaderManager::registerSceneLoader(const String& name, SceneLoader *sl)
-    {
-        if(mSceneLoaders.find(name) != mSceneLoaders.end())
-            OGRE_EXCEPT(Exception::ERR_DUPLICATE_ITEM,
-                String("SceneLoader with name \"") +
-                name + String("\" already exists"),
-                "SceneLoaderManager::registerSceneLoader");
-        
-        mSceneLoaders.insert(std::pair<String, SceneLoader*>(name, sl));
-    }
-    
-    void SceneLoaderManager::load(const String& loaderName, DataStreamPtr& stream, const String& groupName, SceneNode *rootNode)
-    {
-        auto i = mSceneLoaders.find(loaderName);
-        
-        if(i == mSceneLoaders.end())
-            OGRE_EXCEPT(Exception::ERR_ITEM_NOT_FOUND,
-			  String("SceneLoader with name \"") +
-                loaderName + String("\" was not found"),
-                "SceneLoaderManager::load");
-        
-        SceneLoader *sl = i -> second;
-        
-        sl -> load (stream, groupName, rootNode);
-    }
-    
+    OGRE_EXCEPT(Exception::ERR_ITEM_NOT_FOUND,
+        String("no SceneLoader for extension \"") +
+        ext + String("\" was found"),
+        "SceneLoaderManager::load");
+}
+
 }
