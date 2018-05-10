c78ff16957f53e7a6abe5e7d61d92f5d019f8345
Martin Vejdarski <extrulm@gmail.com>
2018-05-10 11:45:04 +0100

    Added support for loading macos dynamic libraries using the run-path

--- OgreMain/src/OSX/macUtils.mm
+++ OgreMain/src/OSX/macUtils.mm
@@ -124,7 +124,13 @@ namespace Ogre {
         if(name[0]!='/')
             fullPath = macPluginPath()+"/"+fullPath;
 
-        return dlopen(fullPath.c_str(), RTLD_LAZY | RTLD_GLOBAL);
+        auto lib = dlopen(fullPath.c_str(), RTLD_LAZY | RTLD_GLOBAL);
+        if(!lib){
+            // Try loading it by using the run-path (@rpath)
+            lib = dlopen(name, RTLD_LAZY | RTLD_GLOBAL);
+        }
+        
+        return lib;
     }
 	
     String macBundlePath()
