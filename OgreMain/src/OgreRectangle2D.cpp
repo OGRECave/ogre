7d1a4ec20b3e1f897349978a42a87e7fac95fa81
Pavel Rojtberg <rojtberg@gmail.com>
2018-05-27 01:47:36 +0200

    Main: Rectangle2D::setUVs - assert instead of silently doing nothing

--- OgreMain/src/OgreRectangle2D.cpp
+++ OgreMain/src/OgreRectangle2D.cpp
@@ -192,8 +192,8 @@ namespace Ogre {
     void Rectangle2D::setUVs( const Ogre::Vector2 &topLeft, const Ogre::Vector2 &bottomLeft,
                                 const Ogre::Vector2 &topRight, const Ogre::Vector2 &bottomRight)
     {
-        if( mRenderOp.vertexData->vertexDeclaration->getElementCount() <= TEXCOORD_BINDING )
-            return; //Vertex data wasn't built with UV buffer
+        OgreAssert(mRenderOp.vertexData->vertexDeclaration->getElementCount() > TEXCOORD_BINDING,
+                   "Vertex data wasn't built with UV buffer");
 
         HardwareVertexBufferSharedPtr vbuf = 
             mRenderOp.vertexData->vertexBufferBinding->getBuffer(TEXCOORD_BINDING);
