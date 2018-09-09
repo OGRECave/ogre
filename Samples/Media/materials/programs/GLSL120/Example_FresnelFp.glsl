3de7422790128289b7080d71e5a5e1502a4b25d5
Pavel Rojtberg <rojtberg@gmail.com>
2018-09-09 20:13:15 +0200

    Samples: clean up Fresnel materials - only require GLSL120
    and drop unused asm shader

--- Samples/Media/materials/programs/GLSL120/Example_FresnelFp.glsl
+++ Samples/Media/materials/programs/GLSL120/Example_FresnelFp.glsl
@@ -1,4 +1,4 @@
-#version 150
+#version 120
 
 uniform vec4 tintColour;
 uniform float noiseScale;
@@ -9,12 +9,10 @@ uniform sampler2D noiseMap;
 uniform sampler2D reflectMap;
 uniform sampler2D refractMap;
 
-in vec3 noiseCoord;
-in vec4 projectionCoord;
-in vec3 eyeDir;
-in vec3 oNormal;
-
-out vec4 fragColour;
+varying vec3 noiseCoord;
+varying vec4 projectionCoord;
+varying vec3 eyeDir;
+varying vec3 oNormal;
 
 // Fragment program for distorting a texture using a 3D noise texture
 void main()
@@ -23,7 +21,7 @@ void main()
 	vec2 final = projectionCoord.xy / projectionCoord.w;
 
 	// Noise
-	vec3 noiseNormal = (texture(noiseMap, (noiseCoord.xy / 5.0)).rgb - 0.5).rbg * noiseScale;
+	vec3 noiseNormal = (texture2D(noiseMap, (noiseCoord.xy / 5.0)).rgb - 0.5).rbg * noiseScale;
 	final += noiseNormal.xz;
 
 	// Fresnel
@@ -31,9 +29,9 @@ void main()
 	float fresnel = fresnelBias + fresnelScale * pow(1.0 + dot(eyeDir, oNormal), fresnelPower);
 
 	// Reflection / refraction
-	vec4 reflectionColour = texture(reflectMap, final);
-	vec4 refractionColour = texture(refractMap, final) + tintColour;
+	vec4 reflectionColour = texture2D(reflectMap, final);
+	vec4 refractionColour = texture2D(refractMap, final) + tintColour;
 
 	// Final colour
-	fragColour = mix(refractionColour, reflectionColour, fresnel);
+	gl_FragColor = mix(refractionColour, reflectionColour, fresnel);
 }
