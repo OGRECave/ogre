OGRECave/ogre

commit 49a6ebfbf0ed2328eb0d0668a5ee8c4b2a88a57f

Main: unify Texture::mSurfaceList across RenderSystems

OgreMain/include/OgreTexture.h
@@ -389,7 +389,7 @@ namespace Ogre {
             @remarks The buffer is invalidated when the resource is unloaded or destroyed.
             Do not use it after the lifetime of the containing texture.
         */
-        virtual HardwarePixelBufferSharedPtr getBuffer(size_t face=0, size_t mipmap=0) = 0;
+        virtual const HardwarePixelBufferSharedPtr& getBuffer(size_t face=0, size_t mipmap=0);
 
 
         /** Populate an Image with the contents of this texture. 
@@ -451,6 +451,10 @@ namespace Ogre {
         bool mTreatLuminanceAsAlpha;
 
         bool mInternalResourcesCreated;
+
+        /// Vector of pointers to subsurfaces
+        typedef std::vector<HardwarePixelBufferSharedPtr> SurfaceList;
+        SurfaceList mSurfaceList;
 
         /// @copydoc Resource::calculateSize
         size_t calculateSize(void) const;

OgreMain/src/OgreTexture.cpp
@@ -244,14 +244,14 @@ namespace Ogre {
         
         if (TextureManager::getSingleton().getVerbose()) {
             // Say what we're doing
-            StringStream str;
+            Log::Stream str = LogManager::getSingleton().stream();
             str << "Texture: " << mName << ": Loading " << faces << " faces"
-                << "(" << PixelUtil::getFormatName(images[0]->getFormat()) << "," <<
-                images[0]->getWidth() << "x" << images[0]->getHeight() << "x" << images[0]->getDepth() <<
-                ")";
+                << "(" << PixelUtil::getFormatName(images[0]->getFormat()) << ","
+                << images[0]->getWidth() << "x" << images[0]->getHeight() << "x"
+                << images[0]->getDepth() << ")";
             if (!(mMipmapsHardwareGenerated && mNumMipmaps == 0))
             {
-                str << " with " << static_cast<int>(mNumMipmaps);
+                str << " with " << mNumMipmaps;
                 if(mUsage & TU_AUTOMIPMAP)
                 {
                     if (mMipmapsHardwareGenerated)
@@ -269,15 +269,10 @@ namespace Ogre {
                     str << " from Image.";
             }
 
-            // Scoped
-            {
-                // Print data about first destination surface
-                HardwarePixelBufferSharedPtr buf = getBuffer(0, 0); 
-                str << " Internal format is " << PixelUtil::getFormatName(buf->getFormat()) << 
-                "," << buf->getWidth() << "x" << buf->getHeight() << "x" << buf->getDepth() << ".";
-            }
-            LogManager::getSingleton().logMessage( 
-                    LML_NORMAL, str.str());
+            // Print data about first destination surface
+            const auto& buf = getBuffer(0, 0);
+            str << " Internal format is " << PixelUtil::getFormatName(buf->getFormat()) << ","
+                << buf->getWidth() << "x" << buf->getHeight() << "x" << buf->getDepth() << ".";
         }
         
         // Main loading loop
@@ -344,6 +339,7 @@ namespace Ogre {
     {
         if (mInternalResourcesCreated)
         {
+            mSurfaceList.clear();
             freeInternalResourcesImpl();
             mInternalResourcesCreated = false;
         }
@@ -420,6 +416,23 @@ namespace Ogre {
         return BLANKSTRING;
 
     }
+    const HardwarePixelBufferSharedPtr& Texture::getBuffer(size_t face, size_t mipmap)
+    {
+        if (face >= getNumFaces())
+        {
+            OGRE_EXCEPT(Exception::ERR_INVALIDPARAMS, "Face index out of range", "Texture::getBuffer");
+        }
+
+        if (mipmap > mNumMipmaps)
+        {
+            OGRE_EXCEPT(Exception::ERR_INVALIDPARAMS, "Mipmap index out of range", "Texture::getBuffer");
+        }
+
+        unsigned long idx = face * (mNumMipmaps + 1) + mipmap;
+        assert(idx < mSurfaceList.size());
+        return mSurfaceList[idx];
+    }
+
     //---------------------------------------------------------------------
     void Texture::convertToImage(Image& destImage, bool includeMipMaps)
     {

RenderSystems/Direct3D9/include/OgreD3D9Texture.h
@@ -61,10 +61,6 @@ namespace Ogre {
         /// Map between device to texture resources.
         DeviceToTextureResourcesMap mMapDeviceToTextureResources;
 
-
-        /// Vector of pointers to subsurfaces
-        typedef std::vector<HardwarePixelBufferSharedPtr> SurfaceList;
-        SurfaceList mSurfaceList;
         /// cube texture individual face names
         String                          mCubeFaceNames[6];  
         /// The memory pool being used
@@ -184,7 +180,7 @@ namespace Ogre {
 
 
         /// @copydoc Texture::getBuffer
-        HardwarePixelBufferSharedPtr getBuffer(size_t face, size_t mipmap);
+        const HardwarePixelBufferSharedPtr& getBuffer(size_t face, size_t mipmap);
         
         /// retrieves a pointer to the actual texture
         IDirect3DBaseTexture9 *getTexture();        

RenderSystems/Direct3D9/src/OgreD3D9Texture.cpp
@@ -80,8 +80,7 @@ namespace Ogre
             OGRE_DELETE_T(textureResource, TextureResources, MEMCATEGORY_RENDERSYS);
             ++it;
         }       
-        mMapDeviceToTextureResources.clear();
-        mSurfaceList.clear();       
+        mMapDeviceToTextureResources.clear();      
     }
     /****************************************************************************************/
     void D3D9Texture::copyToTexture(TexturePtr& target)
@@ -1822,16 +1821,8 @@ namespace Ogre
     }
     #undef GETLEVEL
     /****************************************************************************************/
-    HardwarePixelBufferSharedPtr D3D9Texture::getBuffer(size_t face, size_t mipmap) 
+    const HardwarePixelBufferSharedPtr& D3D9Texture::getBuffer(size_t face, size_t mipmap)
     {
-        if(face >= getNumFaces())
-            OGRE_EXCEPT(Exception::ERR_INVALIDPARAMS, "A three dimensional cube has six faces",
-                    "D3D9Texture::getBuffer");
-        if(mipmap > mNumMipmaps)
-            OGRE_EXCEPT(Exception::ERR_INVALIDPARAMS, "Mipmap index out of range",
-                    "D3D9Texture::getBuffer");
-        size_t idx = face*(mNumMipmaps+1) + mipmap;
-
         IDirect3DDevice9* d3d9Device = D3D9RenderSystem::getActiveD3D9Device();
         TextureResources* textureResources = getTextureResources(d3d9Device);
         if (textureResources == NULL || textureResources->pBaseTex == NULL)
@@ -1839,10 +1830,9 @@ namespace Ogre
             createTextureResources(d3d9Device);
             textureResources = getTextureResources(d3d9Device);         
         }
-    
         assert(textureResources != NULL);
-        assert(idx < mSurfaceList.size());
-        return mSurfaceList[idx];
+
+        return Texture::getBuffer(face, mipmap);
     }
     //---------------------------------------------------------------------
     bool D3D9Texture::useDefaultPool()

RenderSystems/Direct3D11/include/OgreD3D11Texture.h
@@ -60,10 +60,6 @@ namespace Ogre {
 		/// overridden from Texture
 		void loadImage(const Image &img);
 
-
-		/// @copydoc Texture::getBuffer
-		HardwarePixelBufferSharedPtr getBuffer(size_t face, size_t mipmap);
-
 		ID3D11Resource *getTextureResource() { assert(mpTex); return mpTex.Get(); }
 		/// retrieves a pointer to the actual texture
 		ID3D11ShaderResourceView *getTexture() { assert(mpShaderResourceView); return mpShaderResourceView.Get(); }
@@ -165,10 +161,6 @@ namespace Ogre {
 
         D3D11_SHADER_RESOURCE_VIEW_DESC mSRVDesc;
         bool mAutoMipMapGeneration;
-
-        /// Vector of pointers to subsurfaces
-        typedef std::vector<HardwarePixelBufferSharedPtr> SurfaceList;
-        SurfaceList                     mSurfaceList;
     };
 
     /// RenderTexture implementation for D3D11

RenderSystems/Direct3D11/src/OgreD3D11Texture.cpp
@@ -166,7 +166,6 @@ namespace Ogre
     //---------------------------------------------------------------------
     void D3D11Texture::freeInternalResourcesImpl()
     {
-        mSurfaceList.clear();
         mpTex.Reset();
         mpShaderResourceView.Reset();
         mp1DTex.Reset();
@@ -740,19 +739,6 @@ namespace Ogre
                 if(depth > 1 && getTextureType() != TEX_TYPE_2D_ARRAY) depth /= 2;
             }
         }
-    }
-    //---------------------------------------------------------------------
-    HardwarePixelBufferSharedPtr D3D11Texture::getBuffer(size_t face, size_t mipmap) 
-    {
-        if(face >= getNumFaces())
-            OGRE_EXCEPT(Exception::ERR_INVALIDPARAMS, "A three dimensional cube has six faces",
-            "D3D11Texture::getBuffer");
-        if(mipmap > mNumMipmaps)
-            OGRE_EXCEPT(Exception::ERR_INVALIDPARAMS, "Mipmap index out of range",
-            "D3D11Texture::getBuffer");
-        size_t idx = face*(mNumMipmaps+1) + mipmap;
-        assert(idx < mSurfaceList.size());
-        return mSurfaceList[idx];
     }
     //---------------------------------------------------------------------
     void D3D11Texture::prepareImpl( void )

RenderSystems/GL/src/OgreGLTexture.cpp
@@ -289,7 +289,6 @@ namespace Ogre {
     
     void GLTexture::freeInternalResourcesImpl()
     {
-        mSurfaceList.clear();
         if (GLStateCacheManager* stateCacheManager = mRenderSystem->_getStateCacheManager())
         {
             glDeleteTextures(1, &mTextureID);

RenderSystems/GL3Plus/src/OgreGL3PlusTexture.cpp
@@ -385,7 +385,6 @@ namespace Ogre {
 
     void GL3PlusTexture::freeInternalResourcesImpl()
     {
-        mSurfaceList.clear();
         if (GL3PlusStateCacheManager* stateCacheManager = mRenderSystem->_getStateCacheManager())
         {
             OGRE_CHECK_GL_ERROR(glDeleteTextures(1, &mTextureID));

commit a5c791be520aab417fd04d23e14bb39276e01caa

Main: revert API breakage introduced in 49a6ebfbf0ed2328eb0d0668a5ee8c

OgreMain/include/OgreTexture.h
@@ -389,7 +389,7 @@ namespace Ogre {
             @remarks The buffer is invalidated when the resource is unloaded or destroyed.
             Do not use it after the lifetime of the containing texture.
         */
-        virtual const HardwarePixelBufferSharedPtr& getBuffer(size_t face=0, size_t mipmap=0);
+        virtual HardwarePixelBufferSharedPtr getBuffer(size_t face=0, size_t mipmap=0);
 
 
         /** Populate an Image with the contents of this texture. 

OgreMain/src/OgreTexture.cpp
@@ -416,7 +416,7 @@ namespace Ogre {
         return BLANKSTRING;
 
     }
-    const HardwarePixelBufferSharedPtr& Texture::getBuffer(size_t face, size_t mipmap)
+    HardwarePixelBufferSharedPtr Texture::getBuffer(size_t face, size_t mipmap)
     {
         if (face >= getNumFaces())
         {

RenderSystems/Direct3D9/include/OgreD3D9Texture.h
@@ -180,7 +180,7 @@ namespace Ogre {
 
 
         /// @copydoc Texture::getBuffer
-        const HardwarePixelBufferSharedPtr& getBuffer(size_t face, size_t mipmap);
+        HardwarePixelBufferSharedPtr getBuffer(size_t face, size_t mipmap);
         
         /// retrieves a pointer to the actual texture
         IDirect3DBaseTexture9 *getTexture();        

RenderSystems/Direct3D9/src/OgreD3D9Texture.cpp
@@ -1821,7 +1821,7 @@ namespace Ogre
     }
     #undef GETLEVEL
     /****************************************************************************************/
-    const HardwarePixelBufferSharedPtr& D3D9Texture::getBuffer(size_t face, size_t mipmap)
+    HardwarePixelBufferSharedPtr D3D9Texture::getBuffer(size_t face, size_t mipmap)
     {
         IDirect3DDevice9* d3d9Device = D3D9RenderSystem::getActiveD3D9Device();
         TextureResources* textureResources = getTextureResources(d3d9Device);