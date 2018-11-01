Tomarron/ogre-1

add 1.10 api docs

api/1.10/class_ogre_1_1_area_emitter_1_1_cmd_height.js
@@ -1,0 +1,5 @@
+var class_ogre_1_1_area_emitter_1_1_cmd_height =
+[
+    [ "doGet", "class_ogre_1_1_area_emitter_1_1_cmd_height.html#ade59660c5809e8ea13bf08e416e66989", null ],
+    [ "doSet", "class_ogre_1_1_area_emitter_1_1_cmd_height.html#a018b4c9a19bf91d5c37257434e8ba8b9", null ]
+];

api/1.10/class_ogre_1_1_d3_d9_h_l_s_l_program_factory.js
@@ -1,0 +1,8 @@
+var class_ogre_1_1_d3_d9_h_l_s_l_program_factory =
+[
+    [ "D3D9HLSLProgramFactory", "class_ogre_1_1_d3_d9_h_l_s_l_program_factory.html#a4b3edf46c6983c44d273b98fb83a2cd4", null ],
+    [ "~D3D9HLSLProgramFactory", "class_ogre_1_1_d3_d9_h_l_s_l_program_factory.html#a5583d0dd25327a47f9eaa2793024677f", null ],
+    [ "create", "class_ogre_1_1_d3_d9_h_l_s_l_program_factory.html#a3123fef6fb8a5906eae596a224d8c383", null ],
+    [ "destroy", "class_ogre_1_1_d3_d9_h_l_s_l_program_factory.html#a97c41f530e862d8d66a8aafd8084202a", null ],
+    [ "getLanguage", "class_ogre_1_1_d3_d9_h_l_s_l_program_factory.html#aa0ac4b7e93476d99839665191f15a895", null ]
+];

api/1.10/class_ogre_1_1_d3_d9_video_mode.js
@@ -1,0 +1,15 @@
+var class_ogre_1_1_d3_d9_video_mode =
+[
+    [ "D3D9VideoMode", "class_ogre_1_1_d3_d9_video_mode.html#a34a77a0c52fb29311ec6181ac8bee754", null ],
+    [ "D3D9VideoMode", "class_ogre_1_1_d3_d9_video_mode.html#a3092cd96e44807a629e3ac45f05c683c", null ],
+    [ "D3D9VideoMode", "class_ogre_1_1_d3_d9_video_mode.html#ac4d2913efac422ca4a93ab0771373431", null ],
+    [ "~D3D9VideoMode", "class_ogre_1_1_d3_d9_video_mode.html#af819f89d3ecd9236d1c0f9f91a71d0ba", null ],
+    [ "getColourDepth", "class_ogre_1_1_d3_d9_video_mode.html#a1fbeacf84a9adaf51949067616d25fee", null ],
+    [ "getDescription", "class_ogre_1_1_d3_d9_video_mode.html#a31162f0c7366d0a913f009be3eb774d5", null ],
+    [ "getDisplayMode", "class_ogre_1_1_d3_d9_video_mode.html#a7fecdfa941b7519e79131acf5b4dfdf4", null ],
+    [ "getFormat", "class_ogre_1_1_d3_d9_video_mode.html#aebcf36ee2a94af840d455519d7b4e936", null ],
+    [ "getHeight", "class_ogre_1_1_d3_d9_video_mode.html#ace88c7d7936ff8e3d834bbb01f60ca07", null ],
+    [ "getRefreshRate", "class_ogre_1_1_d3_d9_video_mode.html#a723b0e8dbcf12b35b0b53cb216f9aee8", null ],
+    [ "getWidth", "class_ogre_1_1_d3_d9_video_mode.html#a65ffcd6f95dfb7f7d1e3132179e1941d", null ],
+    [ "increaseRefreshRate", "class_ogre_1_1_d3_d9_video_mode.html#a84bc6e2cc34253b99afc560176038b4c", null ]
+];

api/1.10/class_ogre_1_1_file_system_archive.js
@@ -1,0 +1,23 @@
+var class_ogre_1_1_file_system_archive =
+[
+    [ "FileSystemArchive", "class_ogre_1_1_file_system_archive.html#ad5ae519a6d13d89a7d4dc66ea8f64875", null ],
+    [ "~FileSystemArchive", "class_ogre_1_1_file_system_archive.html#ad89960cb80cf490a23cab7b909ffa092", null ],
+    [ "create", "class_ogre_1_1_file_system_archive.html#a886e95b80604575912e8dadecdb47ca5", null ],
+    [ "exists", "class_ogre_1_1_file_system_archive.html#afa703d83c696b6b78dc7efe8114e468e", null ],
+    [ "find", "class_ogre_1_1_file_system_archive.html#a47dc8d47bb6a6738a85f62f816bfd7f8", null ],
+    [ "findFileInfo", "class_ogre_1_1_file_system_archive.html#a2d9eb31d4d17fdb4b35c5ad1c739f1be", null ],
+    [ "getIgnoreHidden", "class_ogre_1_1_file_system_archive.html#a79c4957064dc9858d3abdc87ebadfe7c", null ],
+    [ "getModifiedTime", "class_ogre_1_1_file_system_archive.html#a19a46cfd6ea079bca9538e862042a891", null ],
+    [ "getName", "class_ogre_1_1_file_system_archive.html#aa4860f997861f44b61a436d55cdcc66b", null ],
+    [ "getType", "class_ogre_1_1_file_system_archive.html#ab0fe79a8deba20a1cc1301f68805b621", null ],
+    [ "isCaseSensitive", "class_ogre_1_1_file_system_archive.html#ac3a99075db0a1e06ee0a856dfa05f5a9", null ],
+    [ "isReadOnly", "class_ogre_1_1_file_system_archive.html#aac59f4ba4e35fe3bf8820117593d0998", null ],
+    [ "list", "class_ogre_1_1_file_system_archive.html#a5907d6e8da20de62af12d564d9fbd279", null ],
+    [ "listFileInfo", "class_ogre_1_1_file_system_archive.html#aebb587510a65222aca84fa6f8eb69e0f", null ],
+    [ "load", "class_ogre_1_1_file_system_archive.html#af3fbf5feb7c3875831d22808b3545d75", null ],
+    [ "open", "class_ogre_1_1_file_system_archive.html#a43a6d5e4f1757cfd8616a499fa774150", null ],
+    [ "remove", "class_ogre_1_1_file_system_archive.html#ade8a11a1b84b467ee124c8ef96bd594c", null ],
+    [ "setIgnoreHidden", "class_ogre_1_1_file_system_archive.html#ae1e6d89d5edc833b8ea5ef74d32a5ad1", null ],
+    [ "unload", "class_ogre_1_1_file_system_archive.html#a8364eb430fccfbe249b64c0dd29c2cb5", null ],
+    [ "msIgnoreHidden", "class_ogre_1_1_file_system_archive.html#a197c16fb380d5d245f27ede38eadcb5a", null ]
+];

api/1.10/class_ogre_1_1_overlay_element_commands_1_1_cmd_material.js
@@ -1,0 +1,5 @@
+var class_ogre_1_1_overlay_element_commands_1_1_cmd_material =
+[
+    [ "doGet", "class_ogre_1_1_overlay_element_commands_1_1_cmd_material.html#ab2656d50a7f9144940d1b8b5efe14ad8", null ],
+    [ "doSet", "class_ogre_1_1_overlay_element_commands_1_1_cmd_material.html#a7920aa95725174a957c395e82d0268db", null ]
+];

api/1.10/class_ogre_1_1_pose.js
@@ -1,0 +1,26 @@
+var class_ogre_1_1_pose =
+[
+    [ "ConstNormalsIterator", "class_ogre_1_1_pose.html#a537dcec01c2cc2f8021e4c89aa9fdde9", null ],
+    [ "ConstVertexOffsetIterator", "class_ogre_1_1_pose.html#ab7830f6ad3feaf3740a8e6477d97cef5", null ],
+    [ "NormalsIterator", "class_ogre_1_1_pose.html#a326485fc1f4fd6a07c4e21b0ddf165a3", null ],
+    [ "NormalsMap", "class_ogre_1_1_pose.html#a77b417702362e8a351c220dd4deeaf80", null ],
+    [ "VertexOffsetIterator", "class_ogre_1_1_pose.html#aab53719cd5a035c0b5ca9301c9927a9a", null ],
+    [ "VertexOffsetMap", "class_ogre_1_1_pose.html#a2508702f4d3791dc59ce9af8fdfa998e", null ],
+    [ "Pose", "class_ogre_1_1_pose.html#ac92652fd8e7e7be224a77492004bf285", null ],
+    [ "~Pose", "class_ogre_1_1_pose.html#ac969f4336838560b56d2ad650f48595f", null ],
+    [ "_getHardwareVertexBuffer", "class_ogre_1_1_pose.html#a3d1113ada247586b91465b3e1a02eb67", null ],
+    [ "addVertex", "class_ogre_1_1_pose.html#ae61acae74c70fb3646902ce5f04ee3e7", null ],
+    [ "addVertex", "class_ogre_1_1_pose.html#af88898dd4ae88f28fe8815137a308c51", null ],
+    [ "clearVertices", "class_ogre_1_1_pose.html#aac73c679713e93a8dad3dfed46038809", null ],
+    [ "clone", "class_ogre_1_1_pose.html#a153cee4b0f92d630006125ea2d17e9ee", null ],
+    [ "getIncludesNormals", "class_ogre_1_1_pose.html#a32721dacadc6b0dfbef78318b3a41929", null ],
+    [ "getName", "class_ogre_1_1_pose.html#a73c497f062296a446298258d3b500e86", null ],
+    [ "getNormals", "class_ogre_1_1_pose.html#a56c3f18962e02907fff9eba2c196250c", null ],
+    [ "getNormalsIterator", "class_ogre_1_1_pose.html#aeafa497d9f16d22f6d8782c375e85b7e", null ],
+    [ "getNormalsIterator", "class_ogre_1_1_pose.html#ab72ff022a57f41a11e33edd782089382", null ],
+    [ "getTarget", "class_ogre_1_1_pose.html#a8c18792868f8165a19ef645b9395a20b", null ],
+    [ "getVertexOffsetIterator", "class_ogre_1_1_pose.html#aaf66be533fd63dff6be363d573ae6740", null ],
+    [ "getVertexOffsetIterator", "class_ogre_1_1_pose.html#a0f6ceaf2411fe3b8e63652b4a182ca35", null ],
+    [ "getVertexOffsets", "class_ogre_1_1_pose.html#a7216f1dac1bd9e0bec03160d2929248a", null ],
+    [ "removeVertex", "class_ogre_1_1_pose.html#a139c78110101298ddde260277e0d38f1", null ]
+];

api/1.10/class_ogre_1_1_segment.js
@@ -1,0 +1,14 @@
+var class_ogre_1_1_segment =
+[
+    [ "Segment", "class_ogre_1_1_segment.html#a505c8fb506a5998c49205215512da8a3", null ],
+    [ "Segment", "class_ogre_1_1_segment.html#a44470ed7ead1affb566b776c1d771746", null ],
+    [ "distance", "class_ogre_1_1_segment.html#a7515542e86a7ffd57db0d3e717822882", null ],
+    [ "intersects", "class_ogre_1_1_segment.html#aa420b18ca6960ec392b90ff05cc3b620", null ],
+    [ "set", "class_ogre_1_1_segment.html#ad0cb0efd8bac56a53b12bf044f6635a2", null ],
+    [ "setEndPoint", "class_ogre_1_1_segment.html#aab7e3b6b12f17b430129883443f83cc0", null ],
+    [ "setOrigin", "class_ogre_1_1_segment.html#a54a372be746c3665fa5db9091e9fc8ed", null ],
+    [ "squaredDistance", "class_ogre_1_1_segment.html#a91959f6687d6b38e5330d4c84e4118e9", null ],
+    [ "mDirection", "class_ogre_1_1_segment.html#a4c26a34309f2ef27674caaa76128621d", null ],
+    [ "mExtent", "class_ogre_1_1_segment.html#a44850fd02064b0555e1ec782502032b0", null ],
+    [ "mOrigin", "class_ogre_1_1_segment.html#a6e305c32bbf2be9f6541e56f6003eda9", null ]
+];

OGRECave/ogre

api/1.10/class_ogre_1_1_area_emitter_1_1_cmd_height.js
@@ -1,0 +1,5 @@
+var class_ogre_1_1_area_emitter_1_1_cmd_height =
+[
+    [ "doGet", "class_ogre_1_1_area_emitter_1_1_cmd_height.html#a211634d8f243f4c5b52ee7501f71c10b", null ],
+    [ "doSet", "class_ogre_1_1_area_emitter_1_1_cmd_height.html#a018b4c9a19bf91d5c37257434e8ba8b9", null ]
+];

api/1.10/class_ogre_1_1_d3_d9_h_l_s_l_program_factory.js
@@ -1,0 +1,8 @@
+var class_ogre_1_1_d3_d9_h_l_s_l_program_factory =
+[
+    [ "D3D9HLSLProgramFactory", "class_ogre_1_1_d3_d9_h_l_s_l_program_factory.html#a4b3edf46c6983c44d273b98fb83a2cd4", null ],
+    [ "~D3D9HLSLProgramFactory", "class_ogre_1_1_d3_d9_h_l_s_l_program_factory.html#a5583d0dd25327a47f9eaa2793024677f", null ],
+    [ "getLanguage", "class_ogre_1_1_d3_d9_h_l_s_l_program_factory.html#a4de0b8433bb3341eee43491ff1660de4", null ],
+    [ "create", "class_ogre_1_1_d3_d9_h_l_s_l_program_factory.html#a3123fef6fb8a5906eae596a224d8c383", null ],
+    [ "destroy", "class_ogre_1_1_d3_d9_h_l_s_l_program_factory.html#a97c41f530e862d8d66a8aafd8084202a", null ]
+];

api/1.10/class_ogre_1_1_d3_d9_video_mode.js
@@ -1,0 +1,15 @@
+var class_ogre_1_1_d3_d9_video_mode =
+[
+    [ "D3D9VideoMode", "class_ogre_1_1_d3_d9_video_mode.html#a34a77a0c52fb29311ec6181ac8bee754", null ],
+    [ "D3D9VideoMode", "class_ogre_1_1_d3_d9_video_mode.html#a3092cd96e44807a629e3ac45f05c683c", null ],
+    [ "D3D9VideoMode", "class_ogre_1_1_d3_d9_video_mode.html#ac4d2913efac422ca4a93ab0771373431", null ],
+    [ "~D3D9VideoMode", "class_ogre_1_1_d3_d9_video_mode.html#af819f89d3ecd9236d1c0f9f91a71d0ba", null ],
+    [ "getWidth", "class_ogre_1_1_d3_d9_video_mode.html#a70e53fa440e95ed17b85015a1e5643a6", null ],
+    [ "getHeight", "class_ogre_1_1_d3_d9_video_mode.html#a2993d0fefdcad771e9649e5283d85270", null ],
+    [ "getFormat", "class_ogre_1_1_d3_d9_video_mode.html#a349726fc8e0cf1e82dba2c831dc8bc0c", null ],
+    [ "getRefreshRate", "class_ogre_1_1_d3_d9_video_mode.html#a3f7dd41242c7c00188aad2e8489f41b8", null ],
+    [ "getColourDepth", "class_ogre_1_1_d3_d9_video_mode.html#a327c0921f5f515d59b9f01de46e08b88", null ],
+    [ "getDisplayMode", "class_ogre_1_1_d3_d9_video_mode.html#a4024d30689e34f0c9022cef516405028", null ],
+    [ "increaseRefreshRate", "class_ogre_1_1_d3_d9_video_mode.html#a84bc6e2cc34253b99afc560176038b4c", null ],
+    [ "getDescription", "class_ogre_1_1_d3_d9_video_mode.html#a055a751c6ab62e8f68f55d34d074f4b9", null ]
+];

api/1.10/class_ogre_1_1_file_system_archive.js
@@ -1,0 +1,23 @@
+var class_ogre_1_1_file_system_archive =
+[
+    [ "FileSystemArchive", "class_ogre_1_1_file_system_archive.html#ad5ae519a6d13d89a7d4dc66ea8f64875", null ],
+    [ "~FileSystemArchive", "class_ogre_1_1_file_system_archive.html#ad89960cb80cf490a23cab7b909ffa092", null ],
+    [ "isCaseSensitive", "class_ogre_1_1_file_system_archive.html#a5564d16d6fce7e6a6bd80d5b0eb65801", null ],
+    [ "load", "class_ogre_1_1_file_system_archive.html#af3fbf5feb7c3875831d22808b3545d75", null ],
+    [ "unload", "class_ogre_1_1_file_system_archive.html#a8364eb430fccfbe249b64c0dd29c2cb5", null ],
+    [ "open", "class_ogre_1_1_file_system_archive.html#a2b4cf117c5893227c040cf490f919d4c", null ],
+    [ "create", "class_ogre_1_1_file_system_archive.html#a886e95b80604575912e8dadecdb47ca5", null ],
+    [ "remove", "class_ogre_1_1_file_system_archive.html#ade8a11a1b84b467ee124c8ef96bd594c", null ],
+    [ "list", "class_ogre_1_1_file_system_archive.html#abb3ca4faad474266715b8a6a3f45a515", null ],
+    [ "listFileInfo", "class_ogre_1_1_file_system_archive.html#ad6fddaa31fe76495a73810ee216ed889", null ],
+    [ "find", "class_ogre_1_1_file_system_archive.html#a925dba2d068aebb37a84c825cb63f101", null ],
+    [ "findFileInfo", "class_ogre_1_1_file_system_archive.html#af2fe5863fc66c47008e5c828142c4208", null ],
+    [ "exists", "class_ogre_1_1_file_system_archive.html#ae4025e1efbfc9be5d03482201671d984", null ],
+    [ "getModifiedTime", "class_ogre_1_1_file_system_archive.html#ab1c4a849411e745369385e83e36c2400", null ],
+    [ "setIgnoreHidden", "class_ogre_1_1_file_system_archive.html#ae1e6d89d5edc833b8ea5ef74d32a5ad1", null ],
+    [ "getIgnoreHidden", "class_ogre_1_1_file_system_archive.html#a79c4957064dc9858d3abdc87ebadfe7c", null ],
+    [ "getName", "class_ogre_1_1_file_system_archive.html#a65c4971ab0f1081f0976891c0ee3571b", null ],
+    [ "isReadOnly", "class_ogre_1_1_file_system_archive.html#a3229d2fd6c8d3236f68f7a3f1baedeec", null ],
+    [ "getType", "class_ogre_1_1_file_system_archive.html#a3d9b82e50ec15e683de00d9a6f649809", null ],
+    [ "msIgnoreHidden", "class_ogre_1_1_file_system_archive.html#a197c16fb380d5d245f27ede38eadcb5a", null ]
+];

api/1.10/class_ogre_1_1_overlay_element_commands_1_1_cmd_material.js
@@ -1,0 +1,5 @@
+var class_ogre_1_1_overlay_element_commands_1_1_cmd_material =
+[
+    [ "doGet", "class_ogre_1_1_overlay_element_commands_1_1_cmd_material.html#a5fb215a52235366f8d63013b98b3e5d8", null ],
+    [ "doSet", "class_ogre_1_1_overlay_element_commands_1_1_cmd_material.html#a7920aa95725174a957c395e82d0268db", null ]
+];

api/1.10/class_ogre_1_1_pose.js
@@ -1,0 +1,25 @@
+var class_ogre_1_1_pose =
+[
+    [ "VertexOffsetMap", "class_ogre_1_1_pose.html#a2508702f4d3791dc59ce9af8fdfa998e", null ],
+    [ "VertexOffsetIterator", "class_ogre_1_1_pose.html#aab53719cd5a035c0b5ca9301c9927a9a", null ],
+    [ "ConstVertexOffsetIterator", "class_ogre_1_1_pose.html#ab7830f6ad3feaf3740a8e6477d97cef5", null ],
+    [ "NormalsMap", "class_ogre_1_1_pose.html#a77b417702362e8a351c220dd4deeaf80", null ],
+    [ "NormalsIterator", "class_ogre_1_1_pose.html#a326485fc1f4fd6a07c4e21b0ddf165a3", null ],
+    [ "ConstNormalsIterator", "class_ogre_1_1_pose.html#a537dcec01c2cc2f8021e4c89aa9fdde9", null ],
+    [ "Pose", "class_ogre_1_1_pose.html#ac92652fd8e7e7be224a77492004bf285", null ],
+    [ "getName", "class_ogre_1_1_pose.html#a15ff1d481bb1bf19e53d9fa18f723b35", null ],
+    [ "getTarget", "class_ogre_1_1_pose.html#a8d46f47f7554cc6c7586e2a3b6396180", null ],
+    [ "getIncludesNormals", "class_ogre_1_1_pose.html#a4da3dcf872bedec629309e26a0d056cd", null ],
+    [ "addVertex", "class_ogre_1_1_pose.html#ae61acae74c70fb3646902ce5f04ee3e7", null ],
+    [ "addVertex", "class_ogre_1_1_pose.html#af88898dd4ae88f28fe8815137a308c51", null ],
+    [ "removeVertex", "class_ogre_1_1_pose.html#a139c78110101298ddde260277e0d38f1", null ],
+    [ "clearVertices", "class_ogre_1_1_pose.html#aac73c679713e93a8dad3dfed46038809", null ],
+    [ "getVertexOffsetIterator", "class_ogre_1_1_pose.html#a6f56e495780545b66dd647c0d2cfbc23", null ],
+    [ "getVertexOffsetIterator", "class_ogre_1_1_pose.html#a0f6ceaf2411fe3b8e63652b4a182ca35", null ],
+    [ "getVertexOffsets", "class_ogre_1_1_pose.html#a101cc5eaa30c0876ed10072ffc3fb528", null ],
+    [ "getNormalsIterator", "class_ogre_1_1_pose.html#a2a56b570214e27849751f985dc5afe2e", null ],
+    [ "getNormalsIterator", "class_ogre_1_1_pose.html#ab72ff022a57f41a11e33edd782089382", null ],
+    [ "getNormals", "class_ogre_1_1_pose.html#a71e286c1d9103401d84f8e7eaa0204d8", null ],
+    [ "_getHardwareVertexBuffer", "class_ogre_1_1_pose.html#ac6207382101e044739bc4c9f55304229", null ],
+    [ "clone", "class_ogre_1_1_pose.html#af179dbd96279f8b18764831061e486a0", null ]
+];

api/1.10/class_ogre_1_1_segment.js
@@ -1,0 +1,14 @@
+var class_ogre_1_1_segment =
+[
+    [ "Segment", "class_ogre_1_1_segment.html#a505c8fb506a5998c49205215512da8a3", null ],
+    [ "Segment", "class_ogre_1_1_segment.html#a44470ed7ead1affb566b776c1d771746", null ],
+    [ "set", "class_ogre_1_1_segment.html#ad0cb0efd8bac56a53b12bf044f6635a2", null ],
+    [ "setOrigin", "class_ogre_1_1_segment.html#a54a372be746c3665fa5db9091e9fc8ed", null ],
+    [ "setEndPoint", "class_ogre_1_1_segment.html#aab7e3b6b12f17b430129883443f83cc0", null ],
+    [ "distance", "class_ogre_1_1_segment.html#adfd5cf89317589fbb08d315128a50f4e", null ],
+    [ "squaredDistance", "class_ogre_1_1_segment.html#a8f2fc7eeb151ba60c9d1b770d7dfeed0", null ],
+    [ "intersects", "class_ogre_1_1_segment.html#abc4c3bf90017fe83e602aa568545a65f", null ],
+    [ "mOrigin", "class_ogre_1_1_segment.html#a6e305c32bbf2be9f6541e56f6003eda9", null ],
+    [ "mDirection", "class_ogre_1_1_segment.html#a4c26a34309f2ef27674caaa76128621d", null ],
+    [ "mExtent", "class_ogre_1_1_segment.html#a44850fd02064b0555e1ec782502032b0", null ]
+];