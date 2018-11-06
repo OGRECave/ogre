OGRECave/ogre

commit 4783d10429f6bf23ece0817b07514a31edfe8102

ScriptTranslator: error if custom property is unsupported by GpuProgram
also do not try setting properties on unsupported programs - they would
be forwarded to the wrong implementation.

OgreMain/src/OgreScriptTranslator.cpp
@@ -3660,8 +3660,8 @@ namespace Ogre{
             return;
         }
 
-        std::list<std::pair<String,String> > customParameters;
-        String source;
+        std::vector<std::pair<PropertyAbstractNode*, String> > customParameters;
+        String source, profiles;
         AbstractNodePtr params;
         for(AbstractNodeList::iterator i = obj->children.begin(); i != obj->children.end(); ++i)
         {
@@ -3686,7 +3686,7 @@ namespace Ogre{
                 }
                 else
                 {
-                    String name = prop->name, value;
+                    String value;
                     bool first = true;
                     for(AbstractNodeList::iterator it = prop->values.begin(); it != prop->values.end(); ++it)
                     {
@@ -3709,7 +3709,11 @@ namespace Ogre{
                             }
                         }
                     }
-                    customParameters.push_back(std::make_pair(name, value));
+
+                    if(prop->name == "profiles")
+                        profiles = value;
+                    else
+                        customParameters.push_back(std::make_pair(prop, value));
                 }
             }
             else if((*i)->type == ANT_OBJECT)
@@ -3748,9 +3752,18 @@ namespace Ogre{
         prog->setVertexTextureFetchRequired(false);
         prog->_notifyOrigin(obj->file);
 
+        // special case for Cg
+        if(!profiles.empty())
+            prog->setParameter("profiles", profiles);
+
         // Set the custom parameters
-        for(std::list<std::pair<String,String> >::iterator i = customParameters.begin(); i != customParameters.end(); ++i)
-            prog->setParameter(i->first, i->second);
+        for(const auto& p : customParameters)
+        {
+            if(prog->isSupported() && !prog->setParameter(p.first->name, p.second))
+            {
+                compiler->addError(ScriptCompiler::CE_INVALIDPARAMETERS, p.first->file, p.first->line, p.first->name);
+            }
+        }
 
         // Set up default parameters
         if(prog->isSupported() && params)

commit 9493ad30b9391f540f8d5da1477ac05ef79ea4f6

Main: ScriptTranslator - also special case targets property
4783d10429f6bf23ece0817b07514a31edfe8102 broke most of script defined
hlsl shaders.

OgreMain/src/OgreScriptTranslator.cpp
@@ -3748,7 +3748,7 @@ namespace Ogre{
         }
 
         std::vector<std::pair<PropertyAbstractNode*, String> > customParameters;
-        String source, profiles;
+        String source, profiles, target;
         AbstractNodePtr params;
         for(AbstractNodeList::iterator i = obj->children.begin(); i != obj->children.end(); ++i)
         {
@@ -3788,6 +3788,8 @@ namespace Ogre{
 
                     if(prop->name == "profiles")
                         profiles = value;
+                    else if(prop->name == "target")
+                        target = value;
                     else
                         customParameters.push_back(std::make_pair(prop, value));
                 }
@@ -3833,6 +3835,10 @@ namespace Ogre{
         // special case for Cg
         if(!profiles.empty())
             prog->setParameter("profiles", profiles);
+
+        // special case for HLSL
+        if(!target.empty())
+            prog->setParameter("target", target);
 
         // Set the custom parameters
         for(const auto& p : customParameters)