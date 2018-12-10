77aa1a4621dc8419e1d8a3da47cb5e9e09718168
Eugene Golushkov <e.golushkov@nospam_gmail.com>
2018-12-10 16:22:29 +0200

    Main: DeflateStream - Avoid assert when read is satisfied by cache
    and there are more cached bytes. Fix data corruption on seek(0).

--- OgreMain/src/OgreDeflate.cpp
+++ OgreMain/src/OgreDeflate.cpp
@@ -262,10 +262,10 @@ namespace Ogre
                         }
                     }
                 }
-            }
             
-            // Cache the last bytes read
-            mReadCache.cacheData((char*)buf + cachereads, newReadUncompressed);
+                // Cache the last bytes read not from cache
+                mReadCache.cacheData((char*)buf + cachereads, newReadUncompressed);
+            }
             
             mCurrentPos += newReadUncompressed + cachereads;
             
@@ -405,6 +405,7 @@ namespace Ogre
                 mCompressedStream->seek(0);
                 mZStream->avail_in = static_cast<uint>(mCompressedStream->read(mTmp, getAvailInForSinglePass()));
                 inflateReset(mZStream);
+                mReadCache.clear();
             }
             else 
             {
