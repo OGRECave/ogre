d1fcbb20bdac8f1b12cfa744101057d45be2c9c3
Pavel Rojtberg <rojtberg@gmail.com>
2018-12-14 19:57:42 +0100

    GL*: fix various issues discovered by clang analyzer

--- RenderSystems/GLSupport/include/GLSL/OgreGLSLPreprocessor.h
+++ RenderSystems/GLSupport/include/GLSL/OgreGLSLPreprocessor.h
@@ -106,10 +106,10 @@ namespace Ogre {
             /// Token length in bytes
             size_t Length;
 
-            Token () : Allocated (0), String (NULL)
+            Token () : Allocated (0), String (NULL), Length(0)
             { }
 
-            Token (Kind iType) : Type (iType), Allocated (0), String (NULL)
+            Token (Kind iType) : Type (iType), Allocated (0), String (NULL), Length(0)
             { }
 
             Token (Kind iType, const char *iString, size_t iLength) :
