017e362fc686af0e041f0009059b7479728d29ca
Pavel Rojtberg <rojtberg@gmail.com>
2018-12-26 22:37:34 +0100

    Main: add proper deprecation notes for legacy HardwareBufferLockGuard

--- OgreMain/include/OgreHardwareIndexBuffer.h
+++ OgreMain/include/OgreHardwareIndexBuffer.h
@@ -74,8 +74,8 @@ namespace Ogre {
             // NB subclasses should override lock, unlock, readData, writeData
     };
     
-    /** Locking helper. */    
-    typedef HardwareBufferLockGuard HardwareIndexBufferLockGuard; // deprecated, use HardwareBufferLockGuard directly
+    /// @deprecated use HardwareBufferLockGuard directly
+    OGRE_DEPRECATED typedef HardwareBufferLockGuard HardwareIndexBufferLockGuard;
 
     /** @} */
     /** @} */
