1653e84470d1a0a64bd6382dc983f408b05e75a6
Pavel Rojtberg <rojtberg@gmail.com>
2019-02-24 17:53:28 +0100

    Main: String - fix doxygen inline comment

--- OgreMain/include/OgreString.h
+++ OgreMain/include/OgreString.h
@@ -81,8 +81,8 @@ namespace Ogre {
     class _OgreExport StringUtil
     {
     public:
-        OGRE_DEPRECATED static const String& BLANK; /// @deprecated use Ogre::BLANKSTRING instead
-        OGRE_DEPRECATED typedef StringStream StrStreamType; /// @deprecated use Ogre::StringStream instead
+        OGRE_DEPRECATED static const String& BLANK; //!< @deprecated use Ogre::BLANKSTRING instead
+        OGRE_DEPRECATED typedef StringStream StrStreamType; //!< @deprecated use Ogre::StringStream instead
 
         /** Removes any whitespace characters, be it standard space or
             TABs and so on.
