c07a93833502886da57e15cf3b96981a3940dec7
Pavel Rojtberg <rojtberg@gmail.com>
2019-04-04 21:40:08 +0200

    Bites: Trays - ensure window update on first call

--- Components/Bites/src/OgreTrays.cpp
+++ Components/Bites/src/OgreTrays.cpp
@@ -16,6 +16,9 @@
 namespace OgreBites
 {
 
+// maximal rate to update widgets internally
+static unsigned FRAME_UPDATE_DELAY = 250; // ms
+
 Widget::Widget()
 {
     mTrayLoc = TL_NONE;
@@ -1135,7 +1138,7 @@ TrayManager::TrayManager(const Ogre::String &name, Ogre::RenderWindow *window, T
     mGroupInitProportion(0.0f), mGroupLoadProportion(0.0f), mLoadInc(0.0f)
 {
     mTimer = Ogre::Root::getSingleton().getTimer();
-    mLastStatUpdateTime = 0;
+    mLastStatUpdateTime = -FRAME_UPDATE_DELAY; // update immediately on first call
 
     Ogre::OverlayManager& om = Ogre::OverlayManager::getSingleton();
 
@@ -1935,7 +1938,7 @@ void TrayManager::frameRendered(const Ogre::FrameEvent &evt)
 
 
     unsigned long currentTime = mTimer->getMilliseconds();
-    if (areFrameStatsVisible() && currentTime - mLastStatUpdateTime > 250)
+    if (areFrameStatsVisible() && currentTime - mLastStatUpdateTime >= FRAME_UPDATE_DELAY)
     {
         Ogre::RenderTarget::FrameStats stats = mWindow->getStatistics();
 
@@ -1981,7 +1984,7 @@ void TrayManager::windowUpdate()
 {
 #if OGRE_PLATFORM != OGRE_PLATFORM_APPLE_IOS
     unsigned long currentTime = mTimer->getMilliseconds();
-    if (currentTime - mLastStatUpdateTime > 250)
+    if (currentTime - mLastStatUpdateTime >= FRAME_UPDATE_DELAY)
     {
         mLastStatUpdateTime = currentTime;
         mWindow->update();
