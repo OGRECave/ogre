65596cd823a313c64659d85f2ab40f1d1739b8c6
Pavel Rojtberg <rojtberg@gmail.com>
2019-04-06 01:20:18 +0200

    Main: define a common OGRE_TOKEN_PASTE macro for __LINE__ concat

--- OgreMain/include/OgrePrerequisites.h
+++ OgreMain/include/OgrePrerequisites.h
@@ -33,6 +33,9 @@ THE SOFTWARE
 #include <memory>
 
 namespace Ogre {
+    #define OGRE_TOKEN_PASTE_INNER(x, y) x ## y
+    #define OGRE_TOKEN_PASTE(x, y) OGRE_TOKEN_PASTE_INNER(x, y)
+
     #define OGRE_VERSION    ((OGRE_VERSION_MAJOR << 16) | (OGRE_VERSION_MINOR << 8) | OGRE_VERSION_PATCH)
 
     // define the real number values to be used
