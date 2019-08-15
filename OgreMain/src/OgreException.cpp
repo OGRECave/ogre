9e0dd82309d15597967132e0543d7d8d6a1f37ea
Pavel Rojtberg <rojtberg@gmail.com>
2019-08-15 17:29:32 +0200

    Main: remove logging from Exception constructor altogether
    - it calls a virtual function which is UB
    - Ogre uses exceptions for internal communication which must be fast
    - the messages were only ever received by log-listeners

--- OgreMain/src/OgreException.cpp
+++ OgreMain/src/OgreException.cpp
@@ -48,14 +48,6 @@ namespace Ogre {
         source( src ),
         file( fil )
     {
-        // Log this error, mask it from debug though since it may be caught and ignored
-        if(LogManager::getSingletonPtr())
-        {
-            LogManager::getSingleton().logMessage(
-                this->getFullDescription(), 
-                LML_CRITICAL, true);
-        }
-
     }
 
     Exception::Exception(const Exception& rhs)
