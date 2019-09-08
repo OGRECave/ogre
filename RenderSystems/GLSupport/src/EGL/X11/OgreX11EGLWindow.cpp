3446a0c24a0b28b07b1edf8ef3cad7036a28aa38
Pavel Rojtberg <rojtberg@gmail.com>
2019-09-08 16:18:57 +0200

    GLSupport: unify EGL/ GLX debug output

--- RenderSystems/GLSupport/src/EGL/X11/OgreX11EGLWindow.cpp
+++ RenderSystems/GLSupport/src/EGL/X11/OgreX11EGLWindow.cpp
@@ -574,13 +574,16 @@ namespace Ogre {
         setVSyncInterval(vsyncInterval);
         setVSyncEnabled(vsync);
 
-        int Rsz, Gsz, Bsz, Asz;
+        int Rsz, Gsz, Bsz, Asz, fsaa;
         mGLSupport->getGLConfigAttrib(mEglConfig, EGL_RED_SIZE, &Rsz);
         mGLSupport->getGLConfigAttrib(mEglConfig, EGL_BLUE_SIZE, &Gsz);
         mGLSupport->getGLConfigAttrib(mEglConfig, EGL_GREEN_SIZE, &Bsz);
         mGLSupport->getGLConfigAttrib(mEglConfig, EGL_ALPHA_SIZE, &Asz);
-        LogManager::getSingleton().stream() << "X11EGLWindow::create used FBConfig = " <<
-                Rsz << "/" << Bsz << "/" << Gsz << "/" << Asz;
+        mGLSupport->getGLConfigAttrib(mEglConfig, EGL_SAMPLES, &fsaa);
+
+        LogManager::getSingleton().logMessage(
+            StringUtil::format("X11EGLWindow::create colourBufferSize=%d/%d/%d/%d gamma=%d FSAA=%d", Rsz,
+                               Bsz, Gsz, Asz, mHwGamma, fsaa));
 
         mName = name;
         mWidth = width;
@@ -589,6 +592,7 @@ namespace Ogre {
         mTop = top;
         mActive = true;
         mVisible = true;
+        mFSAA = fsaa;
 
         mClosed = false;
     }
