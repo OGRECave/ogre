fa6fd87844779e5df2e3419f982d06f99ddd62de
Pavel Rojtberg <rojtberg@gmail.com>
2019-12-16 19:30:30 +0100

    RTSS: fix linear skinning shaders and replace existing sample by them

--- Samples/DualQuaternion/include/DualQuaternion.h
+++ Samples/DualQuaternion/include/DualQuaternion.h
@@ -25,15 +25,6 @@ class _OgreSampleClassExport Sample_DualQuaternion : public SdkSample
         mInfo["Category"] = "Animation";
     }
 
-    void testCapabilities(const RenderSystemCapabilities* caps)
-    {
-        if (Root::getSingleton().getRenderSystem()->getName().find("OpenGL 3+") != String::npos)
-            OGRE_EXCEPT(Exception::ERR_NOT_IMPLEMENTED,
-                        "Sample currently out of order in the OpenGL 3+ render system."
-                        "  Try again soon!",
-                        "Sample_DualQuaternion::testCapabilities");
-    }
-
     bool frameRenderingQueued(const FrameEvent& evt)
     {
         const Real start = 30;
@@ -61,6 +52,10 @@ class _OgreSampleClassExport Sample_DualQuaternion : public SdkSample
     void setupContent()
     {
 #if defined(INCLUDE_RTSHADER_SYSTEM) && defined(RTSHADER_SYSTEM_BUILD_EXT_SHADERS)
+        // Make this viewport work with shader generator scheme.
+        mShaderGenerator->invalidateScheme(Ogre::RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME);
+        mViewport->setMaterialScheme(RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME);
+
         // Add the hardware skinning to the shader generator default
         // render state.
         mSrsHardwareSkinning = mShaderGenerator->createSubRenderState(Ogre::RTShader::HardwareSkinning::Type);
@@ -154,21 +149,6 @@ class _OgreSampleClassExport Sample_DualQuaternion : public SdkSample
         // as well.
         RTShader::HardwareSkinningFactory::getSingleton().prepareEntityForSkinning(ent);
         RTShader::HardwareSkinningFactory::getSingleton().prepareEntityForSkinning(entDQ, RTShader::ST_DUAL_QUATERNION, false, true);
-
-        // The following line is needed only because the spine models'
-        // materials have shaders and as such is not automatically
-        // reflected in the RTSS system.
-        RTShader::ShaderGenerator::getSingleton().createShaderBasedTechnique(
-            *ent->getSubEntity(0)->getMaterial(),
-            Ogre::MaterialManager::DEFAULT_SCHEME_NAME,
-            Ogre::RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME,
-            true);
-
-        RTShader::ShaderGenerator::getSingleton().createShaderBasedTechnique(
-            *entDQ->getSubEntity(0)->getMaterial(),
-            Ogre::MaterialManager::DEFAULT_SCHEME_NAME,
-            Ogre::RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME,
-            true);
 #endif
 
         // Create name and value for skinning mode.
