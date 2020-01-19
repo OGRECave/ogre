102d3fbf4f633e2f5e62372f30dd48aff59b750a
Pavel Rojtberg <rojtberg@gmail.com>
2020-01-19 19:00:21 +0100

    Samples: SSAO - port to gbuffer RTSS stage

--- Samples/Media/materials/scripts/SSAO/ShowDepthFP.glsl
+++ Samples/Media/materials/scripts/SSAO/ShowDepthFP.glsl
@@ -2,10 +2,9 @@
 varying vec2 oUv0;
 
 uniform sampler2D mrt1;
-uniform sampler2D tex;
 	
 void main()
 {
     float depth = texture2D(mrt1, oUv0).w;
-    gl_FragColor = vec4(texture2D(tex, vec2(depth*20.0, 0)).rgb, 1.0);
+    gl_FragColor = vec4(vec3(pow(depth, 0.3)), 1.0);
 }
