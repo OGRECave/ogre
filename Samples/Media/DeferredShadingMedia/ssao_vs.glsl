96027df391adbdf1d4eb002945b90f87028735ae
Pavel Rojtberg <rojtberg@gmail.com>
2020-01-19 17:15:40 +0100

    Samples: Deferred - SSAO use standard instead of custom data sources

--- Samples/Media/DeferredShadingMedia/ssao_vs.glsl
+++ Samples/Media/DeferredShadingMedia/ssao_vs.glsl
@@ -5,8 +5,8 @@
 #endif
 
 uniform mat4 worldViewProj;
-uniform vec3 farCorner;
 in vec4 vertex;
+in vec3 normal;
 out vec2 oUv0;
 out vec3 ray;
 
@@ -19,6 +19,6 @@ void main()
     // Convert to image space
     uv = (vec2(uv.x, -uv.y) + 1.0) * 0.5;
     oUv0 = uv;
-    // Calculate the correct ray (modify XY parameters based on screen-space quad XY)
-    ray = farCorner * vec3(sign(vertex.xy), 1);
+    // ray towards frustum far corners
+    ray = normal;
 }
