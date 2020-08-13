08864398a1a6fcdb48535cd80bc419dbade4bd4e
Pavel Rojtberg <rojtberg@gmail.com>
2020-08-13 21:33:18 +0200

    Samples: merge GLSL150 and GLSLES shaders that all were GLSL100

---
+++ Samples/Media/materials/programs/GLSL/GrassFp.glsl
@@ -0,0 +1,20 @@
+////////////////////////////// MOVING GRASS
+// Vertex program to wave some grass about
+// Assumes UV texture coords of v==0 indicates the top of the grass
+uniform sampler2D diffuseMap;
+
+varying vec4 oUv0;
+varying vec4 oColour;
+
+void main()
+{	
+    vec4 texColor = texture2D(diffuseMap, oUv0.xy);
+
+    // Do manual alpha rejection because it is not built into OpenGL ES 2
+    if (texColor.a < 0.588)
+    {
+        discard;
+    }
+
+    gl_FragColor = vec4(texColor.rgb * oColour.rgb, 1.0);
+}
--- Samples/Media/materials/programs/GLSL150/HeatCasterFp.glsl
+++
@@ -1,9 +0,0 @@
-#version 150
-
-in vec2 NDotV;
-out vec4 fragColour;
-
-void main()
-{
-   fragColour = vec4(NDotV.x);
-}
