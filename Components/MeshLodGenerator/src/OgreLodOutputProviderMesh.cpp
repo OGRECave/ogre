f68f9bee74928ceca5cb90c4af3df0ea53931e7f
Pavel Rojtberg <rojtberg@gmail.com>
2020-09-15 21:20:24 +0200

    use mesh provided HardwareBufferManager to create hardware buffers

--- Components/MeshLodGenerator/src/OgreLodOutputProviderMesh.cpp
+++ Components/MeshLodGenerator/src/OgreLodOutputProviderMesh.cpp
@@ -73,7 +73,7 @@ namespace Ogre
                 curLod->indexCount = indexCount;
             }
 
-            curLod->indexBuffer = HardwareBufferManager::getSingleton().createIndexBuffer(
+            curLod->indexBuffer = mMesh->getHardwareBufferManager()->createIndexBuffer(
                 data->mIndexBufferInfoList[i].indexSize == 2 ?
                 HardwareIndexBuffer::IT_16BIT : HardwareIndexBuffer::IT_32BIT,
                 curLod->indexCount, mMesh->getIndexBufferUsage(), mMesh->isIndexBufferShadowed());
