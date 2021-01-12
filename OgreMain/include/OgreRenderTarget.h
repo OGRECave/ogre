a123f7afca2bd9cf54ffd33417b8a09450630587
Pavel Rojtberg <rojtberg@gmail.com>
2021-01-12 20:05:52 +0100

    Main: unify fsaaHint documentation

--- OgreMain/include/OgreRenderTarget.h
+++ OgreMain/include/OgreRenderTarget.h
@@ -375,15 +375,14 @@ namespace Ogre {
         */
         virtual uint getFSAA() const { return mFSAA; }
 
-        /** Gets the FSAA hint (@see Root::createRenderWindow)
-        */
+        /// RenderSystem specific FSAA option. See @ref RenderSystem::_createRenderWindow for details.
         virtual const String& getFSAAHint() const { return mFSAAHint; }
 
         /** Set the level of multisample AA to be used if hardware support it.
             This option will be ignored if the hardware does not support it 
             or setting can not be changed on the fly on per-target level. 
             @param fsaa The number of samples
-            @param fsaaHint Any hinting text (@see Root::createRenderWindow)
+            @param fsaaHint @copybrief getFSAAHint
         */
         virtual void setFSAA(uint fsaa, const String& fsaaHint) { }
 
