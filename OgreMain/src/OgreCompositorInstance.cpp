OGRECave/ogre

commit 334fc4b75ebc0546e21e9e8ae2ece75a325bb358

Bug 423 - Fix for looking up for texture definitions in very complex compositor setups in getSourceForTex and getTargetForTex

OgreMain/src/OgreCompositorInstance.cpp
@@ -528,6 +528,7 @@ void CompositorInstance::setScheme(const String& schemeName, bool reuseTextures)
 	if (tech)
 	{
 		setTechnique(tech, reuseTextures);
+		mActiveScheme = tech->getSchemeName();
 	}
 }
 //-----------------------------------------------------------------------
@@ -958,13 +959,14 @@ RenderTarget *CompositorInstance::getTargetForTex(const String &name)
 	if (texDef != 0 && !texDef->refCompName.empty()) 
 	{
 		//This is a reference - find the compositor and referenced texture definition
-		const CompositorPtr& refComp = CompositorManager::getSingleton().getByName(texDef->refCompName);
-		if (refComp.isNull())
+		Ogre::CompositorInstance *refCompInst = mChain->getCompositor(texDef->refCompName);
+		if(refCompInst == 0)
 		{
 			OGRE_EXCEPT(Exception::ERR_ITEM_NOT_FOUND, "Referencing non-existent compositor",
 				"CompositorInstance::getTargetForTex");
 		}
-		CompositionTechnique::TextureDefinition* refTexDef = refComp->getSupportedTechnique()->getTextureDefinition(texDef->refTexName);
+		Ogre::Compositor *refComp = refCompInst->getCompositor();
+		CompositionTechnique::TextureDefinition* refTexDef = refComp->getSupportedTechnique(refCompInst->getScheme())->getTextureDefinition(texDef->refTexName);
 		if (refTexDef == 0)
 		{
 			OGRE_EXCEPT(Exception::ERR_ITEM_NOT_FOUND, "Referencing non-existent compositor texture",
@@ -1028,14 +1030,14 @@ const String &CompositorInstance::getSourceForTex(const String &name, size_t mrt
 
 	if (!texDef->refCompName.empty()) 
 	{
-		//This is a reference - find the compositor and referenced texture definition
-		const CompositorPtr& refComp = CompositorManager::getSingleton().getByName(texDef->refCompName);
-		if (refComp.isNull())
+		Ogre::CompositorInstance *refCompInst = mChain->getCompositor(texDef->refCompName);
+		if(refCompInst == 0)
 		{
 			OGRE_EXCEPT(Exception::ERR_ITEM_NOT_FOUND, "Referencing non-existent compositor",
-				"CompositorInstance::getSourceForTex");
+			"CompositorInstance::getSourceForTex");
 		}
-		CompositionTechnique::TextureDefinition* refTexDef = refComp->getSupportedTechnique()->getTextureDefinition(texDef->refTexName);
+		Ogre::Compositor *refComp = refCompInst->getCompositor();
+		CompositionTechnique::TextureDefinition* refTexDef = refComp->getSupportedTechnique(refCompInst->getScheme())->getTextureDefinition(texDef->refTexName);
 		if (refTexDef == 0)
 		{
 			OGRE_EXCEPT(Exception::ERR_ITEM_NOT_FOUND, "Referencing non-existent compositor texture",

commit 008d13ecaf50615e9bfb7df0fd5ba3223ca05313

Main: Compositor - allow referencing global textures again
bug introduced by 334fc4b75ebc0546e21e9e8ae2ece75a325bb358

OgreMain/src/OgreCompositorInstance.cpp
@@ -46,6 +46,7 @@ CompositorInstance::CompositorInstance(CompositionTechnique *technique,
         mEnabled(false),
         mAlive(false)
 {
+    OgreAssert(mChain, "Undefined compositor chain");
     mEnabled = false;
     const String& logicName = mTechnique->getCompositorLogicName();
     if (!logicName.empty())
@@ -1031,14 +1032,11 @@ CompositorInstance::resolveTexReference(const CompositionTechnique::TextureDefin
     CompositionTechnique::TextureDefinition* refTexDef = 0;
 
     //Try chain first
-    if(mChain)
+    if (CompositorInstance* refCompInst = mChain->getCompositor(texDef->refCompName))
     {
-        CompositorInstance* refCompInst = mChain->getCompositor(texDef->refCompName);
-        if(!refCompInst)
-            OGRE_EXCEPT(Exception::ERR_ITEM_NOT_FOUND, "Referencing non-existent compositor");
-
-        refTexDef = refCompInst->getCompositor()->getSupportedTechnique(
-            refCompInst->getScheme())->getTextureDefinition(texDef->refTexName);
+        refTexDef = refCompInst->getCompositor()
+                        ->getSupportedTechnique(refCompInst->getScheme())
+                        ->getTextureDefinition(texDef->refTexName);
     }
 
     if(!refTexDef)
@@ -1049,6 +1047,10 @@ CompositorInstance::resolveTexReference(const CompositionTechnique::TextureDefin
         {
             refTexDef = refComp->getSupportedTechnique()->getTextureDefinition(texDef->refTexName);
         }
+
+        if (refTexDef && refTexDef->scope != CompositionTechnique::TS_GLOBAL)
+            OGRE_EXCEPT(Exception::ERR_INVALIDPARAMS,
+                        "Referenced texture '" + texDef->refTexName + "' must have global scope");
     }
 
     if(!refTexDef)
@@ -1088,7 +1090,6 @@ RenderTarget *CompositorInstance::getTargetForTex(const String &name)
             {
                 //Find the instance and check if it is before us
                 CompositorInstance* refCompInst = 0;
-                OgreAssert(mChain, "Undefined compositor chain");
                 bool beforeMe = true;
                 for (CompositorInstance* nextCompInst : mChain->getCompositorInstances())
                 {
@@ -1158,7 +1159,6 @@ const TexturePtr &CompositorInstance::getSourceForTex(const String &name, size_t
             {
                 //Find the instance and check if it is before us
                 CompositorInstance* refCompInst = 0;
-                OgreAssert(mChain, "Undefined compositor chain");
                 bool beforeMe = true;
                 for (CompositorInstance* nextCompInst : mChain->getCompositorInstances())
                 {