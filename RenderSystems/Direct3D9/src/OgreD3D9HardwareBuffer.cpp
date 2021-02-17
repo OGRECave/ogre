58cdb070b0ebbc20451d5337b37dd5bb2ea62055
Pavel Rojtberg <rojtberg@gmail.com>
2021-02-17 23:13:08 +0100

    D3D9: fix wrong cast in createBuffer

--- RenderSystems/Direct3D9/src/OgreD3D9HardwareBuffer.cpp
+++ RenderSystems/Direct3D9/src/OgreD3D9HardwareBuffer.cpp
@@ -307,7 +307,14 @@ namespace Ogre {
             OGRE_EXCEPT(Exception::ERR_RENDERINGAPI_ERROR, "Cannot restore D3D9 buffer: " + msg);
         }
 
-        hr = static_cast<IDirect3DVertexBuffer9*>(bufferResources->mBuffer)->GetDesc(&mBufferDesc);
+        if(mType = D3DFMT_VERTEXDATA)
+        {
+            hr = static_cast<IDirect3DVertexBuffer9*>(bufferResources->mBuffer)->GetDesc(&mBufferDesc);
+        }
+        else
+        {
+            hr = static_cast<IDirect3DIndexBuffer9*>(bufferResources->mBuffer)->GetDesc((D3DINDEXBUFFER_DESC*)&mBufferDesc);
+        }
         if (FAILED(hr))
         {
             String msg = DXGetErrorDescription(hr);
