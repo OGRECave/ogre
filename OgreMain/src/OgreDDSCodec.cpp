7697dff872e950d375ed130f6a8700cd99b366c3
Pavel Rojtberg <rojtberg@gmail.com>
2021-03-04 19:47:49 +0100

    Main: DDSCodec - make DDS constants static

--- OgreMain/src/OgreDDSCodec.cpp
+++ OgreMain/src/OgreDDSCodec.cpp
@@ -121,6 +121,7 @@ namespace Ogre {
 #pragma pack ()
 #endif
 
+namespace {
     const uint32 DDS_MAGIC = FOURCC('D', 'D', 'S', ' ');
     const uint32 DDS_PIXELFORMAT_SIZE = 8 * sizeof(uint32);
     const uint32 DDS_CAPS_SIZE = 4 * sizeof(uint32);
@@ -158,7 +159,7 @@ namespace Ogre {
     const uint32 D3DFMT_R32F            = 114;
     const uint32 D3DFMT_G32R32F         = 115;
     const uint32 D3DFMT_A32B32G32R32F   = 116;
-
+}
 
     //---------------------------------------------------------------------
     DDSCodec* DDSCodec::msInstance = 0;
