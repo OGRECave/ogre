4599c59280ff2c7d4a895467a3c25314638e5359
Pavel Rojtberg <rojtberg@gmail.com>
2021-04-05 13:58:35 +0200

    Samples: CSM - correct capabilities check

--- Samples/CSMShadows/include/CSMShadows.h
+++ Samples/CSMShadows/include/CSMShadows.h
@@ -36,6 +36,7 @@ public:
     void testCapabilities(const Ogre::RenderSystemCapabilities* caps)
     {
         auto mat = MaterialManager::getSingleton().getByName("CSMShadows/Rockwall");
+        OgreAssert(mat, "required material not found");
         mat->load();
         if (mat->getSupportedTechniques().empty())
         {
