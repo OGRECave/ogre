a56b00e0e819df05c6619332a79f15611a468264
Pavel Rojtberg <rojtberg@gmail.com>
2021-05-10 01:24:36 +0200

    Main: Math - Use Möller-Trumbore ray-triangle intersection algorithm
    it does not require a normal and is 25% faster for the common case,
    where we hit the triangle plane and even 50% faster if we would compute
    a normal first.
    When shooting the ray away from the triangle, it is >45% slower though.

--- OgreMain/src/OgreMath.cpp
+++ OgreMain/src/OgreMath.cpp
@@ -592,105 +592,38 @@ namespace Ogre
         return true;
     }
     //-----------------------------------------------------------------------
-    std::pair<bool, Real> Math::intersects(const Ray& ray, const Vector3& a,
-        const Vector3& b, const Vector3& c, const Vector3& normal,
-        bool positiveSide, bool negativeSide)
+    std::pair<bool, Real> Math::intersects(const Ray& ray, const Vector3& a, const Vector3& b,
+                                           const Vector3& c, bool positiveSide, bool negativeSide)
     {
-        //
-        // Calculate intersection with plane.
-        //
-        Real t;
-        {
-            Real denom = normal.dotProduct(ray.getDirection());
+        const Real EPSILON = 1e-6f;
+        Vector3 E1 = b - a;
+        Vector3 E2 = c - a;
+        Vector3 P = ray.getDirection().crossProduct(E2);
+        Real det = E1.dotProduct(P);
 
-            // Check intersect side
-            if (denom > + std::numeric_limits<Real>::epsilon())
-            {
-                if (!negativeSide)
-                    return std::pair<bool, Real>(false, (Real)0);
-            }
-            else if (denom < - std::numeric_limits<Real>::epsilon())
-            {
-                if (!positiveSide)
-                    return std::pair<bool, Real>(false, (Real)0);
-            }
-            else
-            {
-                // Parallel or triangle area is close to zero when
-                // the plane normal not normalised.
-                return std::pair<bool, Real>(false, (Real)0);
-            }
+        // if determinant is near zero, ray lies in plane of triangle
+        if((!positiveSide || det <= EPSILON) && (!negativeSide || det >= -EPSILON))
+            return {false, (Real)0};
+        Real inv_det = 1.0f / det;
 
-            t = normal.dotProduct(a - ray.getOrigin()) / denom;
+        // calculate u parameter and test bounds
+        Vector3 T = ray.getOrigin() - a;
+        Real u = T.dotProduct(P) * inv_det;
+        if(u < 0.0f || u > 1.0f)
+            return {false, (Real)0};
 
-            if (t < 0)
-            {
-                // Intersection is behind origin
-                return std::pair<bool, Real>(false, (Real)0);
-            }
-        }
+        // calculate v parameter and test bounds
+        Vector3 Q = T.crossProduct(E1);
+        Real v = ray.getDirection().dotProduct(Q) * inv_det;
+        if (v < 0.0f || u + v > 1.0f)
+            return {false, (Real)0};
 
-        //
-        // Calculate the largest area projection plane in X, Y or Z.
-        //
-        size_t i0, i1;
-        {
-            Real n0 = Math::Abs(normal[0]);
-            Real n1 = Math::Abs(normal[1]);
-            Real n2 = Math::Abs(normal[2]);
+        // calculate t, ray intersects triangle
+        Real t = E2.dotProduct(Q) * inv_det;
+        if (t < 0.0f)
+            return {false, (Real)0};
 
-            i0 = 1; i1 = 2;
-            if (n1 > n2)
-            {
-                if (n1 > n0) i0 = 0;
-            }
-            else
-            {
-                if (n2 > n0) i1 = 0;
-            }
-        }
-
-        //
-        // Check the intersection point is inside the triangle.
-        //
-        {
-            Real u1 = b[i0] - a[i0];
-            Real v1 = b[i1] - a[i1];
-            Real u2 = c[i0] - a[i0];
-            Real v2 = c[i1] - a[i1];
-            Real u0 = t * ray.getDirection()[i0] + ray.getOrigin()[i0] - a[i0];
-            Real v0 = t * ray.getDirection()[i1] + ray.getOrigin()[i1] - a[i1];
-
-            Real alpha = u0 * v2 - u2 * v0;
-            Real beta  = u1 * v0 - u0 * v1;
-            Real area  = u1 * v2 - u2 * v1;
-
-            // epsilon to avoid float precision error
-            const Real EPSILON = 1e-6f;
-
-            Real tolerance = - EPSILON * area;
-
-            if (area > 0)
-            {
-                if (alpha < tolerance || beta < tolerance || alpha+beta > area-tolerance)
-                    return std::pair<bool, Real>(false, (Real)0);
-            }
-            else
-            {
-                if (alpha > tolerance || beta > tolerance || alpha+beta < area-tolerance)
-                    return std::pair<bool, Real>(false, (Real)0);
-            }
-        }
-
-        return std::pair<bool, Real>(true, (Real)t);
-    }
-    //-----------------------------------------------------------------------
-    std::pair<bool, Real> Math::intersects(const Ray& ray, const Vector3& a,
-        const Vector3& b, const Vector3& c,
-        bool positiveSide, bool negativeSide)
-    {
-        Vector3 normal = calculateBasicFaceNormalWithoutNormalize(a, b, c);
-        return intersects(ray, a, b, c, normal, positiveSide, negativeSide);
+        return {true, t};
     }
     //-----------------------------------------------------------------------
     bool Math::intersects(const Sphere& sphere, const AxisAlignedBox& box)
