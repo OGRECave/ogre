cc8f3ba13b19b97d53e90ab8a2b4d95ae679107c
Pavel Rojtberg <rojtberg@gmail.com>
2021-06-18 19:11:00 +0200

    Samples: AndoidJNI - update sample to show Input Event handling

--- Samples/AndroidJNI/BasicSample.java
+++ Samples/AndroidJNI/BasicSample.java
@@ -31,18 +31,16 @@ package org.ogre.example;
 import org.ogre.*;
 
 import android.app.Activity;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
 import android.content.res.AssetManager;
 
-public class BasicSample extends Activity implements SensorEventListener {
+public class BasicSample extends Activity {
 	protected Handler handler = null;
 	protected SurfaceView surfaceView = null;
 	protected Surface lastSurface = null;
@@ -53,6 +51,7 @@ public class BasicSample extends Activity implements SensorEventListener {
 	private AssetManager assetMgr = null;
 	
 	ApplicationContext ogreApp = null;
+	CameraMan camman = null;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -126,6 +125,9 @@ public class BasicSample extends Activity implements SensorEventListener {
                                     camnode.attachObject(cam);
                                     camnode.setPosition(0, 0, 15);
                                     
+									camman = new CameraMan(camnode);
+									ogreApp.addInputListener(camman);
+
                                     Entity ent = scnMgr.createEntity("Sinbad.mesh");
                                     SceneNode node = scnMgr.getRootSceneNode().createChildSceneNode();
                                     node.attachObject(ent);
@@ -189,13 +191,32 @@ public class BasicSample extends Activity implements SensorEventListener {
 
 	boolean wndCreate = false;
 
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	private int previousX = -1;
+	private int previousY = -1;
 
-	}
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		if(ogreApp == null)
+			return true;
 
-	public void onSensorChanged(SensorEvent event) {
-		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+		if(e.getAction() == MotionEvent.ACTION_MOVE)
+		{
+			// Convert to Ogre Event types, so we can use C++ Listeners like CameraMan
+			// you could also just modify camnode here yourself
+			MouseMotionEvent mme = new MouseMotionEvent();
+			mme.setXrel((int) e.getX() - previousX);
+			mme.setYrel((int) e.getY() - previousY);
+			mme.setType(EventType.MOUSEMOTION.swigValue());
+
+			Event ogreEvt = new Event();
+			ogreEvt.setMotion(mme);
+			ogreApp._fireInputEvent(ogreEvt, 0);
 		}
+
+		previousX = (int)e.getX();
+		previousY = (int)e.getY();
+
+		return true;
 	}
 
 	static {
