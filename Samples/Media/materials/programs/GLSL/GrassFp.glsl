966d740fb722376846059f05400c01b1c75e79d3
Pavel Rojtberg <rojtberg@gmail.com>
2021-06-19 15:21:13 +0200

    Samples: clean-up Grass (static-geometry) sample

--- Samples/Media/materials/programs/GLSL/GrassFp.glsl
+++ Samples/Media/materials/programs/GLSL/GrassFp.glsl
@@ -2,6 +2,7 @@
 // Vertex program to wave some grass about
 // Assumes UV texture coords of v==0 indicates the top of the grass
 uniform sampler2D diffuseMap;
+uniform float alphaThresh;
 
 varying vec4 oUv0;
 varying vec4 oColour;
@@ -11,7 +12,7 @@ void main()
     vec4 texColor = texture2D(diffuseMap, oUv0.xy);
 
     // Do manual alpha rejection because it is not built into OpenGL ES 2
-    if (texColor.a < 0.588)
+    if (texColor.a < alphaThresh)
     {
         discard;
     }
