6c376059f03fbcbfd612e7cbf64216cf1b7ebd5b
Pavel Rojtberg <rojtberg@gmail.com>
2021-06-28 01:21:24 +0200

    Samples: DeferredShading - exclusively use the RTSS for GBuffer shaders

--- Samples/DeferredShading/include/GBufferSchemeHandler.h
+++ Samples/DeferredShading/include/GBufferSchemeHandler.h
@@ -17,7 +17,6 @@ same license as the rest of the engine.
 #define _GBUFFERSCHEMEHANDLER_H
 
 #include "OgreMaterialManager.h"
-#include "GBufferMaterialGenerator.h"
 
 /** Class for handling materials who did not specify techniques for rendering
  *  themselves into the GBuffer. This class allows deferred shading to be used,
@@ -29,45 +28,9 @@ same license as the rest of the engine.
 class GBufferSchemeHandler : public Ogre::MaterialManager::Listener
 {
 public:
-    /** @copydoc MaterialManager::Listener::handleSchemeNotFound */
-    virtual Ogre::Technique* handleSchemeNotFound(unsigned short schemeIndex, 
+    Ogre::Technique* handleSchemeNotFound(unsigned short schemeIndex,
         const Ogre::String& schemeName, Ogre::Material* originalMaterial, unsigned short lodIndex, 
-        const Ogre::Renderable* rend);
-protected:
-    //The material generator
-    GBufferMaterialGenerator mMaterialGenerator;
-    
-    //The string that will be checked in textures to determine whether they are normal maps
-    static const Ogre::String NORMAL_MAP_PATTERN;
-
-    //A structure for containing the properties of a material, relevant to GBuffer rendering
-    //You might need to expand this class to support more options
-    struct PassProperties 
-    {
-        PassProperties() : isDeferred(true), normalMap(0), isSkinned(false) {}
-
-        bool isDeferred;
-        std::vector<Ogre::TextureUnitState*> regularTextures;
-        Ogre::TextureUnitState* normalMap;
-        bool isSkinned;
-        bool hasDiffuseColour;
-        
-        //Example of possible extension : vertex colours
-        //Ogre::TrackVertexColourType vertexColourType;
-    };
-
-    //Inspect a technique and return its relevant properties
-    PassProperties inspectPass(Ogre::Pass* pass, 
-        unsigned short lodIndex, const Ogre::Renderable* rend);
-
-    //Get the permutation of material flags that fit a certain property sheet
-    MaterialGenerator::Perm getPermutation(const PassProperties& props);
-
-    //Fill a pass with the specific data from the pass it is based on
-    void fillPass(Ogre::Pass* gBufferPass, Ogre::Pass* originalPass, const PassProperties& props);
-
-    //Check if a texture is a normal map, and fill property sheet accordingly
-    bool checkNormalMap(Ogre::TextureUnitState* tus, PassProperties& props);
+        const Ogre::Renderable* rend) override;
 };
 
 #endif
