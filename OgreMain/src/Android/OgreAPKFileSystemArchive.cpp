464e87883075ebe71918a48527dc5ed149de7c73
Joilnen <joilnen@gmail.com>
2021-10-06 08:19:16 -0300

    fix warning message in ndk23 the compiler complaint mixing space and tabs between "if"s statments

--- OgreMain/src/Android/OgreAPKFileSystemArchive.cpp
+++ OgreMain/src/Android/OgreAPKFileSystemArchive.cpp
@@ -72,12 +72,12 @@ namespace {
 	APKFileSystemArchive::APKFileSystemArchive(const String& name, const String& archType, AAssetManager* assetMgr)
 		:Archive(name, archType), mAssetMgr(assetMgr)
 	{
-        if (mName.size() > 0 && mName[0] == '/')
-        	mName.erase(mName.begin());
+		if (mName.size() > 0 && mName[0] == '/')
+			mName.erase(mName.begin());
 
-        mPathPreFix = mName;
-        if (mPathPreFix.size() > 0)
-        	mPathPreFix += "/";
+		mPathPreFix = mName;
+		if (mPathPreFix.size() > 0)
+			mPathPreFix += "/";
 			
 		if(!IsFolderParsed( mName )) {
 			ParseFolder( mAssetMgr, mName );
