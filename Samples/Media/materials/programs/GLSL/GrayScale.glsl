6cdaed6dea800bfb8a6174e64d3397ec68ccec01
Pavel Rojtberg <rojtberg@gmail.com>
2021-11-19 02:56:20 +0100

    Samples: use UnifiedShader for B&W compositor to get Vulkan support

--- Samples/Media/materials/programs/GLSL/GrayScale.glsl
+++ Samples/Media/materials/programs/GLSL/GrayScale.glsl
@@ -1,8 +1,12 @@
-uniform sampler2D RT;
-varying vec2 oUv0;
+#define USE_OGRE_FROM_FUTURE
+#include <OgreUnifiedShader.h>
 
-void main()
+SAMPLER2D(RT, 0);
+
+MAIN_PARAMETERS
+IN(vec2 oUv0, TEXCOORD0)
+MAIN_DECLARATION
 {
-    vec3 greyscale = vec3(dot(texture2D(RT, oUv0).rgb, vec3(0.3, 0.59, 0.11)));
+    vec3 greyscale = vec3_splat(dot(texture2D(RT, oUv0).rgb, vec3(0.3, 0.59, 0.11)));
 	gl_FragColor = vec4(greyscale, 1.0);
 }
