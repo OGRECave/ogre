4b7074d82c9ca8cb38a0326c6b6a8cbafd9d4c44
Pavel Rojtberg <rojtberg@gmail.com>
2021-12-12 23:08:38 +0100

    CMake: fix OpenGL build on OSX

--- CMake/Dependencies.cmake
+++ CMake/Dependencies.cmake
@@ -189,7 +189,7 @@ endif ()
 
 # Find OpenGL
 if(NOT ANDROID AND NOT EMSCRIPTEN)
-  find_package(OpenGL COMPONENTS EGL GLX)
+  find_package(OpenGL)
   macro_log_feature(OPENGL_FOUND "OpenGL" "Support for the OpenGL and OpenGL 3+ render systems" "http://www.opengl.org/" FALSE "" "")
 endif()
 
