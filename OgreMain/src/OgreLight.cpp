de9c7fcc3887a7c1cbd4d298eec0c0b062f35420
Pavel Rojtberg <rojtberg@gmail.com>
2022-01-18 19:20:32 +0100

    Main: Light - use point sized AABB to allow retrieval via SceneQuery

--- OgreMain/src/OgreLight.cpp
+++ OgreMain/src/OgreLight.cpp
@@ -272,14 +272,9 @@ namespace Ogre {
     //-----------------------------------------------------------------------
     const AxisAlignedBox& Light::getBoundingBox(void) const
     {
-        // Null, lights are not visible
-        static AxisAlignedBox box;
+        // zero extent to still allow SceneQueries to work
+        static AxisAlignedBox box(Vector3(0, 0, 0), Vector3(0, 0, 0));
         return box;
-    }
-    //-----------------------------------------------------------------------
-    void Light::_updateRenderQueue(RenderQueue* queue)
-    {
-        // Do nothing
     }
     //-----------------------------------------------------------------------
     void Light::visitRenderables(Renderable::Visitor* visitor, 
