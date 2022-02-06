414c36acbe8132b35b0c603317ca962de2db308c
Pavel Rojtberg <rojtberg@gmail.com>
2022-02-06 20:16:46 +0100

    Android: migrate to AndroidX

--- Samples/AndroidJNI/AndroidTextureOES.java
+++ Samples/AndroidJNI/AndroidTextureOES.java
@@ -4,8 +4,8 @@ import android.content.pm.ActivityInfo;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.graphics.SurfaceTexture;
 import android.app.Activity;
