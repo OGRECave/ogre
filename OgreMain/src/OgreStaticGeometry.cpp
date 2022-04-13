OGRECave/ogre

commit 060f9705c35b7278a89e03d5daf47b50e36cc319

Merged soc08-lod branch into trunk (with some minor changes, platform fixes & doc updates)

OgreMain/src/OgreStaticGeometry.cpp
@@ -693,10 +693,9 @@ namespace Ogre {
 		SceneManager* mgr, uint32 regionID, const Vector3& centre)
 		: MovableObject(name), mParent(parent), mSceneMgr(mgr), mNode(0),
 		mRegionID(regionID), mCentre(centre), mBoundingRadius(0.0f),
-		mCurrentLod(0), mEdgeList(0), mVertexProgramInUse(false)
+		mCurrentLod(0), mEdgeList(0), mVertexProgramInUse(false),
+		mLodStrategy(0)
 	{
-		// First LOD mandatory, and always from 0
-		mLodSquaredDistances.push_back(0.0f);
 	}
 	//--------------------------------------------------------------------------
 	StaticGeometry::Region::~Region()
@@ -735,21 +734,38 @@ namespace Ogre {
 	void StaticGeometry::Region::assign(QueuedSubMesh* qmesh)
 	{
 		mQueuedSubMeshes.push_back(qmesh);
-		// update lod distances
+
+        // Set/check lod strategy
+        const LodStrategy *lodStrategy = qmesh->submesh->parent->getLodStrategy();
+        if (mLodStrategy == 0)
+        {
+            mLodStrategy = lodStrategy;
+
+            // First LOD mandatory, and always from base lod value
+            mLodValues.push_back(mLodStrategy->getBaseValue());
+        }
+        else
+        {
+            if (mLodStrategy != lodStrategy)
+                OGRE_EXCEPT(Exception::ERR_INVALIDPARAMS, "Lod strategies do not match",
+                    "StaticGeometry::Region::assign");
+        }
+
+		// update lod values
 		ushort lodLevels = qmesh->submesh->parent->getNumLodLevels();
 		assert(qmesh->geometryLodList->size() == lodLevels);
 
-		while(mLodSquaredDistances.size() < lodLevels)
+		while(mLodValues.size() < lodLevels)
 		{
-			mLodSquaredDistances.push_back(0.0f);
+			mLodValues.push_back(0.0f);
 		}
 		// Make sure LOD levels are max of all at the requested level
 		for (ushort lod = 1; lod < lodLevels; ++lod)
 		{
 			const MeshLodUsage& meshLod =
 				qmesh->submesh->parent->getLodLevel(lod);
-			mLodSquaredDistances[lod] = std::max(mLodSquaredDistances[lod],
-				meshLod.fromDepthSquared);
+			mLodValues[lod] = std::max(mLodValues[lod],
+				meshLod.value);
 		}
 
 		// update bounds
@@ -771,10 +787,10 @@ namespace Ogre {
 		mNode->attachObject(this);
 		// We need to create enough LOD buckets to deal with the highest LOD
 		// we encountered in all the meshes queued
-		for (ushort lod = 0; lod < mLodSquaredDistances.size(); ++lod)
+		for (ushort lod = 0; lod < mLodValues.size(); ++lod)
 		{
 			LODBucket* lodBucket =
-				OGRE_NEW LODBucket(this, lod, mLodSquaredDistances[lod]);
+				OGRE_NEW LODBucket(this, lod, mLodValues[lod]);
 			mLodBucketList.push_back(lodBucket);
 			// Now iterate over the meshes and assign to LODs
 			// LOD bucket will pick the right LOD to use
@@ -848,41 +864,27 @@ namespace Ogre {
 	//--------------------------------------------------------------------------
 	void StaticGeometry::Region::_notifyCurrentCamera(Camera* cam)
 	{
-		// Calculate squared view depth
-		Vector3 diff = cam->getLodCamera()->getDerivedPosition() - mCentre;
-		Real squaredDepth = diff.squaredLength();
+        // Set camera
+        mCamera = cam;
 
-		// Determine whether to still render
-		Real renderingDist = mParent->getRenderingDistance();
-		if (renderingDist > 0)
-		{
-			// Max distance to still render
-			Real maxDist = renderingDist + mBoundingRadius;
-			if (squaredDepth > Math::Sqr(maxDist))
-			{
-				mBeyondFarDistance = true;
-				return;
-			}
-		}
+        // Cache squared view depth for use by GeometryBucket
+        mSquaredViewDepth = mParentNode->getSquaredViewDepth(cam->getLodCamera());
 
-		mBeyondFarDistance = false;
+        // No lod strategy set yet, skip (this indicates that there are no submeshes)
+        if (mLodStrategy == 0)
+            return;
 
-		// Distance from the edge of the bounding sphere
-		mCamDistanceSquared = squaredDepth - mBoundingRadius * mBoundingRadius;
-		// Clamp to 0
-		mCamDistanceSquared = std::max(static_cast<Real>(0.0), mCamDistanceSquared);
+        // Sanity check
+        assert(!mLodValues.empty());
 
-		// Determine active lod
-		mCurrentLod = static_cast<ushort>(mLodSquaredDistances.size() - 1);
-		for (ushort i = 0; i < mLodSquaredDistances.size(); ++i)
-		{
-			if (mLodSquaredDistances[i] > mCamDistanceSquared)
-			{
-				mCurrentLod = i - 1;
-				break;
-			}
-		}
+        // Calculate lod value
+        Real lodValue = mLodStrategy->getValue(this, cam);
 
+        // Store lod value for this strategy
+        mLodValue = lodValue;
+
+        // Get lod index
+        mCurrentLod = mLodStrategy->getIndex(lodValue, mLodValues);
 	}
 	//--------------------------------------------------------------------------
 	const AxisAlignedBox& StaticGeometry::Region::getBoundingBox(void) const
@@ -898,7 +900,7 @@ namespace Ogre {
 	void StaticGeometry::Region::_updateRenderQueue(RenderQueue* queue)
 	{
 		mLodBucketList[mCurrentLod]->addRenderables(queue, mRenderQueueID,
-			mCamDistanceSquared);
+			mLodValue);
 	}
 	//---------------------------------------------------------------------
 	void StaticGeometry::Region::visitRenderables(Renderable::Visitor* visitor, 
@@ -1089,8 +1091,8 @@ namespace Ogre {
 	//--------------------------------------------------------------------------
 	//--------------------------------------------------------------------------
 	StaticGeometry::LODBucket::LODBucket(Region* parent, unsigned short lod,
-		Real lodDist)
-		: mParent(parent), mLod(lod), mSquaredDistance(lodDist)
+		Real lodValue)
+		: mParent(parent), mLod(lod), mLodValue(lodValue)
 	{
 	}
 	//--------------------------------------------------------------------------
@@ -1158,14 +1160,14 @@ namespace Ogre {
 	}
 	//--------------------------------------------------------------------------
 	void StaticGeometry::LODBucket::addRenderables(RenderQueue* queue,
-		uint8 group, Real camDistanceSquared)
+		uint8 group, Real lodValue)
 	{
 		// Just pass this on to child buckets
 		MaterialBucketMap::iterator i, iend;
 		iend =  mMaterialBucketMap.end();
 		for (i = mMaterialBucketMap.begin(); i != iend; ++i)
 		{
-			i->second->addRenderables(queue, group, camDistanceSquared);
+			i->second->addRenderables(queue, group, lodValue);
 		}
 	}
 	//--------------------------------------------------------------------------
@@ -1180,7 +1182,7 @@ namespace Ogre {
 	{
 		of << "LOD Bucket " << mLod << std::endl;
 		of << "------------------" << std::endl;
-		of << "Distance: " << Math::Sqrt(mSquaredDistance) << std::endl;
+		of << "Lod Value: " << mLodValue << std::endl;
 		of << "Number of Materials: " << mMaterialBucketMap.size() << std::endl;
 		for (MaterialBucketMap::const_iterator i = mMaterialBucketMap.begin();
 			i != mMaterialBucketMap.end(); ++i)
@@ -1273,12 +1275,21 @@ namespace Ogre {
 	}
 	//--------------------------------------------------------------------------
 	void StaticGeometry::MaterialBucket::addRenderables(RenderQueue* queue,
-		uint8 group, Real camDistanceSquared)
+		uint8 group, Real lodValue)
 	{
-		// Determine the current material technique
-		mTechnique = mMaterial->getBestTechnique(
-			mMaterial->getLodIndexSquaredDepth(camDistanceSquared));
+        // Get region
+        Region *region = mParent->getParent();
 
+        // Get material lod strategy
+        const LodStrategy *materialLodStrategy = mMaterial->getLodStrategy();
+
+        // If material strategy doesn't match, recompute lod value with correct strategy
+        if (materialLodStrategy != region->mLodStrategy)
+            lodValue = materialLodStrategy->getValue(region, region->mCamera);
+
+		// Determine the current material technique
+		mTechnique = mMaterial->getTechnique(
+			mMaterial->getLodIndex(lodValue));
 		GeometryBucketList::iterator i, iend;
 		iend =  mGeometryBucketList.end();
 		for (i = mGeometryBucketList.begin(); i != iend; ++i)
@@ -1436,7 +1447,11 @@ namespace Ogre {
 	//--------------------------------------------------------------------------
 	Real StaticGeometry::GeometryBucket::getSquaredViewDepth(const Camera* cam) const
 	{
-		return mParent->getParent()->getSquaredDistance();
+        const Region *region = mParent->getParent()->getParent();
+        if (cam == region->mCamera)
+            return region->mSquaredViewDepth;
+        else
+            return region->getParentNode()->getSquaredViewDepth(cam->getLodCamera());
 	}
 	//--------------------------------------------------------------------------
 	const LightList& StaticGeometry::GeometryBucket::getLights(void) const

Docs/src/manual.texi
@@ -377,9 +377,26 @@ You can also reference vertex and fragment programs (or vertex and pixel shaders
 @subheading Top-level material attributes
 The outermost section of a material definition does not have a lot of attributes of its own (most of the configurable parameters are within the child sections. However, it does have some, and here they are:@*@*
 @anchor{lod_distances}
-@subheading lod_distances
-This attribute controls the distances at which different Techniques can come into effect. @xref{Techniques} for a full discussion of this option.
+@subheading lod_distances (deprecated)
+This option is deprecated in favour of @ref{lod_values} now. 
 @*@*
+@anchor{lod_strategy}
+@subheading lod_strategy 
+Sets the name of the LOD strategy to use. Defaults to 'Distance' which means LOD changes based on distance from the camera. Also supported is 'PixelCount' which changes LOD based on an estimate of the screen-space pixels affected. @*@*
+Format: lod_strategy <name>@*
+Default: lod_strategy Distance@*@*
+@subheading lod_values 
+This attribute defines the values used to control the LOD transition for this material. By setting this attribute, you indicate that you want this material to alter the Technique that it uses based on some metric, such as the distance from the camera, or the approximate screen space coverage. The exact meaning of these values is determined by the option you select for @ref{lod_strategy} - it is a list of distances for the 'Distance' strategy, and a list of pixel counts for the 'PixelCount' strategy, for example. You must give it a list of values, in order from highest LOD value to lowest LOD value, each one indicating the point at which the material will switch to the next LOD. Implicitly, all materials activate LOD index 0 for values less than the first entry, so you do not have to specify '0' at the start of the list. You must ensure that there is at least one Technique with a @ref{lod_index} value for each value in the list (so if you specify 3 values, you must have techniques for LOD indexes 0, 1, 2 and 3). Note you must always have at least one Technique at lod_index 0.@*@*
+@*@*
+Format: lod_values <value0> <value1> <value2> ...@*
+Default: none@*@*
+
+Example: @*
+lod_strategy Distance
+lod_values 300.0 600.5 1200@*@*
+
+The above example would cause the material to use the best Technique at lod_index 0 up to a distance of 300 world units, the best from lod_index 1 from 300 up to 600, lod_index 2 from 600 to 1200, and lod_index 3 from 1200 upwards.@*@*
+
 @anchor{receive_shadows}
 @subheading receive_shadows
 This attribute controls whether objects using this material can have shadows cast upon them.@*@* 
@@ -466,18 +483,6 @@ All techniques must belong to a LOD index, by default they all belong to index 0
 The distance at which a LOD level is applied is determined by the lod_distances attribute of the containing material, @xref{lod_distances} for details.@*@*
 
 Default: lod_index 0@*@*
-
-@anchor{lod_distances}
-@subheading lod_distances
-@strong{Note: this attribute must be specified in the outer material section (ie the parent of all the techniques), but it's specified here since it is most relevant to this section.}@*@*
-
-By setting this attribute, you indicate that you want this material to alter the Technique that it uses based on distance from the camera. You must give it a list of distances, in ascending order, each one indicating the distance at which the material will switch to the next LOD. Implicitly, all materials activate LOD index 0 for distances less than the smallest of these. You must ensure that there is at least one Technique with a @ref{lod_index} value for each distance in the list (so if you specify 3 distances, you must have techniques for indexes 1, 2 and 3). Note you must always have at least one Technique at lod_index 0.@*@*
-
-Format: lod_distances <distance_1> [<distance_2> ... <distance_n>]@*
-
-Example: lod_distances 300.0 600.5 1200@*@*
-
-The above example would cause the material to use the best Technique at lod_index 0 up to a distance of 300 world units, the best from lod_index 1 from 300 up to 600, lod_index 2 from 600 to 1200, and lod_index 3 from 1200 upwards.@*@*
 
 Techniques also contain one or more passes (and there must be at least one), @xref{Passes}.
 

Mac/Ogre/Ogre.xcodeproj/project.pbxproj
@@ -581,6 +581,15 @@
 		AA7510680DDE20FE009D23BA /* OgreMemoryAllocatorConfig.h in Headers */ = {isa = PBXBuildFile; fileRef = AA7510640DDE20FE009D23BA /* OgreMemoryAllocatorConfig.h */; settings = {ATTRIBUTES = (Public, ); }; };
 		AA7510690DDE20FE009D23BA /* OgreMemoryStdAlloc.h in Headers */ = {isa = PBXBuildFile; fileRef = AA7510650DDE20FE009D23BA /* OgreMemoryStdAlloc.h */; settings = {ATTRIBUTES = (Public, ); }; };
 		AA75106A0DDE20FE009D23BA /* OgreMemorySTLAllocator.h in Headers */ = {isa = PBXBuildFile; fileRef = AA7510660DDE20FE009D23BA /* OgreMemorySTLAllocator.h */; settings = {ATTRIBUTES = (Public, ); }; };
+		AA81343A0EFE94A9004EA3A5 /* OgreDistanceLodStrategy.h in Headers */ = {isa = PBXBuildFile; fileRef = AA8134360EFE94A9004EA3A5 /* OgreDistanceLodStrategy.h */; };
+		AA81343B0EFE94A9004EA3A5 /* OgreLodListener.h in Headers */ = {isa = PBXBuildFile; fileRef = AA8134370EFE94A9004EA3A5 /* OgreLodListener.h */; };
+		AA81343C0EFE94A9004EA3A5 /* OgreLodStrategyManager.h in Headers */ = {isa = PBXBuildFile; fileRef = AA8134380EFE94A9004EA3A5 /* OgreLodStrategyManager.h */; };
+		AA81343D0EFE94A9004EA3A5 /* OgrePixelCountLodStrategy.h in Headers */ = {isa = PBXBuildFile; fileRef = AA8134390EFE94A9004EA3A5 /* OgrePixelCountLodStrategy.h */; };
+		AA8134420EFE94DC004EA3A5 /* OgreDistanceLodStrategy.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AA81343E0EFE94DC004EA3A5 /* OgreDistanceLodStrategy.cpp */; };
+		AA8134430EFE94DC004EA3A5 /* OgreLodStrategy.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AA81343F0EFE94DC004EA3A5 /* OgreLodStrategy.cpp */; };
+		AA8134440EFE94DC004EA3A5 /* OgreLodStrategyManager.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AA8134400EFE94DC004EA3A5 /* OgreLodStrategyManager.cpp */; };
+		AA8134450EFE94DC004EA3A5 /* OgrePixelCountLodStrategy.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AA8134410EFE94DC004EA3A5 /* OgrePixelCountLodStrategy.cpp */; };
+		AA8134630EFEA36D004EA3A5 /* OgreLodStrategy.h in Headers */ = {isa = PBXBuildFile; fileRef = AA8134620EFEA36D004EA3A5 /* OgreLodStrategy.h */; };
 		AA879F810EBB02180048013E /* OgreCgFxScriptLoader.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AA879F800EBB02180048013E /* OgreCgFxScriptLoader.cpp */; };
 		AA89775B0E5F4B9400A70BC4 /* OgreRenderToVertexBuffer.h in Headers */ = {isa = PBXBuildFile; fileRef = AA89775A0E5F4B9400A70BC4 /* OgreRenderToVertexBuffer.h */; settings = {ATTRIBUTES = (Public, ); }; };
 		AA89775E0E5F4BB900A70BC4 /* OgreRenderToVertexBuffer.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AA89775D0E5F4BB900A70BC4 /* OgreRenderToVertexBuffer.cpp */; };
@@ -1391,6 +1400,15 @@
 		AA7510640DDE20FE009D23BA /* OgreMemoryAllocatorConfig.h */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.c.h; name = OgreMemoryAllocatorConfig.h; path = ../../OgreMain/include/OgreMemoryAllocatorConfig.h; sourceTree = SOURCE_ROOT; };
 		AA7510650DDE20FE009D23BA /* OgreMemoryStdAlloc.h */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.c.h; name = OgreMemoryStdAlloc.h; path = ../../OgreMain/include/OgreMemoryStdAlloc.h; sourceTree = SOURCE_ROOT; };
 		AA7510660DDE20FE009D23BA /* OgreMemorySTLAllocator.h */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.c.h; name = OgreMemorySTLAllocator.h; path = ../../OgreMain/include/OgreMemorySTLAllocator.h; sourceTree = SOURCE_ROOT; };
+		AA8134360EFE94A9004EA3A5 /* OgreDistanceLodStrategy.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = OgreDistanceLodStrategy.h; path = ../../OgreMain/include/OgreDistanceLodStrategy.h; sourceTree = SOURCE_ROOT; };
+		AA8134370EFE94A9004EA3A5 /* OgreLodListener.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = OgreLodListener.h; path = ../../OgreMain/include/OgreLodListener.h; sourceTree = SOURCE_ROOT; };
+		AA8134380EFE94A9004EA3A5 /* OgreLodStrategyManager.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = OgreLodStrategyManager.h; path = ../../OgreMain/include/OgreLodStrategyManager.h; sourceTree = SOURCE_ROOT; };
+		AA8134390EFE94A9004EA3A5 /* OgrePixelCountLodStrategy.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = OgrePixelCountLodStrategy.h; path = ../../OgreMain/include/OgrePixelCountLodStrategy.h; sourceTree = SOURCE_ROOT; };
+		AA81343E0EFE94DC004EA3A5 /* OgreDistanceLodStrategy.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = OgreDistanceLodStrategy.cpp; path = ../../OgreMain/src/OgreDistanceLodStrategy.cpp; sourceTree = SOURCE_ROOT; };
+		AA81343F0EFE94DC004EA3A5 /* OgreLodStrategy.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = OgreLodStrategy.cpp; path = ../../OgreMain/src/OgreLodStrategy.cpp; sourceTree = SOURCE_ROOT; };
+		AA8134400EFE94DC004EA3A5 /* OgreLodStrategyManager.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = OgreLodStrategyManager.cpp; path = ../../OgreMain/src/OgreLodStrategyManager.cpp; sourceTree = SOURCE_ROOT; };
+		AA8134410EFE94DC004EA3A5 /* OgrePixelCountLodStrategy.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = OgrePixelCountLodStrategy.cpp; path = ../../OgreMain/src/OgrePixelCountLodStrategy.cpp; sourceTree = SOURCE_ROOT; };
+		AA8134620EFEA36D004EA3A5 /* OgreLodStrategy.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = OgreLodStrategy.h; path = ../../OgreMain/include/OgreLodStrategy.h; sourceTree = SOURCE_ROOT; };
 		AA879F7E0EBB02030048013E /* OgreCgFxScriptLoader.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = OgreCgFxScriptLoader.h; path = ../../PlugIns/CgProgramManager/include/OgreCgFxScriptLoader.h; sourceTree = SOURCE_ROOT; };
 		AA879F800EBB02180048013E /* OgreCgFxScriptLoader.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = OgreCgFxScriptLoader.cpp; path = ../../PlugIns/CgProgramManager/src/OgreCgFxScriptLoader.cpp; sourceTree = SOURCE_ROOT; };
 		AA89775A0E5F4B9400A70BC4 /* OgreRenderToVertexBuffer.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = OgreRenderToVertexBuffer.h; path = ../../OgreMain/include/OgreRenderToVertexBuffer.h; sourceTree = SOURCE_ROOT; };
@@ -1690,6 +1708,7 @@
 				000E1F150B38D5AA004EA2DF /* OgreDDSCodec.cpp */,
 				000E1F160B38D5AA004EA2DF /* OgreDefaultHardwareBufferManager.cpp */,
 				000E1F170B38D5AA004EA2DF /* OgreDefaultSceneQueries.cpp */,
+				AA81343E0EFE94DC004EA3A5 /* OgreDistanceLodStrategy.cpp */,
 				000E1F180B38D5AA004EA2DF /* OgreDynLib.cpp */,
 				000E1F190B38D5AA004EA2DF /* OgreDynLibManager.cpp */,
 				000E1F1A0B38D5AA004EA2DF /* OgreEdgeListBuilder.cpp */,
@@ -1718,6 +1737,8 @@
 				005E5EA50B7F39CB00658C32 /* OgreInstancedGeometry.cpp */,
 				000E1F300B38D5AA004EA2DF /* OgreKeyFrame.cpp */,
 				000E1F310B38D5AA004EA2DF /* OgreLight.cpp */,
+				AA81343F0EFE94DC004EA3A5 /* OgreLodStrategy.cpp */,
+				AA8134400EFE94DC004EA3A5 /* OgreLodStrategyManager.cpp */,
 				000E1F320B38D5AA004EA2DF /* OgreLog.cpp */,
 				000E1F330B38D5AA004EA2DF /* OgreLogManager.cpp */,
 				000E1F340B38D5AA004EA2DF /* ogremain.map */,
@@ -1758,6 +1779,7 @@
 				000E1F560B38D5AA004EA2DF /* OgrePatchMesh.cpp */,
 				000E1F570B38D5AA004EA2DF /* OgrePatchSurface.cpp */,
 				000E1F580B38D5AA004EA2DF /* OgrePixelConversions.h */,
+				AA8134410EFE94DC004EA3A5 /* OgrePixelCountLodStrategy.cpp */,
 				000E1F590B38D5AA004EA2DF /* OgrePixelFormat.cpp */,
 				000E1F5A0B38D5AA004EA2DF /* OgrePlane.cpp */,
 				000E1F5B0B38D5AA004EA2DF /* OgrePlatformInformation.cpp */,
@@ -1891,6 +1913,7 @@
 				000E1D760B38D565004EA2DF /* OgreDataStream.h */,
 				000E1D770B38D565004EA2DF /* OgreDDSCodec.h */,
 				000E1D780B38D565004EA2DF /* OgreDefaultHardwareBufferManager.h */,
+				AA8134360EFE94A9004EA3A5 /* OgreDistanceLodStrategy.h */,
 				000E1D790B38D565004EA2DF /* OgreDynLib.h */,
 				000E1D7A0B38D565004EA2DF /* OgreDynLibManager.h */,
 				000E1D7B0B38D565004EA2DF /* OgreEdgeListBuilder.h */,
@@ -1924,6 +1947,9 @@
 				000E1D950B38D566004EA2DF /* OgreIteratorWrappers.h */,
 				000E1D960B38D566004EA2DF /* OgreKeyFrame.h */,
 				000E1D970B38D566004EA2DF /* OgreLight.h */,
+				AA8134370EFE94A9004EA3A5 /* OgreLodListener.h */,
+				AA8134620EFEA36D004EA3A5 /* OgreLodStrategy.h */,
+				AA8134380EFE94A9004EA3A5 /* OgreLodStrategyManager.h */,
 				000E1D980B38D566004EA2DF /* OgreLog.h */,
 				000E1D990B38D566004EA2DF /* OgreLogManager.h */,
 				000E1D9A0B38D566004EA2DF /* OgreManualObject.h */,
@@ -1969,6 +1995,7 @@
 				000E1DC00B38D566004EA2DF /* OgrePass.h */,
 				000E1DC10B38D566004EA2DF /* OgrePatchMesh.h */,
 				000E1DC20B38D566004EA2DF /* OgrePatchSurface.h */,
+				AA8134390EFE94A9004EA3A5 /* OgrePixelCountLodStrategy.h */,
 				000E1DC30B38D566004EA2DF /* OgrePixelFormat.h */,
 				000E1DC40B38D566004EA2DF /* OgrePlane.h */,
 				000E1DC50B38D566004EA2DF /* OgrePlaneBoundedVolume.h */,
@@ -2782,6 +2809,11 @@
 				AA89775B0E5F4B9400A70BC4 /* OgreRenderToVertexBuffer.h in Headers */,
 				AAE7DD060E68917000E1371A /* OgreAtomicWrappers.h in Headers */,
 				AA9567C60EB79DA4009431A6 /* OgreMemoryNedAlloc.h in Headers */,
+				AA81343A0EFE94A9004EA3A5 /* OgreDistanceLodStrategy.h in Headers */,
+				AA81343B0EFE94A9004EA3A5 /* OgreLodListener.h in Headers */,
+				AA81343C0EFE94A9004EA3A5 /* OgreLodStrategyManager.h in Headers */,
+				AA81343D0EFE94A9004EA3A5 /* OgrePixelCountLodStrategy.h in Headers */,
+				AA8134630EFEA36D004EA3A5 /* OgreLodStrategy.h in Headers */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3179,6 +3211,10 @@
 				AAE047FD0E5E0BCE001D1ADC /* OgreShadowCameraSetupPSSM.cpp in Sources */,
 				AA89775E0E5F4BB900A70BC4 /* OgreRenderToVertexBuffer.cpp in Sources */,
 				AA5607850E901ED60041BF06 /* OgreAxisAlignedBox.cpp in Sources */,
+				AA8134420EFE94DC004EA3A5 /* OgreDistanceLodStrategy.cpp in Sources */,
+				AA8134430EFE94DC004EA3A5 /* OgreLodStrategy.cpp in Sources */,
+				AA8134440EFE94DC004EA3A5 /* OgreLodStrategyManager.cpp in Sources */,
+				AA8134450EFE94DC004EA3A5 /* OgrePixelCountLodStrategy.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

OgreMain/include/Makefile.am
@@ -38,6 +38,7 @@ pkginclude_HEADERS = Ogre.h \
                      OgreDataStream.h \
                      OgreDDSCodec.h \
                      OgreDefaultHardwareBufferManager.h \
+                     OgreDistanceLodStrategy.h \
                      OgreDynLib.h \
                      OgreDynLibManager.h \
                      OgreEdgeListBuilder.h \
@@ -73,6 +74,9 @@ pkginclude_HEADERS = Ogre.h \
                      OgreIteratorWrappers.h \
                      OgreKeyFrame.h \
                      OgreLight.h \
+                     OgreLodListener.h \
+                     OgreLodStrategy.h \
+                     OgreLodStrategyManager.h \
                      OgreLog.h \
                      OgreLogManager.h \
                      OgreManualObject.h \
@@ -118,6 +122,7 @@ pkginclude_HEADERS = Ogre.h \
                      OgrePass.h \
                      OgrePatchMesh.h \
                      OgrePatchSurface.h \
+                     OgrePixelCountLodStrategy.h \
                      OgrePixelFormat.h \
                      OgrePlane.h \
                      OgrePlaneBoundedVolume.h \

OgreMain/include/OgreCamera.h
@@ -149,8 +149,8 @@ namespace Ogre {
 		Frustum *mCullFrustum;
 		/// Whether or not the rendering distance of objects should take effect for this camera
 		bool mUseRenderingDistance;
-		/// Camera to use for LOD calculation
-		const Camera* mLodCamera;
+        /// Camera to use for LOD calculation
+        const Camera* mLodCamera;
 
 
         // Internal functions for calcs

commit 09a8ad8b6a833335a5d37176fe1a3a3bb9d2c1c8

Main: fix StaticGeometry::setRenderingDistance
Setting the rendering distance on a static geometry had
no influence whether batches were skipped while rendering.

Fixes #2427

Broken since commit 060f9705c35b7278a89e03d5daf47b50e36cc319

OgreMain/src/OgreStaticGeometry.cpp
@@ -813,6 +813,21 @@ namespace Ogre {
         if (mLodStrategy == 0)
             return;
 
+        // Determine whether to still render
+        mBeyondFarDistance = false;
+
+        Real renderingDist = mParent->getRenderingDistance();
+        if (renderingDist > 0 && cam->getUseRenderingDistance())
+        {
+            // Max distance to still render
+            Real maxDist = renderingDist + mBoundingRadius;
+            if (mSquaredViewDepth > Math::Sqr(maxDist))
+            {
+                mBeyondFarDistance = true;
+                return;
+            }
+        }
+
         // Sanity check
         assert(!mLodValues.empty());
 