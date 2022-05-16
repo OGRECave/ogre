10f9f47fc2b92f266d38cca74595f994bea26a3f
Pavel Rojtberg <rojtberg@gmail.com>
2022-05-17 01:48:04 +0200

    Bites: ImGuiInputListener - finish port to event-based IO

--- Components/Bites/src/OgreImGuiInputListener.cpp
+++ Components/Bites/src/OgreImGuiInputListener.cpp
@@ -22,78 +22,141 @@ static int sdl2imgui(int b)
     }
 }
 
-// SDL2 keycode to scancode
-static int kc2sc(int kc)
-{
-    return kc & ~(1 << 30);
-}
-
 namespace OgreBites
 {
-
-static int keypad2kc(int sym, int mod)
+static ImGuiKey ImGui_ImplSDL2_KeycodeToImGuiKey(int keycode)
 {
-    if (sym < SDLK_KP_1 || sym > SDLK_KP_PERIOD)
-        return sym;
-    bool numlock = (mod & KMOD_NUM) != 0;
-    if (numlock)
-        return sym;
-    if (sym == SDLK_KP_1)
-        sym = SDLK_END;
-    else if (sym == SDLK_KP_2)
-        sym = SDLK_DOWN;
-    else if (sym == SDLK_KP_3)
-        sym = SDLK_PAGEDOWN;
-    else if (sym == SDLK_KP_4)
-        sym = SDLK_LEFT;
-    else if (sym == SDLK_KP_6)
-        sym = SDLK_RIGHT;
-    else if (sym == SDLK_KP_7)
-        sym = SDLK_HOME;
-    else if (sym == SDLK_KP_8)
-        sym = SDLK_UP;
-    else if (sym == SDLK_KP_9)
-        sym = SDLK_PAGEUP;
-    else if (sym == SDLK_KP_0)
-        sym = SDLK_INSERT;
-    else if (sym == SDLK_KP_PERIOD)
-        sym = SDLK_DELETE;
-    return sym;
+    switch (keycode)
+    {
+        case '\t': return ImGuiKey_Tab;
+        case SDLK_LEFT: return ImGuiKey_LeftArrow;
+        case SDLK_RIGHT: return ImGuiKey_RightArrow;
+        case SDLK_UP: return ImGuiKey_UpArrow;
+        case SDLK_DOWN: return ImGuiKey_DownArrow;
+        case SDLK_PAGEUP: return ImGuiKey_PageUp;
+        case SDLK_PAGEDOWN: return ImGuiKey_PageDown;
+        case SDLK_HOME: return ImGuiKey_Home;
+        case SDLK_END: return ImGuiKey_End;
+        case SDLK_INSERT: return ImGuiKey_Insert;
+        case SDLK_DELETE: return ImGuiKey_Delete;
+        case '\b': return ImGuiKey_Backspace;
+        case SDLK_SPACE: return ImGuiKey_Space;
+        case SDLK_RETURN: return ImGuiKey_Enter;
+        case SDLK_ESCAPE: return ImGuiKey_Escape;
+        case '\'': return ImGuiKey_Apostrophe;
+        case ',': return ImGuiKey_Comma;
+        case '-': return ImGuiKey_Minus;
+        case '.': return ImGuiKey_Period;
+        case '/': return ImGuiKey_Slash;
+        case ';': return ImGuiKey_Semicolon;
+        case '=': return ImGuiKey_Equal;
+        case '[': return ImGuiKey_LeftBracket;
+        case '\\': return ImGuiKey_Backslash;
+        case ']': return ImGuiKey_RightBracket;
+        case '`': return ImGuiKey_GraveAccent;
+        //case SDLK_CAPSLOCK: return ImGuiKey_CapsLock;
+        case SDLK_SCROLLLOCK: return ImGuiKey_ScrollLock;
+        case SDLK_NUMLOCKCLEAR: return ImGuiKey_NumLock;
+        case SDLK_PRINTSCREEN: return ImGuiKey_PrintScreen;
+        case SDLK_PAUSE: return ImGuiKey_Pause;
+        case SDLK_KP_0: return ImGuiKey_Keypad0;
+        case SDLK_KP_1: return ImGuiKey_Keypad1;
+        case SDLK_KP_2: return ImGuiKey_Keypad2;
+        case SDLK_KP_3: return ImGuiKey_Keypad3;
+        case SDLK_KP_4: return ImGuiKey_Keypad4;
+        case SDLK_KP_5: return ImGuiKey_Keypad5;
+        case SDLK_KP_6: return ImGuiKey_Keypad6;
+        case SDLK_KP_7: return ImGuiKey_Keypad7;
+        case SDLK_KP_8: return ImGuiKey_Keypad8;
+        case SDLK_KP_9: return ImGuiKey_Keypad9;
+        case SDLK_KP_PERIOD: return ImGuiKey_KeypadDecimal;
+        case SDLK_KP_DIVIDE: return ImGuiKey_KeypadDivide;
+        case SDLK_KP_MULTIPLY: return ImGuiKey_KeypadMultiply;
+        case SDLK_KP_MINUS: return ImGuiKey_KeypadSubtract;
+        case SDLK_KP_PLUS: return ImGuiKey_KeypadAdd;
+        case SDLK_KP_ENTER: return ImGuiKey_KeypadEnter;
+        //case SDLK_KP_EQUALS: return ImGuiKey_KeypadEqual;
+        //case SDLK_LCTRL: return ImGuiKey_LeftCtrl;
+        case SDLK_LSHIFT: return ImGuiKey_LeftShift;
+        /*case SDLK_LALT: return ImGuiKey_LeftAlt;
+        case SDLK_LGUI: return ImGuiKey_LeftSuper;
+        case SDLK_RCTRL: return ImGuiKey_RightCtrl;
+        case SDLK_RSHIFT: return ImGuiKey_RightShift;
+        case SDLK_RALT: return ImGuiKey_RightAlt;
+        case SDLK_RGUI: return ImGuiKey_RightSuper;
+        case SDLK_APPLICATION: return ImGuiKey_Menu;*/
+    }
+    return ImGuiKey_None;
+}
+
+static bool keyEvent(const KeyboardEvent& arg)
+{
+    ImGuiIO& io = ImGui::GetIO();
+
+    int key = arg.keysym.sym;
+
+    if(key >= '0' && key <= '9')
+        key = key - '0' + ImGuiKey_0;
+    else if(key >= 'a' && key <= 'z')
+        key = key - 'a' + ImGuiKey_A;
+    else if(key >= SDLK_F1 && key <= SDLK_F12)
+        key = key - SDLK_F1 + ImGuiKey_F1;
+    else
+        key = ImGui_ImplSDL2_KeycodeToImGuiKey(key);
+
+    io.AddKeyEvent(ImGuiKey_ModShift, arg.keysym.mod & KMOD_SHIFT);
+    io.AddKeyEvent(ImGuiKey_ModCtrl, arg.keysym.mod & KMOD_CTRL);
+    io.AddKeyEvent(ImGuiKey_ModAlt, arg.keysym.mod & KMOD_ALT);
+    io.AddKeyEvent(ImGuiKey_ModSuper, arg.keysym.mod & KMOD_GUI);
+
+    if (key == ImGuiKey_None)
+        return io.WantCaptureKeyboard;
+
+    io.AddKeyEvent(key, arg.type == OgreBites::KEYDOWN);
+    return io.WantCaptureKeyboard;
+}
+
+static bool buttonEvent(const ButtonEvent& evt)
+{
+    ImGuiIO& io = ImGui::GetIO();
+    bool down = evt.type == OgreBites::CONTROLLERBUTTONDOWN;
+    switch(evt.button)
+    {
+    case 0:
+        io.AddKeyEvent(ImGuiKey_GamepadFaceDown, down);
+        break;
+    case 1:
+        io.AddKeyEvent(ImGuiKey_GamepadFaceRight, down);
+        break;
+    case 2:
+        io.AddKeyEvent(ImGuiKey_GamepadFaceLeft, down);
+        break;
+    case 11:
+        io.AddKeyEvent(ImGuiKey_GamepadDpadUp, down);
+        break;
+    case 12:
+        io.AddKeyEvent(ImGuiKey_GamepadDpadDown, down);
+        break;
+    case 13:
+        io.AddKeyEvent(ImGuiKey_GamepadDpadLeft, down);
+        break;
+    case 14:
+        io.AddKeyEvent(ImGuiKey_GamepadDpadRight, down);
+        break;
+    }
+    return true;
 }
 
 ImGuiInputListener::ImGuiInputListener()
 {
     ImGuiIO& io = ImGui::GetIO();
-    // Keyboard mapping. ImGui will use those indices to peek into the io.KeyDown[] array that we will
-    // update during the application lifetime.
-    io.KeyMap[ImGuiKey_Tab] = '\t';
-    io.KeyMap[ImGuiKey_LeftArrow] = kc2sc(SDLK_LEFT);
-    io.KeyMap[ImGuiKey_RightArrow] = kc2sc(SDLK_RIGHT);
-    io.KeyMap[ImGuiKey_UpArrow] = kc2sc(SDLK_UP);
-    io.KeyMap[ImGuiKey_DownArrow] = kc2sc(SDLK_DOWN);
-    io.KeyMap[ImGuiKey_PageUp] = kc2sc(SDLK_PAGEUP);
-    io.KeyMap[ImGuiKey_PageDown] = kc2sc(SDLK_PAGEDOWN);
-    io.KeyMap[ImGuiKey_Home] = kc2sc(SDLK_HOME);
-    io.KeyMap[ImGuiKey_End] = kc2sc(SDLK_END);
-    io.KeyMap[ImGuiKey_Insert] = kc2sc(SDLK_INSERT);
-    io.KeyMap[ImGuiKey_Delete] = kc2sc(SDLK_DELETE);
-    io.KeyMap[ImGuiKey_Backspace] = '\b';
-    io.KeyMap[ImGuiKey_Enter] = SDLK_RETURN;
-    io.KeyMap[ImGuiKey_Escape] = '\033';
-    io.KeyMap[ImGuiKey_Space] = ' ';
-    io.KeyMap[ImGuiKey_KeypadEnter] = kc2sc(SDLK_KP_ENTER);
-    io.KeyMap[ImGuiKey_A] = 'a';
-    io.KeyMap[ImGuiKey_C] = 'c';
-    io.KeyMap[ImGuiKey_V] = 'v';
-    io.KeyMap[ImGuiKey_X] = 'x';
-    io.KeyMap[ImGuiKey_Y] = 'y';
-    io.KeyMap[ImGuiKey_Z] = 'z';
+    io.BackendFlags |= ImGuiBackendFlags_HasGamepad;
 }
 
 bool ImGuiInputListener::mouseWheelRolled(const MouseWheelEvent& arg)
 {
     ImGuiIO& io = ImGui::GetIO();
-    io.MouseWheel = Ogre::Math::Sign(arg.y);
+    io.AddMouseWheelEvent(0.f, Ogre::Math::Sign(arg.y));
     return io.WantCaptureMouse;
 }
 
@@ -101,10 +164,7 @@ bool ImGuiInputListener::mouseMoved(const MouseMotionEvent& arg)
 {
 
     ImGuiIO& io = ImGui::GetIO();
-
-    io.MousePos.x = arg.x;
-    io.MousePos.y = arg.y;
-
+    io.AddMousePosEvent(arg.x, arg.y);
     return io.WantCaptureMouse;
 }
 
@@ -128,60 +188,14 @@ bool ImGuiInputListener::mouseReleased(const MouseButtonEvent& arg)
     }
     return io.WantCaptureMouse;
 }
-bool ImGuiInputListener::keyEvent (const KeyboardEvent& arg)
-{
-    ImGuiIO& io = ImGui::GetIO ();
-    int sym = keypad2kc (arg.keysym.sym, arg.keysym.mod);
-    int key = kc2sc (sym);
-    io.KeysDown[key] = (arg.type == OgreBites::KEYDOWN);
-    io.KeyShift = (arg.keysym.mod & KMOD_SHIFT) != 0;
-    io.KeyCtrl = (arg.keysym.mod & KMOD_CTRL) != 0;
-    io.KeyAlt = (arg.keysym.mod & KMOD_ALT) != 0;
-    io.KeySuper = (arg.keysym.mod & KMOD_GUI) != 0;
-    return io.WantCaptureKeyboard;
-}
-bool ImGuiInputListener::keyPressed(const KeyboardEvent& arg)
-{
-    return keyEvent (arg);
-}
-bool ImGuiInputListener::keyReleased(const KeyboardEvent& arg)
-{
-    return keyEvent (arg);
-}
 bool ImGuiInputListener::textInput (const TextInputEvent& evt)
 {
     ImGuiIO& io = ImGui::GetIO ();
     io.AddInputCharactersUTF8 (evt.chars);
     return true;
 }
-bool ImGuiInputListener::buttonPressed(const ButtonEvent& evt)
-{
-    ImGuiIO& io = ImGui::GetIO();
-    switch(evt.button)
-    {
-    case 0:
-        io.NavInputs[ImGuiNavInput_Activate] = 1;
-        break;
-    case 1:
-        io.NavInputs[ImGuiNavInput_Cancel] = 1;
-        break;
-    case 2:
-        io.NavInputs[ImGuiNavInput_Menu] = 1;
-        break;
-    case 11:
-        io.NavInputs[ImGuiNavInput_DpadUp] = 1;
-        break;
-    case 12:
-        io.NavInputs[ImGuiNavInput_DpadDown] = 1;
-        break;
-    case 13:
-        io.NavInputs[ImGuiNavInput_DpadLeft] = 1;
-        break;
-    case 14:
-        io.NavInputs[ImGuiNavInput_DpadRight] = 1;
-        break;
-    }
-    return true;
-}
-
+bool ImGuiInputListener::keyPressed(const KeyboardEvent& arg) { return keyEvent(arg); }
+bool ImGuiInputListener::keyReleased(const KeyboardEvent& arg) { return keyEvent(arg); }
+bool ImGuiInputListener::buttonPressed(const ButtonEvent& evt) { return buttonEvent(evt); }
+bool ImGuiInputListener::buttonReleased(const ButtonEvent& evt) { return buttonEvent(evt); }
 } // namespace OgreBites
