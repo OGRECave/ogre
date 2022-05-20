b99437f5a7147e49b10807e134ece51598ce06f8
Pavel Rojtberg <rojtberg@gmail.com>
2022-05-20 18:08:22 +0200

    Main: ShadowCameraSetup - do not re-derive orientation when possible

--- OgreMain/src/OgreShadowCameraSetup.cpp
+++ OgreMain/src/OgreShadowCameraSetup.cpp
@@ -117,7 +117,9 @@ namespace Ogre
 
              //convert back to world space
              pos = rot * lightSpacePos;
-            
+
+             texCam->getParentNode()->setPosition(pos);
+             texCam->getParentNode()->setOrientation(rot);
         }
         // Spotlight
         else if (light->getType() == Light::LT_SPOTLIGHT)
@@ -131,12 +133,7 @@ namespace Ogre
                 fovy = Degree(175);
             texCam->setFOVy(fovy);
 
-            // Calculate position, which same as spotlight position
-            pos = light->getDerivedPosition();
-
-            // Calculate direction, which same as spotlight direction
-            dir = - light->getDerivedDirection(); // backwards since point down -z
-            dir.normalise();
+            // keep position & orientation set by ShadowRenderer::prepareShadowTextures
         }
         // Point light
         else
@@ -157,35 +154,17 @@ namespace Ogre
 
             dir = (pos - target); // backwards since point down -z
             dir.normalise();
-        }
 
-        // Finally set position
-        texCam->getParentSceneNode()->setPosition(pos);
-
-        // Calculate orientation based on direction calculated above
-        /*
-        // Next section (camera oriented shadow map) abandoned
-        // Always point in the same direction, if we don't do this then
-        // we get 'shadow swimming' as camera rotates
-        // As it is, we get swimming on moving but this is less noticeable
-
-        // calculate up vector, we want it aligned with cam direction
-        Vector3 up = cam->getDerivedDirection();
-        // Check it's not coincident with dir
-        if (up.dotProduct(dir) >= 1.0f)
-        {
-        // Use camera up
-        up = cam->getUp();
+            Vector3 up = Vector3::UNIT_Y;
+            // Check it's not coincident with dir
+            if (Math::Abs(up.dotProduct(dir)) >= 1.0f)
+            {
+                // Use camera up
+                up = Vector3::UNIT_Z;
+            }
+            texCam->getParentNode()->setOrientation(Math::lookRotation(dir, up));
+            // keep position set by ShadowRenderer::prepareShadowTextures
         }
-        */
-        Vector3 up = Vector3::UNIT_Y;
-        // Check it's not coincident with dir
-        if (Math::Abs(up.dotProduct(dir)) >= 1.0f)
-        {
-            // Use camera up
-            up = Vector3::UNIT_Z;
-        }
-        texCam->getParentNode()->setOrientation(Math::lookRotation(dir, up));
     }
 
 
