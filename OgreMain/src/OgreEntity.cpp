OGRECave/ogre

commit 5362b48529321f87b905d230433148df8e667257

some more loops changed to range loops (#2572)

OgreMain/src/OgreEntity.cpp
@@ -304,11 +304,10 @@ namespace Ogre {
         if (mInitialised)
         {
             // Copy material settings
-            SubEntityList::const_iterator i;
             unsigned int n = 0;
-            for (i = mSubEntityList.begin(); i != mSubEntityList.end(); ++i, ++n)
+            for (const auto *e : mSubEntityList)
             {
-                newEnt->getSubEntity(n)->setMaterialName((*i)->getMaterialName());
+                newEnt->getSubEntity(n)->setMaterialName(e->getMaterialName());
             }
             if (mAnimationState)
             {
@@ -2225,7 +2224,7 @@ namespace Ogre {
         bool debugRenderables)
     {
         // Visit each SubEntity
-        for (auto & i : mSubEntityList)
+        for (auto& i : mSubEntityList)
         {
             visitor->visit(i, 0, false);
         }

OgreMain/src/OgreDepthBuffer.cpp
@@ -105,14 +105,8 @@ namespace Ogre
     //-----------------------------------------------------------------------
     void DepthBuffer::detachFromAllRenderTargets()
     {
-        RenderTargetSet::const_iterator itor = mAttachedRenderTargets.begin();
-        RenderTargetSet::const_iterator end  = mAttachedRenderTargets.end();
-        while( itor != end )
-        {
-            //If we call, detachDepthBuffer, we'll invalidate the iterators
-            (*itor)->_detachDepthBuffer();
-            ++itor;
-        }
+        for (auto *r : mAttachedRenderTargets)
+            r->_detachDepthBuffer();
 
         mAttachedRenderTargets.clear();
     }

OgreMain/src/OgreExternalTextureSourceManager.cpp
@@ -87,11 +87,10 @@ namespace Ogre
     void ExternalTextureSourceManager::destroyAdvancedTexture( const String& sTextureName,
         const String& groupName )
     {
-        TextureSystemList::iterator i;
-        for( i = mTextureSystems.begin(); i != mTextureSystems.end(); ++i )
+        for(auto& t : mTextureSystems)
         {
             //Broadcast to every registered System... Only the true one will destroy texture
-            i->second->destroyAdvancedTexture( sTextureName, groupName );
+            t.second->destroyAdvancedTexture( sTextureName, groupName );
         }
     }
 
@@ -101,20 +100,18 @@ namespace Ogre
         LogManager::getSingleton().logMessage( "Registering Texture Controller: Type = "
                         + sTexturePlugInType + " Name = " + pTextureSystem->getPluginStringName());
 
-        TextureSystemList::iterator i;
-            
-        for( i = mTextureSystems.begin(); i != mTextureSystems.end(); ++i )
+        for(auto& t : mTextureSystems)
         {
-            if( i->first == sTexturePlugInType )
+            if( t.first == sTexturePlugInType )
             {
-                LogManager::getSingleton().logMessage( "Shutting Down Texture Controller: " 
-                        + i->second->getPluginStringName() 
+                LogManager::getSingleton().logMessage( "Shutting Down Texture Controller: "
+                        + t.second->getPluginStringName()
                         + " To be replaced by: "
                         + pTextureSystem->getPluginStringName());
 
-                i->second->shutDown();              //Only one plugIn of Sent Type can be registered at a time
+                t.second->shutDown();              //Only one plugIn of Sent Type can be registered at a time
                                                     //so shut down old plugin before starting new plugin
-                i->second = pTextureSystem;
+                t.second = pTextureSystem;
                 // **Moved this line b/c Rendersystem needs to be selected before things
                 // such as framelistners can be added
                 // pTextureSystem->Initialise();
@@ -127,11 +124,10 @@ namespace Ogre
     //****************************************************************************************
     ExternalTextureSource* ExternalTextureSourceManager::getExternalTextureSource( const String& sTexturePlugInType )
     {
-        TextureSystemList::iterator i;
-        for( i = mTextureSystems.begin(); i != mTextureSystems.end(); ++i )
+        for(auto& t : mTextureSystems)
         {
-            if( i->first == sTexturePlugInType )
-                return i->second;
+            if(t.first == sTexturePlugInType)
+                return t.second;
         }
         return 0;
     }

commit 7f2d6ea1aef42c10150cbce4054cb4ba53f25821

Main: fixup Entity::clone
broken in 5362b48529321f87b905d230433148df8e667257

OgreMain/src/OgreEntity.cpp
@@ -307,7 +307,7 @@ namespace Ogre {
             unsigned int n = 0;
             for (const auto *e : mSubEntityList)
             {
-                newEnt->getSubEntity(n)->setMaterialName(e->getMaterialName());
+                newEnt->getSubEntities()[n++]->setMaterialName(e->getMaterialName());
             }
             if (mAnimationState)
             {