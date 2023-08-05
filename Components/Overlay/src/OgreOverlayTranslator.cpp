a7dc6bde5ae87b85630b979511317ba64c6950a3
Pavel Rojtberg <rojtberg@gmail.com>
2023-08-05 15:41:21 +0200

    Overlay: TextArea - draw missing space & deprecate space_width property

--- Components/Overlay/src/OgreOverlayTranslator.cpp
+++ Components/Overlay/src/OgreOverlayTranslator.cpp
@@ -216,6 +216,9 @@ void ElementTranslator::translate(ScriptCompiler* compiler, const AbstractNodePt
                 succ = getString(prop->values.front(), &val);
             }
 
+            if(prop->name == "space_width")
+                compiler->addError(ScriptCompiler::CE_DEPRECATEDSYMBOL, prop->file, prop->line, prop->name);
+
             if(!succ || !newElement->setParameter(prop->name, val))
                 compiler->addError(ScriptCompiler::CE_INVALIDPARAMETERS, prop->file, prop->line);
         }
