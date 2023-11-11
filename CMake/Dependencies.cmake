956e722481481e9f79d0748f406d6eada4b028f9
Pavel Rojtberg <rojtberg@gmail.com>
2023-11-11 23:02:19 +0100

    Dependencies: update Assimp to version 5.3.1

--- CMake/Dependencies.cmake
+++ CMake/Dependencies.cmake
@@ -160,10 +160,10 @@ if(OGRE_BUILD_DEPENDENCIES AND NOT EXISTS ${OGREDEPS_PATH})
 
       message(STATUS "Building Assimp")
       file(DOWNLOAD
-              https://github.com/assimp/assimp/archive/refs/tags/v5.2.5.zip
-          ${PROJECT_BINARY_DIR}/v5.2.5.tar.gz)
+              https://github.com/assimp/assimp/archive/refs/tags/v5.3.1.zip
+          ${PROJECT_BINARY_DIR}/v5.3.1.tar.gz)
       execute_process(COMMAND ${CMAKE_COMMAND}
-          -E tar xf v5.2.5.tar.gz WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+          -E tar xf v5.3.1.tar.gz WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
       execute_process(COMMAND ${BUILD_COMMAND_COMMON}
           -DZLIB_ROOT=${OGREDEPS_PATH}
           -DBUILD_SHARED_LIBS=OFF
@@ -171,10 +171,10 @@ if(OGRE_BUILD_DEPENDENCIES AND NOT EXISTS ${OGREDEPS_PATH})
           -DASSIMP_NO_EXPORT=TRUE
           -DASSIMP_BUILD_OGRE_IMPORTER=OFF
           -DASSIMP_BUILD_ASSIMP_TOOLS=OFF
-          ${PROJECT_BINARY_DIR}/assimp-5.2.5
-          WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/assimp-5.2.5)
+          ${PROJECT_BINARY_DIR}/assimp-5.3.1
+          WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/assimp-5.3.1)
       execute_process(COMMAND ${CMAKE_COMMAND}
-        --build ${PROJECT_BINARY_DIR}/assimp-5.2.5 ${BUILD_COMMAND_OPTS})
+        --build ${PROJECT_BINARY_DIR}/assimp-5.3.1 ${BUILD_COMMAND_OPTS})
     endif()
 
     message(STATUS "Building Bullet")
