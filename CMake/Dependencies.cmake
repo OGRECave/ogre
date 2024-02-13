1886f5be03ea66a728e7f50d50c7ac6d2b3b4c5b
Pavel Rojtberg <rojtberg@gmail.com>
2024-02-13 16:04:54 +0100

    Update CMake/Dependencies.cmake

--- CMake/Dependencies.cmake
+++ CMake/Dependencies.cmake
@@ -148,7 +148,7 @@ if(OGRE_BUILD_DEPENDENCIES AND NOT EXISTS ${OGREDEPS_PATH})
       file(DOWNLOAD
           https://github.com/madler/zlib/releases/download/v1.3.1/zlib-1.3.1.tar.gz
           ${PROJECT_BINARY_DIR}/zlib-1.3.tar.gz
-          EXPECTED_HASH SHA256=ff0ba4c292013dbc27530b3a81e1f9a813cd39de01ca5e0f8bf355702efa593e)
+          EXPECTED_HASH SHA256=9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23)
       execute_process(COMMAND ${CMAKE_COMMAND}
           -E tar xf zlib-1.3.tar.gz WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
       execute_process(COMMAND ${BUILD_COMMAND_COMMON}
