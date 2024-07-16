4f974bdf4a9e30eea2ba659df5f9e179224bdc83
Frank Goyens <frankgoyens@hotmail.com>
2024-07-16 15:57:41 +0200

    Either increment the iterator or assign when erasing
    Erasing the element invalidates the iterator so this is not possible in a simple range-based for loop causing a crash

--- OgreMain/src/OgreRenderSystem.cpp
+++ OgreMain/src/OgreRenderSystem.cpp
@@ -556,14 +556,18 @@ namespace Ogre {
         // Remove all the render targets. Destroy primary target last since others may depend on it.
         // Keep mRenderTargets valid all the time, so that render targets could receive
         // appropriate notifications, for example FBO based about GL context destruction.
-        RenderTarget* primary {nullptr};
-        for (auto &&a : mRenderTargets) {
-            if (!primary && a.second->isPrimary()) {
-                primary = a.second;
+        RenderTarget* primary{nullptr};
+        for (auto it = mRenderTargets.begin(); it != mRenderTargets.end();)
+        {
+            if (!primary && it->second->isPrimary())
+            {
+                primary = it->second;
+                ++it;
                 continue;
             }
-            OGRE_DELETE a.second;
-            mRenderTargets.erase(a.first);
+
+            OGRE_DELETE it->second;
+            it = mRenderTargets.erase(it);
         }
         OGRE_DELETE primary;
         mRenderTargets.clear();
