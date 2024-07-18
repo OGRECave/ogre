2ebaedde3935eeca6463e5356bec7181183cd9bb
Joilnen <joilnen@gmail.com>
2024-07-18 20:25:20 -0300

    Update Components/RTShaderSystem/src/OgreShaderGenerator.cpp
    Co-authored-by: Pavel Rojtberg <rojtberg@gmail.com>

--- Components/RTShaderSystem/src/OgreShaderGenerator.cpp
+++ Components/RTShaderSystem/src/OgreShaderGenerator.cpp
@@ -965,7 +965,7 @@ bool ShaderGenerator::cloneShaderBasedTechniques(Material& srcMat, Material& dst
                         RenderState* srcRenderState = t->getRenderState(pi);
                         RenderState* dstRenderState = getRenderState(srcToTechniqueScheme, dstMat, pi);
 
-                        for(const auto& s : srcRenderState->getSubRenderStates())
+                        for(SubRenderState* srcSubState : srcRenderState->getSubRenderStates())
                         {
                             SubRenderState* srcSubState = s;
                             SubRenderState* dstSubState = createSubRenderState(srcSubState->getType());
