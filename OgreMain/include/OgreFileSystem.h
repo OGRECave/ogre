fcec00897be9dc0d44cee15caea004ca2138dd43
Pavel Rojtberg <rojtberg@gmail.com>
2024-08-10 23:06:53 +0200

    Main: suppress some false doxygen warnings

--- OgreMain/include/OgreFileSystem.h
+++ OgreMain/include/OgreFileSystem.h
@@ -57,7 +57,9 @@ namespace Ogre {
         /// @copydoc FactoryObj::getType
         const String& getType(void) const override;
 
+        //! @cond Doxygen_Suppress
         using ArchiveFactory::createInstance;
+        //! @endcond
 
         Archive *createInstance( const String& name, bool readOnly ) override;
 
