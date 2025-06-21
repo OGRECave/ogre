43c677fbbc34ae3a7f9349bde079d98a9abbf680
Pavel Rojtberg <rojtberg@gmail.com>
2025-06-21 13:12:13 +0200

    Overlay: ImGui - preserve alpha in output buffer
    as is done upstream

--- Components/Overlay/src/OgreImGuiOverlay.cpp
+++ Components/Overlay/src/OgreImGuiOverlay.cpp
@@ -100,10 +100,8 @@ void ImGuiOverlay::ImGUIRenderable::createMaterial()
     Pass* mPass = mMaterial->getTechnique(0)->getPass(0);
     mPass->setCullingMode(CULL_NONE);
     mPass->setVertexColourTracking(TVC_DIFFUSE);
-    mPass->setSceneBlending(SBT_TRANSPARENT_ALPHA);
-    mPass->setSeparateSceneBlendingOperation(SBO_ADD, SBO_ADD);
-    mPass->setSeparateSceneBlending(SBF_SOURCE_ALPHA, SBF_ONE_MINUS_SOURCE_ALPHA,
-                                    SBF_ONE_MINUS_SOURCE_ALPHA, SBF_ZERO);
+    mPass->setSceneBlendingOperation(SBO_ADD);
+    mPass->setSeparateSceneBlending(SBF_SOURCE_ALPHA, SBF_ONE_MINUS_SOURCE_ALPHA, SBF_ONE, SBF_ONE_MINUS_SOURCE_ALPHA);
 
     TextureUnitState* mTexUnit = mPass->createTextureUnitState();
     mTexUnit->setTexture(mFontTex);
