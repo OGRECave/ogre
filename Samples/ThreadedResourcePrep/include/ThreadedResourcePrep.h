8c42396dc4422bdfd6f955670583077ae6fa6db5
ohlidalp <petr@ohlidal.cz>
2025-09-21 16:51:42 +0200

    Added empty 'ThreadedResourcePrep' sample.

---
+++ Samples/ThreadedResourcePrep/include/ThreadedResourcePrep.h
@@ -0,0 +1,32 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2014 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+You may use this sample code for anything you like, it is not covered by the
+same license as the rest of the engine.
+-----------------------------------------------------------------------------
+*/
+
+#ifndef _Sample_ThreadedResourcePrep_H_
+#define _Sample_ThreadedResourcePrep_H_
+
+#include "SdkSample.h"
+#include "SamplePlugin.h"
+
+#include <Ogre.h>
+
+using namespace Ogre;
+using namespace OgreBites;
+
+class _OgreSampleClassExport Sample_ThreadedResourcePrep : public SdkSample
+{
+public:
+    Sample_ThreadedResourcePrep();
+};
+
+#endif // _Sample_ThreadedResourcePrep_H_
---
+++ Samples/ThreadedResourcePrep/src/ThreadedResourcePrep.cpp
@@ -0,0 +1,22 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2014 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+You may use this sample code for anything you like, it is not covered by the
+same license as the rest of the engine.
+-----------------------------------------------------------------------------
+*/
+
+#include "ThreadedResourcePrep.h"
+
+Sample_ThreadedResourcePrep::Sample_ThreadedResourcePrep()
+{
+    mInfo["Title"] = "Threaded Resource Prep";
+    mInfo["Description"] = "Showcases and benchmarks background resource queue";
+    mInfo["Category"] = "Unsorted";
+}
