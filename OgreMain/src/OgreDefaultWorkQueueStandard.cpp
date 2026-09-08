75839850050711d7f99ac48dfd03ceba271073d4
Ichiro <dark.hades.1102@gmail.com>
2026-09-08 22:18:59 +0900

    Refactor thread name setting for Apple platforms

--- OgreMain/src/OgreDefaultWorkQueueStandard.cpp
+++ OgreMain/src/OgreDefaultWorkQueueStandard.cpp
@@ -68,8 +68,10 @@ namespace Ogre
 #  if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
                 // std::wstring wname(name.begin(), name.end());
                 // SetThreadDescription(static_cast<HANDLE>(t.native_handle()), wname.c_str());
-#  elif OGRE_PLATFORM == OGRE_PLATFORM_LINUX || OGRE_PLATFORM == OGRE_PLATFORM_APPLE_IOS || OGRE_PLATFORM == OGRE_PLATFORM_ANDROID
+#  elif OGRE_PLATFORM == OGRE_PLATFORM_LINUX || OGRE_PLATFORM == OGRE_PLATFORM_ANDROID
                 pthread_setname_np(pthread_self(), mName.substr(0, 15).c_str());
+#  elif OGRE_PLATFORM == OGRE_PLATFORM_APPLE_IOS || OGRE_PLATFORM == OGRE_PLATFORM_APPLE
+                pthread_setname_np(name.c_str());
 #  endif
                 _threadMain();
             });
